edb5446440e717b7ba96e2dca765689d823ac728
Oliver Woodman <olly@google.com>
2014-07-18 14:31:55 +0100

    Bump version to 1.0.11.

--- library/src/main/java/com/google/android/exoplayer/ExoPlayerLibraryInfo.java
+++ library/src/main/java/com/google/android/exoplayer/ExoPlayerLibraryInfo.java
@@ -26,7 +26,7 @@ public class ExoPlayerLibraryInfo {
   /**
    * The version of the library, expressed as a string.
    */
-  public static final String VERSION = "1.0.10";
+  public static final String VERSION = "1.0.11";
 
   /**
    * The version of the library, expressed as an integer.
