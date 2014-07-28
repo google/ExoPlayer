1ded0fc6db526611470a113b004087f84c8fd482
Martin Bonnin <martin.bonnin@dailymotion.com>
2014-07-28 12:35:01 +0200

    JNI compiles

--- library/jni/TSExtractorNative.c
+++ library/jni/TSExtractorNative.c
@@ -3,6 +3,7 @@
 #include <android/log.h>
 #include <inttypes.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define TAG "TSExtractorNativeJNI"
 
@@ -29,8 +30,9 @@ struct PayloadHandler {
     int pid;
     PayloadHandler *next;
     int isPES;
-    TSParser *parser;
+    TSParser *tsp;
     void (*destroy)(PayloadHandler *ph);
+    int cc_counter;
 };
 
 struct SectionHandler {
@@ -61,9 +63,15 @@ struct TSParser{
     PayloadHandler *activePayloadHandlerHead;
     PayloadHandler **activePayloadHandlerLastNext;
 
+    int audioConfigFound;
     int sampleRateIndex;
     int channelConfigIndex;
+
     int pmt_pid;
+
+    jobject inputStream;
+    int inputStreamFinished;
+    int incompleteReadPosition;
 };
 
 static inline void *_malloc(int size)
@@ -99,6 +107,7 @@ static void payload_handler_init(PayloadHandler *ph, TSParser *tsp, int pid)
 
 static void payload_handler_exit(PayloadHandler *ph)
 {
+    TSParser *tsp = ph->tsp;
     PayloadHandler *cur = tsp->activePayloadHandlerHead;
     PayloadHandler *prev = NULL;
 
@@ -133,14 +142,15 @@ static void section_handler_handle_payload(PayloadHandler *ph, uint8_t *packet, 
         offset++;
         int tableID = packet[offset++];
         if (sh->tableID != tableID) {
-            __android_log_print(ANDROID_LOG_ERROR, TAG, "unexepected tableID: " + tableID + " != " + sh->tableID);
+            __android_log_print(ANDROID_LOG_ERROR, TAG, "unexepected tableID: %d != %d", tableID, sh->tableID);
         }
-        sh->sectionLength = ((packet[offset++] & 0xf) << 8) | packet[offset++];
+        sh->sectionLength = ((packet[offset] & 0xf) << 8) | packet[offset+1];
+        offset += 2;
         if (sh->sectionLength > sh->sectionMaxSize) {
             if (sh->section)
                 _free(sh->section);
             sh->section = _malloc(sh->sectionLength * 2);
-            sh->sectionMaxSize = sectionLength * 2;
+            sh->sectionMaxSize = sh->sectionLength * 2;
         }
         sh->sectionWriteOffset = 0;
     }
@@ -154,15 +164,16 @@ static void section_handler_handle_payload(PayloadHandler *ph, uint8_t *packet, 
     if (sh->sectionWriteOffset == sh->sectionLength) {
         int sectionLength = sh->sectionLength;
         sh->sectionLength = 0;
-        sh->handleSection(sh, section, sectionLength);
+        sh->handleSection(sh, sh->section, sectionLength);
     }
 
 }
 
 static void section_handler_init(SectionHandler *sh, TSParser *tsp, int pid)
 {
-    payload_handler_init((PayloadHandler*)sh, tsp, pid);
-    sh->handlePayload = section_handler_handle_payload;
+    PayloadHandler *ph = (PayloadHandler*)sh;
+    payload_handler_init(ph, tsp, pid);
+    ph->handlePayload = section_handler_handle_payload;
 }
 
 static void section_handler_exit(SectionHandler*sh)
@@ -194,17 +205,19 @@ static SectionHandler *pmt_handler_create(TSParser *tsp, int pid)
 
 static void pat_handler_handle_section(SectionHandler *sh, uint8_t *data, int dataLength)
 {
+    int i;
+    TSParser *tsp = ((PayloadHandler*)sh)->tsp;
     // start from 5 to skip transport_stream_id, version_number, current_next_indicator, etc.
     // stop at length - 4 to skip crc
-    for (int i = 5; i < dataLength - 4; ){
+    for (i = 5; i < dataLength - 4; ){
         // XXX
-        int programNumber = (data[i] << 8) - data[i+1];
+        //int programNumber = (data[i] << 8) - data[i+1];
         i += 2;
         tsp->pmt_pid = ((data[i] & 0x1f) << 8) | data[i+1];
         i += 2;
         // we just take the first program
         __android_log_print(ANDROID_LOG_ERROR, TAG, "found PMT pid: %04x", tsp->pmt_pid);
-        pmt_handler_create(tsp, tsp->pmt_pid)
+        pmt_handler_create(tsp, tsp->pmt_pid);
     }
 
     // do not listen to PAT updates
@@ -228,34 +241,38 @@ static SectionHandler *pat_handler_create(TSParser *tsp, int pid)
 
 static TSParser *tsparser_create(JNIEnv *env, jobject thiz)
 {
-    TSParser *tsp = _mallocz(1, sizeof(*tsp));
+    TSParser *tsp = _mallocz(sizeof(*tsp));
     jfieldID fid;
     jclass cls;
+    int i;
 
-    cls = env->FindClass(env, "com/google/android/exoplayer/parser/ts/TSExtractorNative");
-    fid = env->GetFieldID(env, cls, "inputStream", "Lcom.google.android.exoplayer.upstream.NonBlockingInputStream")
+    cls = (*env)->FindClass(env, "com/google/android/exoplayer/parser/ts/TSExtractorNative");
+    fid = (*env)->GetFieldID(env, cls, "inputStream", "Lcom/google/android/exoplayer/upstream/NonBlockingInputStream;");
 
-    tsp->dataByteArray = env->NewGlobalRef(env->NewByteArray(env, BUFFER_SIZE));
-    tsp->data = env->GetByteArrayElements(env, tsp->dataByteArray);
+    tsp->dataByteArray = (*env)->NewGlobalRef(env, (*env)->NewByteArray(env, BUFFER_SIZE));
+    tsp->data = (uint8_t*)(*env)->GetByteArrayElements(env, tsp->dataByteArray, NULL);
 
-    for (int i = 0; i < TYPE_COUNT; i++) {
+    for (i = 0; i < TYPE_COUNT; i++) {
         tsp->sampleHead[i] = NULL;
         tsp->sampleLastNext[i] = &tsp->sampleHead[i];
     }
     tsp->activePayloadHandlerHead = NULL;
     tsp->activePayloadHandlerLastNext = &tsp->activePayloadHandlerHead;
 
-    tsp->inputStream = env->GetObjectField(env, thiz, fid)
+    tsp->inputStream = (*env)->NewGlobalRef(env, (*env)->GetObjectField(env, thiz, fid));
+
+    pat_handler_create(tsp, 0);
     return tsp;
 }
 
-static void tsparser_destroy(TSParser*tsp)
+static void tsparser_destroy(JNIEnv *env, TSParser*tsp)
 {
-    env->ReleaseByteArrayElements(env, tsp->dataByteArray, tsp->data, NULL);
-    env->DeleteGlobalRef(tsp->dataByteArray);
+    (*env)->ReleaseByteArrayElements(env, tsp->dataByteArray, (jbyte*)tsp->data, JNI_ABORT);
+    (*env)->DeleteGlobalRef(env, tsp->dataByteArray);
+    (*env)->DeleteGlobalRef(env, tsp->inputStream);
 }
 
-static void _refill_data(TSParser *tsp)
+static void _refill_data(JNIEnv *env, TSParser *tsp)
 {
     jint offset = 0;
     jint length = BUFFER_SIZE;
@@ -266,7 +283,10 @@ static void _refill_data(TSParser *tsp)
         length = BUFFER_SIZE - tsp->dataIncompletePosition;
     }
 
-    jint ret = env->CallIntMethod(env, tsp->inputStream, tsp->dataByteArray, offset, length);
+    jclass cls = (*env)->FindClass(env,  "com/google/android/exoplayer/upstream/NonBlockingInputStream");
+    jmethodID mid = (*env)->GetMethodID(env, cls, "read", "([BII)I");
+
+    jint ret = (*env)->CallIntMethod(env, tsp->inputStream, mid, tsp->dataByteArray, offset, length);
     if (ret == -1) {
         int padding;
         tsp->inputStreamFinished = 1;
@@ -274,7 +294,7 @@ static void _refill_data(TSParser *tsp)
         if (padding) {
             __android_log_print(ANDROID_LOG_ERROR, TAG, "TS file is not a multiple of 188 bytes ?");
         }
-        tsp->dataSize = 188 * ((tsp->dataSize + 187) / 188)
+        tsp->dataSize = 188 * ((tsp->dataSize + 187) / 188);
     } else {
         offset += ret;
         if (offset % 188) {
@@ -287,19 +307,23 @@ static void _refill_data(TSParser *tsp)
     }
 }
 
-static int tsparser_parse_one_packet(TSParser*tsp)
+#define PARSE_ONE_PACKET_AGAIN 0
+#define PARSE_ONE_PACKET_WAIT 1
+#define PARSE_ONE_PACKET_FINISHED 2
+
+static int tsparser_parse_one_packet(JNIEnv *env, TSParser*tsp)
 {
-    uitn8_t *packet;
-    if (tsp->dataPosition == tsp->dataSize || tsp->incompleteReadPosition) {
-        _refill_data(tsp);
+    uint8_t *packet;
+    if (tsp->dataPosition == tsp->dataSize || tsp->incompleteReadPosition) {
+        _refill_data(env, tsp);
         if (tsp->inputStreamFinished) {
-            return -1;
-        } else if (tsp->dataPosition == tsp->dataSize || tsp->incompleteReadPosition) {
-            return -1;
+            return PARSE_ONE_PACKET_FINISHED;
+        } else if (tsp->dataPosition == tsp->dataSize || tsp->incompleteReadPosition) {
+            return PARSE_ONE_PACKET_WAIT;
         }
     }
 
-    packet = tsp->data[tsp->dataPosition];
+    packet = &tsp->data[tsp->dataPosition];
     tsp->dataPosition += 188;
 
     int unitStart = (packet[1] & 0x40) != 0;
@@ -315,12 +339,12 @@ static int tsparser_parse_one_packet(TSParser*tsp)
         }
     }
     if (!payloadHandler) {
-        return 0;
+        return PARSE_ONE_PACKET_AGAIN;
     }
 
     int expected_cc_counter = (payloadHandler->cc_counter + 1) & 0xf;
     if (expected_cc_counter != cc_counter) {
-        __android_log_print(ANDROID_LOG_DEBUG, TAG, "cc_error: " + payloadHandler->cc_counter + " -> " + cc_counter);
+        __android_log_print(ANDROID_LOG_DEBUG, TAG, "cc_error: %d -> %d", payloadHandler->cc_counter, cc_counter);
     }
     payloadHandler->cc_counter = cc_counter;
 
@@ -332,7 +356,9 @@ static int tsparser_parse_one_packet(TSParser*tsp)
         payload_offset += packet[4] + 1;
     }
 
-    payloadHandler->handlePayload(packet, payload_offset, unit_start);
+    payloadHandler->handlePayload(payloadHandler, packet, payload_offset, unitStart);
+
+    return PARSE_ONE_PACKET_AGAIN;
 
 }
 
@@ -341,19 +367,19 @@ static TSParser *_retrieve_tsp(JNIEnv *env, jobject thiz)
     jfieldID fid;
     jclass cls;
 
-    cls = env->FindClass(env, "com/google/android/exoplayer/parser/ts/TSExtractorNative");
-    fid = env->GetFieldID(env, cls, "nativeHandle", "J")
-    return (TSParser*)env->GetLongField(env, thiz, fid);
+    cls = (*env)->FindClass(env, "com/google/android/exoplayer/parser/ts/TSExtractorNative");
+    fid = (*env)->GetFieldID(env, cls, "nativeHandle", "J");
+    return (TSParser*)(*env)->GetLongField(env, thiz, fid);
 }
 
-static void _set_tsp(JNIEnv *env, jobject thiz)
+static void _set_tsp(JNIEnv *env, jobject thiz, TSParser *tsp)
 {
     jfieldID fid;
     jclass cls;
 
-    cls = env->FindClass(env, "com/google/android/exoplayer/parser/ts/TSExtractorNative");
-    fid = env->GetFieldID(env, cls, "nativeHandle", "J")
-    env->SetLongField(env, thiz, fid, (jlong)tsp);
+    cls = (*env)->FindClass(env, "com/google/android/exoplayer/parser/ts/TSExtractorNative");
+    fid = (*env)->GetFieldID(env, cls, "nativeHandle", "J");
+    (*env)->SetLongField(env, thiz, fid, (jlong)tsp);
 }
 
 void Java_com_google_android_exoplayer_parser_ts_TSExtractorNative_nativeInit(JNIEnv* env, jobject thiz)
@@ -364,7 +390,7 @@ void Java_com_google_android_exoplayer_parser_ts_TSExtractorNative_nativeInit(JN
 
     tsp = tsparser_create(env, thiz);
 
-    _set_tsp(tsp);
+    _set_tsp(env, thiz, tsp);
 }
 
 static void _fill_holder(JNIEnv *env, jobject holder, Sample *sample)
@@ -375,32 +401,32 @@ static void _fill_holder(JNIEnv *env, jobject holder, Sample *sample)
     jobject data;
     jlong capacity;
 
-    cls = env->FindClass(env, "com/google/android/exoplayer/SampleHolder");
-    fid = env->GetFieldID(env, cls, "data", "Ljava/nio/ByteBuffer");
-    data = env->GetObjectField(env, thiz, fid);
+    cls = (*env)->FindClass(env, "com/google/android/exoplayer/SampleHolder");
+    fid = (*env)->GetFieldID(env, cls, "data", "Ljava/nio/ByteBuffer;");
+    data = (*env)->GetObjectField(env, holder, fid);
     if (!data) {
         __android_log_print(ANDROID_LOG_ERROR, TAG, "holder has no ByteBuffer");
         return;
     }
 
-    ptr = env->GetDirectBufferAddress(env, data);
+    ptr = (*env)->GetDirectBufferAddress(env, data);
     if (!ptr) {
         __android_log_print(ANDROID_LOG_ERROR, TAG, "ByteBuffer is not direct");
         return;
     }
 
-    capacity = env->GetDirectBufferCapacity(env, data);
+    capacity = (*env)->GetDirectBufferCapacity(env, data);
     if (capacity < sample->size) {
         memcpy(ptr, sample->data, capacity);
         __android_log_print(ANDROID_LOG_ERROR, TAG, "ByteBuffer capacity is too small %d < %d", (int)capacity, sample->size);
     } else {
         memcpy(ptr, sample->data, sample->size);
     }
-    fid = env->GetFieldID(env, cls, "timeUs", "J");
-    env->SetObjectField(env, thiz, fid, sample->timeUs);
-    // set SAMPLE_FLAG_SYNC
-    fid = env->GetFieldID(env, cls, "flags", "I");
-    env->SetObjectField(env, thiz, fid, 1);
+    fid = (*env)->GetFieldID(env, cls, "timeUs", "J");
+    (*env)->SetLongField(env, holder, fid, sample->timeUs);
+#define SAMPLE_FLAG_SYNC 1
+    fid = (*env)->GetFieldID(env, cls, "flags", "I");
+    (*env)->SetIntField(env, holder, fid, SAMPLE_FLAG_SYNC);
 
 }
 
@@ -414,14 +440,14 @@ jint Java_com_google_android_exoplayer_parser_ts_TSExtractorNative_nativeRead(JN
         return RESULT_END_OF_STREAM;
     }
 
-    while (tsp->sampleHead[type] == null) {
-        if (tsparser_parse_one_packet() != 0) {
+    while (tsp->sampleHead[type] == NULL) {
+        if (tsparser_parse_one_packet(env, tsp) != PARSE_ONE_PACKET_AGAIN) {
             break;
         };
     }
 
     if (tsp->sampleHead[type]) {
-        _fill_holder(JNIEnv *env, jobject, holder, tsp->sampleHead[type]);
+        _fill_holder(env, holder, tsp->sampleHead[type]);
 
         // remove from list
         *tsp->sampleLastNext[type] = tsp->sampleHead[type]->next;
@@ -448,7 +474,12 @@ jint Java_com_google_android_exoplayer_parser_ts_TSExtractorNative_nativeGetSamp
         return 0;
     }
 
-    return 0;
+    while (!tsp->audioConfigFound) {
+        if (tsparser_parse_one_packet(env, tsp) == PARSE_ONE_PACKET_FINISHED)
+            break;
+    }
+
+    return tsp->sampleRateIndex;
 }
 
 jint Java_com_google_android_exoplayer_parser_ts_TSExtractorNative_nativeGetChannelConfigIndex(JNIEnv* env, jobject thiz)
@@ -461,7 +492,12 @@ jint Java_com_google_android_exoplayer_parser_ts_TSExtractorNative_nativeGetChan
         return 0;
     }
 
-    return 0;
+    while (!tsp->audioConfigFound) {
+        if (tsparser_parse_one_packet(env, tsp) == PARSE_ONE_PACKET_FINISHED)
+            break;
+    }
+
+    return tsp->channelConfigIndex;
 }
 
 jboolean Java_com_google_android_exoplayer_parser_ts_TSExtractorNative_nativeIsReadFinished(JNIEnv* env, jobject thiz)
