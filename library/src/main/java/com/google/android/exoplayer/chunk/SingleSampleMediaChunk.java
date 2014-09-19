b2fc944af14c6d87e6eeb48ef5dc8e4bf6906ee7
Oliver Woodman <olly@google.com>
2014-09-19 18:26:04 +0100

    Remove getLoadedData API from ExoPlayer components.
    This API wasn't particularly nice. Best to remove it whilst
    hopefully no-one is using it. Leaving the ReadHead abstraction
    in place, since it might well prove useful in the future.

--- library/src/main/java/com/google/android/exoplayer/chunk/SingleSampleMediaChunk.java
+++ library/src/main/java/com/google/android/exoplayer/chunk/SingleSampleMediaChunk.java
@@ -64,10 +64,8 @@ public class SingleSampleMediaChunk extends MediaChunk {
    * @param nextChunkIndex The index of the next chunk, or -1 if this is the last chunk.
    * @param sampleFormat The format of the media contained by the chunk.
    * @param headerData Custom header data for the sample. May be null. If set, the header data is
-   *     prepended to the sample data returned when {@link #read(SampleHolder)} is called. It is
-   *     however not considered part of the loaded data, and so is not prepended to the data
-   *     returned by {@link #getLoadedData()}. It is also not reflected in the values returned by
-   *     {@link #bytesLoaded()} and {@link #getLength()}.
+   *     prepended to the sample data returned when {@link #read(SampleHolder)} is called. It is not
+   *     reflected in the values returned by {@link #bytesLoaded()} and {@link #getLength()}.
    */
   public SingleSampleMediaChunk(DataSource dataSource, DataSpec dataSpec, Format format,
       int trigger, long startTimeUs, long endTimeUs, int nextChunkIndex, MediaFormat sampleFormat,
