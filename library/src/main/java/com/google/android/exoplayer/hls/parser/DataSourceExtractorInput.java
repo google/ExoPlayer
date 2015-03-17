e54d07c1b09ee051d4ceedcde12f50433111c45d
Oliver Woodman <olly@google.com>
2015-03-17 12:36:46 +0000

    Fix issue with new extractor input

--- library/src/main/java/com/google/android/exoplayer/hls/parser/DataSourceExtractorInput.java
+++ library/src/main/java/com/google/android/exoplayer/hls/parser/DataSourceExtractorInput.java
@@ -82,15 +82,15 @@ public final class DataSourceExtractorInput implements ExtractorInput {
       if (Thread.interrupted()) {
         throw new InterruptedException();
       }
-      int bytesRead = dataSource.read(SCRATCH_SPACE, 0, remaining);
+      int bytesRead = dataSource.read(SCRATCH_SPACE, 0, Math.min(SCRATCH_SPACE.length, remaining));
       if (bytesRead == -1) {
         isEnded = true;
-        return true;
+        return false;
       }
       remaining -= bytesRead;
     }
     position += length;
-    return false;
+    return true;
   }
 
   @Override
