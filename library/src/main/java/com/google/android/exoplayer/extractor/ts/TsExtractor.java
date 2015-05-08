d8af120b98b7fbbba6e5abaaf540c29ef502bbff
Oliver Woodman <olly@google.com>
2015-05-08 17:06:59 +0100

    Fix treating all DVB data as AC-3.
    Issue #434

--- library/src/main/java/com/google/android/exoplayer/extractor/ts/TsExtractor.java
+++ library/src/main/java/com/google/android/exoplayer/extractor/ts/TsExtractor.java
@@ -43,7 +43,6 @@ public final class TsExtractor implements Extractor, SeekMap {
 
   private static final int TS_STREAM_TYPE_AAC = 0x0F;
   private static final int TS_STREAM_TYPE_ATSC_AC3 = 0x81;
-  private static final int TS_STREAM_TYPE_DVB_AC3 = 0x06;
   private static final int TS_STREAM_TYPE_H264 = 0x1B;
   private static final int TS_STREAM_TYPE_ID3 = 0x15;
   private static final int TS_STREAM_TYPE_EIA608 = 0x100; // 0xFF + 1
@@ -308,13 +307,13 @@ public final class TsExtractor implements Extractor, SeekMap {
           continue;
         }
 
+        // TODO: Detect and read DVB AC-3 streams with Ac3Reader.
         ElementaryStreamReader pesPayloadReader = null;
         switch (streamType) {
           case TS_STREAM_TYPE_AAC:
             pesPayloadReader = new AdtsReader(output.track(TS_STREAM_TYPE_AAC));
             break;
           case TS_STREAM_TYPE_ATSC_AC3:
-          case TS_STREAM_TYPE_DVB_AC3:
             pesPayloadReader = new Ac3Reader(output.track(streamType));
             break;
           case TS_STREAM_TYPE_H264:
