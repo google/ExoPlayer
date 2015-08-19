ea7caf4f91e29dc4787666a50e12009df08f78d1
Oliver Woodman <olly@google.com>
2015-08-19 16:32:33 +0100

    Bit of a hack to pass through original MediaFormat from FrameworkSampleSource.

--- library/src/main/java/com/google/android/exoplayer/FrameworkSampleSource.java
+++ library/src/main/java/com/google/android/exoplayer/FrameworkSampleSource.java
@@ -315,10 +315,12 @@ public final class FrameworkSampleSource implements SampleSource, SampleSourceRe
     }
     long durationUs = format.containsKey(android.media.MediaFormat.KEY_DURATION)
         ? format.getLong(android.media.MediaFormat.KEY_DURATION) : C.UNKNOWN_TIME_US;
-    return new MediaFormat(mimeType, maxInputSize, durationUs, width, height, rotationDegrees,
-        MediaFormat.NO_VALUE, channelCount, sampleRate, language,
+    MediaFormat mediaFormat = new MediaFormat(mimeType, maxInputSize, durationUs, width, height,
+        rotationDegrees, MediaFormat.NO_VALUE, channelCount, sampleRate, language,
         MediaFormat.OFFSET_SAMPLE_RELATIVE, initializationData, false, MediaFormat.NO_VALUE,
         MediaFormat.NO_VALUE);
+    mediaFormat.setFrameworkFormatV16(format);
+    return mediaFormat;
   }
 
   @TargetApi(16)
