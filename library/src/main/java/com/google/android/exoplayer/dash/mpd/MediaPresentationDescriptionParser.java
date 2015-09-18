cea3d10dc32c1557d1ab6820f9484f628e7ac744
Oliver Woodman <olly@google.com>
2015-09-18 18:25:07 +0100

    Stylistic tweaks.

--- library/src/main/java/com/google/android/exoplayer/dash/mpd/MediaPresentationDescriptionParser.java
+++ library/src/main/java/com/google/android/exoplayer/dash/mpd/MediaPresentationDescriptionParser.java
@@ -590,10 +590,13 @@ public class MediaPresentationDescriptionParser extends DefaultHandler
 
   protected int parseAudioChannelConfiguration(XmlPullParser xpp)
       throws XmlPullParserException, IOException {
-    int audioChannels = -1;
-    String schemeIdUri = parseString(xpp, "schemeIdUri", "");
-    if (schemeIdUri.equals("urn:mpeg:dash:23003:3:audio_channel_configuration:2011"))
+    int audioChannels;
+    String schemeIdUri = parseString(xpp, "schemeIdUri", null);
+    if ("urn:mpeg:dash:23003:3:audio_channel_configuration:2011".equals(schemeIdUri)) {
       audioChannels = parseInt(xpp, "value");
+    } else {
+      audioChannels = -1;
+    }
     do {
       xpp.next();
     } while (!ParserUtil.isEndTag(xpp, "AudioChannelConfiguration"));
