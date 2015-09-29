3e36f529f8fe8ef4c3550dc00f16684c532d0da0
joli <jeoliva@gmail.com>
2015-09-29 16:20:41 +0200

    FLV Support - Added Video Reader and parsing improvements

--- library/src/main/java/com/google/android/exoplayer/extractor/flv/AudioTagPayloadReader.java
+++ library/src/main/java/com/google/android/exoplayer/extractor/flv/AudioTagPayloadReader.java
@@ -1,8 +1,21 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.android.exoplayer.extractor.flv;
 
-import android.util.Log;
 import android.util.Pair;
-
 import com.google.android.exoplayer.C;
 import com.google.android.exoplayer.MediaFormat;
 import com.google.android.exoplayer.extractor.TrackOutput;
@@ -14,48 +27,27 @@ import com.google.android.exoplayer.util.ParsableByteArray;
 import java.util.Collections;
 
 /**
- * Created by joliva on 9/27/15.
+ * Parses audio tags of from an FLV stream and extracts AAC frames.
  */
-public class AudioTagReader extends TagReader{
-
-  private static final String TAG = "AudioTagReader";
-
+final class AudioTagPayloadReader extends TagPayloadReader {
   // Sound format
-  private static final int AUDIO_FORMAT_LINEAR_PCM_PLATFORM_ENDIAN = 0;
-  private static final int AUDIO_FORMAT_ADPCM = 1;
-  private static final int AUDIO_FORMAT_MP3 = 2;
-  private static final int AUDIO_FORMAT_LINEAR_PCM_LITTLE_ENDIAN = 3;
-  private static final int AUDIO_FORMAT_NELLYMOSER_16KHZ_MONO = 4;
-  private static final int AUDIO_FORMAT_NELLYMOSER_8KHZ_MONO = 5;
-  private static final int AUDIO_FORMAT_NELLYMOSER = 6;
-  private static final int AUDIO_FORMAT_G711_A_LAW = 7;
-  private static final int AUDIO_FORMAT_G711_MU_LAW = 8;
-  private static final int AUDIO_FORMAT_RESERVED = 9;
   private static final int AUDIO_FORMAT_AAC = 10;
-  private static final int AUDIO_FORMAT_SPEEX = 11;
-  private static final int AUDIO_FORMAT_MP3_8KHZ = 14;
-  private static final int AUDIO_FORMAT_DEVICE_SPECIFIC = 15;
 
   // AAC PACKET TYPE
   private static final int AAC_PACKET_TYPE_SEQUENCE_HEADER = 0;
   private static final int AAC_PACKET_TYPE_AAC_RAW = 1;
 
+  // SAMPLING RATES
   private static final int[] AUDIO_SAMPLING_RATE_TABLE = new int[] {
       5500, 11000, 22000, 44000
   };
 
-  private int format;
-  private int sampleRate;
-  private int bitsPerSample;
-  private int channels;
-
-  private boolean hasParsedAudioData;
+  // State variables
+  private boolean hasParsedAudioDataHeader;
   private boolean hasOutputFormat;
 
-  /**
-   * @param output A {@link TrackOutput} to which samples should be written.
-   */
-  public AudioTagReader(TrackOutput output) {
+
+  public AudioTagPayloadReader(TrackOutput output) {
     super(output);
   }
 
@@ -65,8 +57,10 @@ public class AudioTagReader extends TagReader{
   }
 
   @Override
-  protected void parseHeader(ParsableByteArray data) throws UnsupportedTrack {
-    if (!hasParsedAudioData) {
+  protected boolean parseHeader(ParsableByteArray data) throws UnsupportedTrack {
+    // Parse audio data header, if it was not done, to extract information
+    // about the audio codec and audio configuration.
+    if (!hasParsedAudioDataHeader) {
       int header = data.readUnsignedByte();
       int soundFormat = (header >> 4) & 0x0F;
       int sampleRateIndex = (header >> 2) & 0x03;
@@ -78,42 +72,29 @@ public class AudioTagReader extends TagReader{
       }
 
       if (!hasOutputFormat) {
-        switch (soundFormat) {
-          // raw audio data. Just creates media format
-          case AUDIO_FORMAT_LINEAR_PCM_LITTLE_ENDIAN:
-            output.format(MediaFormat.createAudioFormat(MimeTypes.AUDIO_RAW, MediaFormat.NO_VALUE,
-                MediaFormat.NO_VALUE, MediaFormat.NO_VALUE, channels,
-                AUDIO_SAMPLING_RATE_TABLE[sampleRateIndex], null, null));
-            hasOutputFormat = true;
-            break;
-
-          case AUDIO_FORMAT_AAC:
-            break;
-
-          case AUDIO_FORMAT_MP3:
-          case AUDIO_FORMAT_MP3_8KHZ:
-          case AUDIO_FORMAT_LINEAR_PCM_PLATFORM_ENDIAN:
-          default:
-            throw new UnsupportedTrack("Audio track not supported. Format: " + soundFormat +
-                ", Sample rate: " + sampleRateIndex + ", bps: " + bitsPerSample + ", channels: " +
-                channels);
+        // TODO: Adds support for MP3 and PCM
+        if (soundFormat != AUDIO_FORMAT_AAC) {
+          throw new UnsupportedTrack("Audio track not supported. Format: " + soundFormat +
+              ", Sample rate: " + sampleRateIndex + ", bps: " + bitsPerSample + ", channels: " +
+              channels);
         }
       }
 
-      this.format = soundFormat;
-      this.sampleRate = AUDIO_SAMPLING_RATE_TABLE[sampleRateIndex];
-      this.bitsPerSample = bitsPerSample;
-      this.channels = channels;
-
-      hasParsedAudioData = true;
+      hasParsedAudioDataHeader = true;
     } else {
+      // Skip header if it was parsed previously.
       data.skipBytes(1);
     }
+
+    // In all the cases we will be managing AAC format (otherwise an exception would be
+    // fired so we can just always return true
+    return true;
   }
 
   @Override
   protected void parsePayload(ParsableByteArray data, long timeUs) {
     int packetType = data.readUnsignedByte();
+    // Parse sequence header just in case it was not done before.
     if (packetType == AAC_PACKET_TYPE_SEQUENCE_HEADER && !hasOutputFormat) {
       ParsableBitArray adtsScratch = new ParsableBitArray(new byte[data.bytesLeft()]);
       data.readBytes(adtsScratch.data, 0, data.bytesLeft());
@@ -134,17 +115,11 @@ public class AudioTagReader extends TagReader{
       output.format(mediaFormat);
       hasOutputFormat = true;
     } else if (packetType == AAC_PACKET_TYPE_AAC_RAW) {
+      // Sample audio AAC frames
       int bytesToWrite = data.bytesLeft();
       output.sampleData(data, bytesToWrite);
       output.sampleMetadata(timeUs, C.SAMPLE_FLAG_SYNC, bytesToWrite, 0, null);
-
-      Log.d(TAG, "AAC TAG. Size: " + bytesToWrite + ", timeUs: " + timeUs);
     }
-  }
-
-  @Override
-  protected boolean shouldParsePayload() {
-    return (format == AUDIO_FORMAT_AAC);
   }
 
 }
--- library/src/main/java/com/google/android/exoplayer/extractor/flv/TagPayloadReader.java
+++ library/src/main/java/com/google/android/exoplayer/extractor/flv/TagPayloadReader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.android.exoplayer.extractor.flv;
 
 import com.google.android.exoplayer.C;
@@ -5,17 +20,19 @@ import com.google.android.exoplayer.extractor.TrackOutput;
 import com.google.android.exoplayer.util.ParsableByteArray;
 
 /**
- * Extracts individual samples from FLV tags.
+ * Extracts individual samples from FLV tags, preserving original order.
  */
-/* package */ abstract class TagReader {
+/* package */ abstract class TagPayloadReader {
 
   protected final TrackOutput output;
-  public long durationUs;
+
+  // Duration of the track
+  protected long durationUs;
 
   /**
    * @param output A {@link TrackOutput} to which samples should be written.
    */
-  protected TagReader(TrackOutput output) {
+  protected TagPayloadReader(TrackOutput output) {
     this.output = output;
     this.durationUs = C.UNKNOWN_TIME_US;
   }
@@ -32,8 +49,11 @@ import com.google.android.exoplayer.util.ParsableByteArray;
   /**
    * Parses tag header
    * @param data Buffer where the tag header is stored
+   * @return True if header was parsed successfully and then payload should be read;
+   * Otherwise, false
+   * @throws UnsupportedTrack
    */
-  protected abstract void parseHeader(ParsableByteArray data) throws UnsupportedTrack;
+  protected abstract boolean parseHeader(ParsableByteArray data) throws UnsupportedTrack;
 
   /**
    * Parses tag payload
@@ -43,25 +63,28 @@ import com.google.android.exoplayer.util.ParsableByteArray;
   protected abstract void parsePayload(ParsableByteArray data, long timeUs);
 
   /**
-   * Evaluate if for the current tag, payload should be parsed
-   * @return
-   */
-  protected abstract boolean shouldParsePayload();
-
-  /**
-   * Consumes (possibly partial) payload data.
+   * Consumes payload data.
    *
    * @param data The payload data to consume.
    * @param timeUs The timestamp associated with the payload.
    */
   public void consume(ParsableByteArray data, long timeUs) throws UnsupportedTrack {
-    parseHeader(data);
-
-    if (shouldParsePayload()) {
+    if (parseHeader(data)) {
       parsePayload(data, timeUs);
     }
   }
 
+  /**
+   * Sets duration in microseconds
+   * @param durationUs duration in microseconds
+   */
+  public void setDurationUs(long durationUs) {
+    this.durationUs = durationUs;
+  }
+
+  public long getDurationUs() {
+    return durationUs;
+  }
   /**
    * Thrown when format described in the AudioTrack is not supported
    */
