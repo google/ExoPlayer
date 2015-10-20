7d923c2d37bed54c1d6230a02ce16239c0aaa03a
Rik Heijdens <rik@jwplayer.com>
2015-10-20 14:51:31 -0400

    Parse NAME label from HLS Master Playlist
    The HlsPlaylistParser now parses the "NAME" property
    from the MasterHlsPlaylist and saves it in the Variant

--- library/src/main/java/com/google/android/exoplayer/hls/Variant.java
+++ library/src/main/java/com/google/android/exoplayer/hls/Variant.java
@@ -26,9 +26,11 @@ public final class Variant implements FormatWrapper {
 
   public final String url;
   public final Format format;
+  public final String name;
 
-  public Variant(int index, String url, int bitrate, String codecs, int width, int height) {
+  public Variant(int index, String name, String url, int bitrate, String codecs, int width, int height) {
     this.url = url;
+    this.name = name;
     format = new Format(Integer.toString(index), MimeTypes.APPLICATION_M3U8, width, height, -1, -1,
         -1, bitrate, null, codecs);
   }
