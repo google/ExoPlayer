e81a1dce0455ce2e8b863cadb23580e5ea217c44
aquilescanta <aquilescanta@google.com>
2016-03-07 04:01:30 -0800

    Consider udta box as leaf
    The MP4 standard considers the udta box as a regular container box.
    Quicktime, however, considers it as a container (of only leaf atoms)
    that can have a terminating 32 bit integer with value 0. Since this
    breaks the principle of not having content in container boxes, this
    CL considers the udta box as a leaf box that contains other boxes and
    does the parsing manually.
    
    Issue: #1315
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=116532740

--- library/src/main/java/com/google/android/exoplayer/extractor/mp4/Mp4Extractor.java
+++ library/src/main/java/com/google/android/exoplayer/extractor/mp4/Mp4Extractor.java
@@ -285,9 +285,9 @@ public final class Mp4Extractor implements Extractor, SeekMap {
     List<Mp4Track> tracks = new ArrayList<>();
     long earliestSampleOffset = Long.MAX_VALUE;
     GaplessInfo gaplessInfo = null;
-    Atom.ContainerAtom udta = moov.getContainerAtomOfType(Atom.TYPE_udta);
+    Atom.LeafAtom udta = moov.getLeafAtomOfType(Atom.TYPE_udta);
     if (udta != null) {
-      gaplessInfo = AtomParsers.parseUdta(udta);
+      gaplessInfo = AtomParsers.parseUdta(udta, isQuickTime);
     }
     for (int i = 0; i < moov.containerChildren.size(); i++) {
       Atom.ContainerAtom atom = moov.containerChildren.get(i);
@@ -440,7 +440,7 @@ public final class Mp4Extractor implements Extractor, SeekMap {
         || atom == Atom.TYPE_stsd || atom == Atom.TYPE_stts || atom == Atom.TYPE_stss
         || atom == Atom.TYPE_ctts || atom == Atom.TYPE_elst || atom == Atom.TYPE_stsc
         || atom == Atom.TYPE_stsz || atom == Atom.TYPE_stco || atom == Atom.TYPE_co64
-        || atom == Atom.TYPE_tkhd || atom == Atom.TYPE_ftyp || atom == Atom.TYPE_meta;
+        || atom == Atom.TYPE_tkhd || atom == Atom.TYPE_ftyp || atom == Atom.TYPE_udta;
   }
 
   /**
@@ -448,8 +448,7 @@ public final class Mp4Extractor implements Extractor, SeekMap {
    */
   private static boolean shouldParseContainerAtom(int atom) {
     return atom == Atom.TYPE_moov || atom == Atom.TYPE_trak || atom == Atom.TYPE_mdia
-        || atom == Atom.TYPE_minf || atom == Atom.TYPE_stbl || atom == Atom.TYPE_edts
-        || atom == Atom.TYPE_udta;
+        || atom == Atom.TYPE_minf || atom == Atom.TYPE_stbl || atom == Atom.TYPE_edts;
   }
 
   private static final class Mp4Track {
