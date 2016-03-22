5b06bfd5a8b04df836e681706f50dd132a917e5b
olly <olly@google.com>
2016-03-22 03:59:35 -0700

    Fixed TS+H262 playback when no pts is available
    If this situation is encountered, we assume that the encoder has a good reason to
    do this and use the last pts + frameDuration as new pts.
    
    Issue: #1295
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=117808961

--- library/src/main/java/com/google/android/exoplayer/extractor/ts/TsExtractor.java
+++ library/src/main/java/com/google/android/exoplayer/extractor/ts/TsExtractor.java
@@ -600,7 +600,7 @@ public final class TsExtractor implements Extractor {
 
     private void parseHeaderExtension() {
       pesScratch.setPosition(0);
-      timeUs = 0;
+      timeUs = C.UNKNOWN_TIME_US;
       if (ptsFlag) {
         pesScratch.skipBits(4); // '0010' or '0011'
         long pts = (long) pesScratch.readBits(3) << 30;
