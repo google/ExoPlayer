b9d71d8e0250e911eac4a4b944e26fc7b8a40aa4
Aaron Pittenger <apittenger@michiganlabs.com>
2016-05-16 18:26:52 -0400

    Updated documentation and cleaned up headers

--- library/src/main/java/com/google/android/exoplayer/metadata/id3/TIT2Frame.java
+++ library/src/main/java/com/google/android/exoplayer/metadata/id3/TIT2Frame.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.android.exoplayer.metadata.id3;
 
 /**
- * Created by apittenger on 5/11/16.
+ * TIT2 (Title/songname/content description) ID3 frame.
  */
 public class TIT2Frame extends Id3Frame {
 
