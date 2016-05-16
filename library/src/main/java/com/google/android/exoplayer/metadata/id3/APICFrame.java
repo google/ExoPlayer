78566a77c4f918c6ed3848dae985e99887536571
Aaron Pittenger <apittenger@michiganlabs.com>
2016-05-16 18:39:03 -0400

    Updated indentation to be 2 spaces instead of 4

--- library/src/main/java/com/google/android/exoplayer/metadata/id3/APICFrame.java
+++ library/src/main/java/com/google/android/exoplayer/metadata/id3/APICFrame.java
@@ -18,24 +18,23 @@ package com.google.android.exoplayer.metadata.id3;
 /**
  * APIC (Attached Picture) ID3 frame.
  */
-public class APICFrame extends Id3Frame{
-    public static final String ID = "APIC";
+public class APICFrame extends Id3Frame {
+  public static final String ID = "APIC";
 
-    public final byte encoding;
-    public final String mimeType;
-    public final byte pictureType;
-    public final String description;
-    public final byte[] pictureData;
+  public final byte encoding;
+  public final String mimeType;
+  public final byte pictureType;
+  public final String description;
+  public final byte[] pictureData;
 
-    public APICFrame(byte encoding, String mimeType, byte pictureType, String description, byte[] pictureData) {
-        super(ID);
-        this.encoding = encoding;
-        this.mimeType = mimeType;
-        this.pictureType = pictureType;
-        this.description = description;
-        this.pictureData = pictureData;
-    }
-
+  public APICFrame(byte encoding, String mimeType, byte pictureType, String description, byte[] pictureData) {
+    super(ID);
+    this.encoding = encoding;
+    this.mimeType = mimeType;
+    this.pictureType = pictureType;
+    this.description = description;
+    this.pictureData = pictureData;
+  }
 
 
 }
