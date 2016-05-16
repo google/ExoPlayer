f18063bf053d37ecef1cf889806c9e62dc581e3f
Aaron Pittenger <apittenger@michiganlabs.com>
2016-05-16 18:26:52 -0400

    Updated documentation and cleaned up headers

--- library/src/main/java/com/google/android/exoplayer/metadata/id3/APICFrame.java
+++ library/src/main/java/com/google/android/exoplayer/metadata/id3/APICFrame.java
@@ -1,12 +1,23 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.android.exoplayer.metadata.id3;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-
 /**
- * Created by apittenger on 5/11/16.
+ * APIC (Attached Picture) ID3 frame.
  */
-
 public class APICFrame extends Id3Frame{
     public static final String ID = "APIC";
 
