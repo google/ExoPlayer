77a28757cfec76a5acb21b8900944f4094d78b9b
Nathan Bruning <nathan@innovattic.com>
2016-06-17 17:30:33 +0200

    DASH: silently ignore changing representation set in live stream

--- library/src/main/java/com/google/android/exoplayer/dash/DashChunkSource.java
+++ library/src/main/java/com/google/android/exoplayer/dash/DashChunkSource.java
@@ -1018,11 +1018,18 @@ public class DashChunkSource implements ChunkSource, Output {
       List<Representation> representations = period.adaptationSets
           .get(selectedTrack.adaptationSetIndex).representations;
 
-      for (int j = 0; j < representationIndices.length; j++) {
-        Representation representation = representations.get(representationIndices[j]);
-        representationHolders.get(representation.format.id).updateRepresentation(periodDurationUs,
-            representation);
+      for (Representation representation : representations) {
+        String id = representation.format.id;
+        RepresentationHolder holder = representationHolders.get(id);
+        if (holder == null) {
+          // Don't assume the representations in this period are the same as
+          // in the first requested period (handle gracefully). See class comment above.
+          Log.w(TAG, "Unexpected representation, ignoring: " + id);
+          continue;
+        }
+        holder.updateRepresentation(periodDurationUs, representation);
       }
+
       updateRepresentationIndependentProperties(periodDurationUs,
           representations.get(representationIndices[0]));
     }
