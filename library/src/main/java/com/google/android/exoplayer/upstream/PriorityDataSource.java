660b26e3ad18c5d0be339c030b7255ec43835fcf
olly <olly@google.com>
2016-07-06 03:22:52 -0700

    Make NetworkLock non-static and less specific. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=126682871

--- library/src/main/java/com/google/android/exoplayer/upstream/PriorityDataSource.java
+++ library/src/main/java/com/google/android/exoplayer/upstream/PriorityDataSource.java
@@ -16,39 +16,51 @@
 package com.google.android.exoplayer.upstream;
 
 import com.google.android.exoplayer.util.Assertions;
+import com.google.android.exoplayer.util.PriorityTaskManager;
 
 import android.net.Uri;
 
 import java.io.IOException;
 
 /**
- * Allows {@link #open(DataSpec)} and {@link #read(byte[], int, int)} calls only if the specified
- * priority is the highest priority of any task. {@link NetworkLock.PriorityTooLowException} is
- * thrown when this condition does not hold.
+ * A {@link DataSource} that can be used as part of a task registered with a
+ * {@link PriorityTaskManager}.
+ * <p>
+ * Calls to {@link #open(DataSpec)} and {@link #read(byte[], int, int)} are allowed to proceed only
+ * if there are no higher priority tasks registered to the {@link PriorityTaskManager}. If there
+ * exists a higher priority task then {@link PriorityTaskManager.PriorityTooLowException} is thrown.
+ * <p>
+ * Instances of this class are intended to be used as parts of (possibly larger) tasks that are
+ * registered with the {@link PriorityTaskManager}, and hence do <em>not</em> register as tasks
+ * themselves.
  */
 public final class PriorityDataSource implements DataSource {
 
   private final DataSource upstream;
+  private final PriorityTaskManager priorityTaskManager;
   private final int priority;
 
   /**
-   * @param priority The priority of the source.
    * @param upstream The upstream {@link DataSource}.
+   * @param priorityTaskManager The priority manager to which the task is registered.
+   * @param priority The priority of the task.
    */
-  public PriorityDataSource(int priority, DataSource upstream) {
-    this.priority = priority;
+  public PriorityDataSource(DataSource upstream, PriorityTaskManager priorityTaskManager,
+      int priority) {
     this.upstream = Assertions.checkNotNull(upstream);
+    this.priorityTaskManager = Assertions.checkNotNull(priorityTaskManager);
+    this.priority = priority;
   }
 
   @Override
   public long open(DataSpec dataSpec) throws IOException {
-    NetworkLock.instance.proceedOrThrow(priority);
+    priorityTaskManager.proceedOrThrow(priority);
     return upstream.open(dataSpec);
   }
 
   @Override
   public int read(byte[] buffer, int offset, int max) throws IOException {
-    NetworkLock.instance.proceedOrThrow(priority);
+    priorityTaskManager.proceedOrThrow(priority);
     return upstream.read(buffer, offset, max);
   }
 
