ceebb4c561032b11fa356715b9637dde935d4e38
olly <olly@google.com>
2016-09-01 04:01:57 -0700

    Fix incorrect index in DefaultAllocator.trim
    This could cause us to "lose" allocations backed by an
    initial block, meaning they became unavailable for use
    despite still being allocated.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=131931465

--- library/src/main/java/com/google/android/exoplayer2/upstream/DefaultAllocator.java
+++ library/src/main/java/com/google/android/exoplayer2/upstream/DefaultAllocator.java
@@ -134,7 +134,7 @@ public final class DefaultAllocator implements Allocator {
         if (lowAllocation.data == initialAllocationBlock) {
           lowIndex++;
         } else {
-          Allocation highAllocation = availableAllocations[lowIndex];
+          Allocation highAllocation = availableAllocations[highIndex];
           if (highAllocation.data != initialAllocationBlock) {
             highIndex--;
           } else {
