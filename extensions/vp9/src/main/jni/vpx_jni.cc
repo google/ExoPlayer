a5628b49f3677dd312c1c43100571e870b8634b4
benm <benm@google.com>
2016-09-07 11:43:14 -0700

    Fix a warning in libvpxJNI
    v2/extensions/vp9/src/main/jni/vpx_jni.cc:63:31: warning: missing field 'w' initializer [-Wmissing-field-initializers]
      vpx_codec_dec_cfg_t cfg = {0};
                                  ^
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=132461351

--- extensions/vp9/src/main/jni/vpx_jni.cc
+++ extensions/vp9/src/main/jni/vpx_jni.cc
@@ -60,7 +60,7 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
 
 FUNC(jlong, vpxInit) {
   vpx_codec_ctx_t* context = new vpx_codec_ctx_t();
-  vpx_codec_dec_cfg_t cfg = {0};
+  vpx_codec_dec_cfg_t cfg = {0, 0, 0};
   cfg.threads = android_getCpuCount();
   if (vpx_codec_dec_init(context, &vpx_codec_vp9_dx_algo, &cfg, 0)) {
     LOGE("ERROR: Fail to initialize libvpx decoder.");
