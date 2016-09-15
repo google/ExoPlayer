8294121e98b7ab885693345a72d23780effa15cb
andrewlewis <andrewlewis@google.com>
2016-09-15 03:21:00 -0700

    Fix peeking the end of the stream then reading it.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=133240502

--- library/src/main/java/com/google/android/exoplayer/extractor/DefaultExtractorInput.java
+++ library/src/main/java/com/google/android/exoplayer/extractor/DefaultExtractorInput.java
@@ -124,7 +124,6 @@ public final class DefaultExtractorInput implements ExtractorInput {
       throws IOException, InterruptedException {
     ensureSpaceForPeek(length);
     int bytesPeeked = Math.min(peekBufferLength - peekBufferPosition, length);
-    peekBufferLength += length - bytesPeeked;
     while (bytesPeeked < length) {
       bytesPeeked = readFromDataSource(peekBuffer, peekBufferPosition, length, bytesPeeked,
           allowEndOfInput);
@@ -133,6 +132,7 @@ public final class DefaultExtractorInput implements ExtractorInput {
       }
     }
     peekBufferPosition += length;
+    peekBufferLength = Math.max(peekBufferLength, peekBufferPosition);
     return true;
   }
 
