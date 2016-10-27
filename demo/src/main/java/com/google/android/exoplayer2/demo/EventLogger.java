70cc98bb99e81d60ce691f1a4676e148af201347
andrewlewis <andrewlewis@google.com>
2016-10-27 07:29:39 -0700

    Fix broken log call
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=137392736

--- demo/src/main/java/com/google/android/exoplayer2/demo/EventLogger.java
+++ demo/src/main/java/com/google/android/exoplayer2/demo/EventLogger.java
@@ -371,7 +371,7 @@ import java.util.Locale;
       } else if (entry instanceof CommentFrame) {
         CommentFrame commentFrame = (CommentFrame) entry;
         Log.d(TAG, prefix + String.format("%s: language=%s description=%s", commentFrame.id,
-            commentFrame.language, commentFrame.description, commentFrame.text));
+            commentFrame.language, commentFrame.description));
       } else if (entry instanceof Id3Frame) {
         Id3Frame id3Frame = (Id3Frame) entry;
         Log.d(TAG, prefix + String.format("%s", id3Frame.id));
