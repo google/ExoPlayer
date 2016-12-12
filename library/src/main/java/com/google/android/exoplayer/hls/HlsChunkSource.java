9812aa765c34a4b6fbdd34d322a94427a908fd87
aquilescanta <aquilescanta@google.com>
2016-12-12 12:37:09 -0800

    Ignore discontinuities in TSs for HLS
    Issue:#1921
    Issue:#1978
    Issue:#2163
    Issue:#2172
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=141797474

--- library/src/main/java/com/google/android/exoplayer/hls/HlsChunkSource.java
+++ library/src/main/java/com/google/android/exoplayer/hls/HlsChunkSource.java
@@ -513,8 +513,8 @@ public class HlsChunkSource implements HlsTrackSelector.Output {
         // The master source has yet to instantiate an adjuster for the discontinuity sequence.
         return;
       }
-      // This flag ensures the change of pid between streams does not affect the sample queues.
-      int workaroundFlags = TsExtractor.WORKAROUND_MAP_BY_TYPE;
+      // Enable HLS workarounds for the extractor.
+      int workaroundFlags = TsExtractor.WORKAROUND_HLS_MODE;
       String codecs = format.codecs;
       if (!TextUtils.isEmpty(codecs)) {
         // Sometimes AAC and H264 streams are declared in TS chunks even though they don't really
