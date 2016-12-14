fc93940463542b41aa748ba7f74a2978e7022c48
eguven <eguven@google.com>
2016-12-14 02:54:04 -0800

    Fix widevine playback tests
    Passes drmSessionManager to DebugMediaCodecVideoRenderer.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=141997225

--- playbacktests/src/main/java/com/google/android/exoplayer2/playbacktests/util/DebugSimpleExoPlayer.java
+++ playbacktests/src/main/java/com/google/android/exoplayer2/playbacktests/util/DebugSimpleExoPlayer.java
@@ -49,7 +49,7 @@ public class DebugSimpleExoPlayer extends SimpleExoPlayer {
       @ExtensionRendererMode int extensionRendererMode, VideoRendererEventListener eventListener,
       long allowedVideoJoiningTimeMs, ArrayList<Renderer> out) {
     out.add(new DebugMediaCodecVideoRenderer(context, MediaCodecSelector.DEFAULT,
-        allowedVideoJoiningTimeMs, mainHandler, eventListener,
+        allowedVideoJoiningTimeMs, mainHandler, drmSessionManager, eventListener,
         MAX_DROPPED_VIDEO_FRAME_COUNT_TO_NOTIFY));
   }
 
@@ -68,10 +68,12 @@ public class DebugSimpleExoPlayer extends SimpleExoPlayer {
     private int bufferCount;
 
     public DebugMediaCodecVideoRenderer(Context context, MediaCodecSelector mediaCodecSelector,
-        long allowedJoiningTimeMs, Handler eventHandler, VideoRendererEventListener eventListener,
+        long allowedJoiningTimeMs, Handler eventHandler,
+        DrmSessionManager<FrameworkMediaCrypto> drmSessionManager,
+        VideoRendererEventListener eventListener,
         int maxDroppedFrameCountToNotify) {
-      super(context, mediaCodecSelector, allowedJoiningTimeMs, null, false, eventHandler,
-          eventListener, maxDroppedFrameCountToNotify);
+      super(context, mediaCodecSelector, allowedJoiningTimeMs, drmSessionManager, false,
+          eventHandler, eventListener, maxDroppedFrameCountToNotify);
       startIndex = 0;
       queueSize = 0;
     }
