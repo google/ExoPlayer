18a24a1fde68753737030bac9c7d56e6e7c292e4
cdrolle <cdrolle@google.com>
2017-01-17 10:35:18 -0800

    Added the CEA-708 support to the open-source project.
    Issue: #1807
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=144726542

--- library/src/main/java/com/google/android/exoplayer2/text/SubtitleDecoderFactory.java
+++ library/src/main/java/com/google/android/exoplayer2/text/SubtitleDecoderFactory.java
@@ -17,6 +17,7 @@ package com.google.android.exoplayer2.text;
 
 import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.text.cea.Cea608Decoder;
+import com.google.android.exoplayer2.text.cea.Cea708Decoder;
 import com.google.android.exoplayer2.text.subrip.SubripDecoder;
 import com.google.android.exoplayer2.text.ttml.TtmlDecoder;
 import com.google.android.exoplayer2.text.tx3g.Tx3gDecoder;
@@ -58,6 +59,7 @@ public interface SubtitleDecoderFactory {
    * <li>SubRip ({@link SubripDecoder})</li>
    * <li>TX3G ({@link Tx3gDecoder})</li>
    * <li>Cea608 ({@link Cea608Decoder})</li>
+   * <li>Cea708 ({@link Cea708Decoder})</li>
    * </ul>
    */
   SubtitleDecoderFactory DEFAULT = new SubtitleDecoderFactory() {
@@ -78,6 +80,9 @@ public interface SubtitleDecoderFactory {
             || format.sampleMimeType.equals(MimeTypes.APPLICATION_MP4CEA608)) {
           return clazz.asSubclass(SubtitleDecoder.class).getConstructor(String.class, Integer.TYPE)
               .newInstance(format.sampleMimeType, format.accessibilityChannel);
+        } else if (format.sampleMimeType.equals(MimeTypes.APPLICATION_CEA708)) {
+          return clazz.asSubclass(SubtitleDecoder.class).getConstructor(Integer.TYPE)
+              .newInstance(format.accessibilityChannel);
         } else {
           return clazz.asSubclass(SubtitleDecoder.class).getConstructor().newInstance();
         }
@@ -105,6 +110,8 @@ public interface SubtitleDecoderFactory {
           case MimeTypes.APPLICATION_CEA608:
           case MimeTypes.APPLICATION_MP4CEA608:
             return Class.forName("com.google.android.exoplayer2.text.cea.Cea608Decoder");
+          case MimeTypes.APPLICATION_CEA708:
+            return Class.forName("com.google.android.exoplayer2.text.cea.Cea708Decoder");
           default:
             return null;
         }
