b6a1d0a55dc036d20aa67822b44e2955d73930ce
vigneshv <vigneshv@google.com>
2017-03-21 01:39:52 -0700

    extensions/vp9: Update filter & remove sort in libvpx.mk
    Update the filter that selects %.asm.[sS] and remove the
    "remove duplicates" step in the android makefile.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=150728021

--- extensions/vp9/src/main/jni/libvpx.mk
+++ extensions/vp9/src/main/jni/libvpx.mk
@@ -37,9 +37,7 @@ LOCAL_SRC_FILES += $(addprefix libvpx/, $(filter-out vpx_config.c, \
 # include assembly files if they exist
 # "%.asm.[sS]" covers neon assembly and "%.asm" covers x86 assembly
 LOCAL_SRC_FILES += $(addprefix libvpx/, \
-                     $(filter %.asm.s %.asm, $(libvpx_codec_srcs)))
-LOCAL_SRC_FILES += $(addprefix libvpx/, \
-                     $(filter %.asm.S %.asm, $(libvpx_codec_srcs)))
+                     $(filter %.asm.s %.asm.S %.asm, $(libvpx_codec_srcs)))
 
 ifneq ($(findstring armeabi-v7a, $(TARGET_ARCH_ABI)),)
 # append .neon to *_neon.c and *.[sS]
@@ -47,9 +45,6 @@ LOCAL_SRC_FILES := $(subst _neon.c,_neon.c.neon,$(LOCAL_SRC_FILES))
 LOCAL_SRC_FILES := $(subst .s,.s.neon,$(LOCAL_SRC_FILES))
 LOCAL_SRC_FILES := $(subst .S,.S.neon,$(LOCAL_SRC_FILES))
 endif
-
-# remove duplicates
-LOCAL_SRC_FILES := $(sort $(LOCAL_SRC_FILES))
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libvpx \
                            $(LOCAL_PATH)/libvpx/vpx
