c9fd9b87182027b51b0e835fe35be095722b5102
tonihei <tonihei@google.com>
2017-04-11 03:35:33 -0700

    Support 'styl' in Tx3g decoder.
    Extended Tx3gDecoder to read additional information after subtitle text.
    Currently parses font face, font size, and foreground colour.
    
    Font identifier and other information provided in subtitle sample description
    not yet supported.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=152793774

--- library/core/src/main/java/com/google/android/exoplayer2/text/tx3g/Tx3gDecoder.java
+++ library/core/src/main/java/com/google/android/exoplayer2/text/tx3g/Tx3gDecoder.java
@@ -15,10 +15,21 @@
  */
 package com.google.android.exoplayer2.text.tx3g;
 
+import android.graphics.Typeface;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.text.Cue;
 import com.google.android.exoplayer2.text.SimpleSubtitleDecoder;
 import com.google.android.exoplayer2.text.Subtitle;
+import com.google.android.exoplayer2.text.SubtitleDecoderException;
+import com.google.android.exoplayer2.util.Assertions;
 import com.google.android.exoplayer2.util.ParsableByteArray;
+import com.google.android.exoplayer2.util.Util;
+import java.nio.charset.Charset;
 
 /**
  * A {@link SimpleSubtitleDecoder} for tx3g.
@@ -26,6 +37,20 @@ import com.google.android.exoplayer2.util.ParsableByteArray;
  * Currently only supports parsing of a single text track.
  */
 public final class Tx3gDecoder extends SimpleSubtitleDecoder {
+
+  private static final char BOM_UTF16_BE = '\uFEFF';
+  private static final char BOM_UTF16_LE = '\uFFFE';
+
+  private static final int TYPE_STYL = Util.getIntegerCodeForString("styl");
+
+  private static final int SIZE_ATOM_HEADER = 8;
+  private static final int SIZE_SHORT = 2;
+  private static final int SIZE_BOM_UTF16 = 2;
+  private static final int SIZE_STYLE_RECORD = 12;
+
+  private static final int FONT_FACE_BOLD = 0x0001;
+  private static final int FONT_FACE_ITALIC = 0x0002;
+  private static final int FONT_FACE_UNDERLINE = 0x0004;
 
   private final ParsableByteArray parsableByteArray;
 
@@ -35,14 +60,90 @@ public final class Tx3gDecoder extends SimpleSubtitleDecoder {
   }
 
   @Override
-  protected Subtitle decode(byte[] bytes, int length, boolean reset) {
-    parsableByteArray.reset(bytes, length);
-    int textLength = parsableByteArray.readUnsignedShort();
-    if (textLength == 0) {
-      return Tx3gSubtitle.EMPTY;
+  protected Subtitle decode(byte[] bytes, int length, boolean reset)
+      throws SubtitleDecoderException {
+    try {
+      parsableByteArray.reset(bytes, length);
+      String cueTextString = readSubtitleText(parsableByteArray);
+      if (cueTextString.isEmpty()) {
+        return Tx3gSubtitle.EMPTY;
+      }
+      SpannableStringBuilder cueText = new SpannableStringBuilder(cueTextString);
+      while (parsableByteArray.bytesLeft() >= SIZE_ATOM_HEADER) {
+        int atomSize = parsableByteArray.readInt();
+        int atomType = parsableByteArray.readInt();
+        if (atomType == TYPE_STYL) {
+          Assertions.checkArgument(parsableByteArray.bytesLeft() >= SIZE_SHORT);
+          int styleRecordCount = parsableByteArray.readUnsignedShort();
+          for (int i = 0; i < styleRecordCount; i++) {
+            applyStyleRecord(parsableByteArray, cueText);
+          }
+        } else {
+          parsableByteArray.skipBytes(atomSize - SIZE_ATOM_HEADER);
+        }
+      }
+      return new Tx3gSubtitle(new Cue(cueText));
+    } catch (IllegalArgumentException e) {
+      throw new SubtitleDecoderException("Unexpected subtitle format.", e);
     }
-    String cueText = parsableByteArray.readString(textLength);
-    return new Tx3gSubtitle(new Cue(cueText));
   }
 
+  private static String readSubtitleText(ParsableByteArray parsableByteArray) {
+    Assertions.checkArgument(parsableByteArray.bytesLeft() >= SIZE_SHORT);
+    int textLength = parsableByteArray.readUnsignedShort();
+    if (textLength == 0) {
+      return "";
+    }
+    if (parsableByteArray.bytesLeft() >= SIZE_BOM_UTF16) {
+      char firstChar = parsableByteArray.peekChar();
+      if (firstChar == BOM_UTF16_BE || firstChar == BOM_UTF16_LE) {
+        return parsableByteArray.readString(textLength, Charset.forName(C.UTF16_NAME));
+      }
+    }
+    return parsableByteArray.readString(textLength, Charset.forName(C.UTF8_NAME));
+  }
+
+  private static void applyStyleRecord(ParsableByteArray parsableByteArray,
+      SpannableStringBuilder cueText) {
+    Assertions.checkArgument(parsableByteArray.bytesLeft() >= SIZE_STYLE_RECORD);
+    int start = parsableByteArray.readUnsignedShort();
+    int end = parsableByteArray.readUnsignedShort();
+    parsableByteArray.skipBytes(2); // font identifier
+    int fontFace = parsableByteArray.readUnsignedByte();
+    parsableByteArray.skipBytes(1); // font size
+    int colorRgba = parsableByteArray.readInt();
+
+    if (fontFace != 0) {
+      attachFontFace(cueText, fontFace, start, end);
+    }
+    attachColor(cueText, colorRgba, start, end);
+  }
+
+  private static void attachFontFace(SpannableStringBuilder cueText, int fontFace, int start,
+      int end) {
+    boolean isBold = (fontFace & FONT_FACE_BOLD) != 0;
+    boolean isItalic = (fontFace & FONT_FACE_ITALIC) != 0;
+    if (isBold) {
+      if (isItalic) {
+        cueText.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), start, end,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+      } else {
+        cueText.setSpan(new StyleSpan(Typeface.BOLD), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+      }
+    } else if (isItalic) {
+      cueText.setSpan(new StyleSpan(Typeface.ITALIC), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+
+    boolean isUnderlined = (fontFace & FONT_FACE_UNDERLINE) != 0;
+    if (isUnderlined) {
+      cueText.setSpan(new UnderlineSpan(), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+  }
+
+  private static void attachColor(SpannableStringBuilder cueText, int colorRgba, int start,
+      int end) {
+    int colorArgb = ((colorRgba & 0xFF) << 24) | (colorRgba >>> 8);
+    cueText.setSpan(new ForegroundColorSpan(colorArgb), start, end,
+        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+  }
 }
