161bc10f42c5d540e0d8e03b26fd7607f8f3efcb
aquilescanta <aquilescanta@google.com>
2017-05-26 07:48:43 -0700

    Use AVERAGE-BANDWIDTH instead of BANDWIDTH when available
    Also prevent BANDWIDTH's regex from matching the AVERAGE-BANDWIDTH attribute.
    
    Issue:#2863
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=157219453

--- library/hls/src/main/java/com/google/android/exoplayer2/source/hls/playlist/HlsPlaylistParser.java
+++ library/hls/src/main/java/com/google/android/exoplayer2/source/hls/playlist/HlsPlaylistParser.java
@@ -75,7 +75,9 @@ public final class HlsPlaylistParser implements ParsingLoadable.Parser<HlsPlayli
 
   private static final String ATTR_CLOSED_CAPTIONS_NONE = "CLOSED-CAPTIONS=NONE";
 
-  private static final Pattern REGEX_BANDWIDTH = Pattern.compile("BANDWIDTH=(\\d+)\\b");
+  private static final Pattern REGEX_AVERAGE_BANDWIDTH =
+      Pattern.compile("AVERAGE-BANDWIDTH=(\\d+)\\b");
+  private static final Pattern REGEX_BANDWIDTH = Pattern.compile("[^-]BANDWIDTH=(\\d+)\\b");
   private static final Pattern REGEX_CODECS = Pattern.compile("CODECS=\"(.+?)\"");
   private static final Pattern REGEX_RESOLUTION = Pattern.compile("RESOLUTION=(\\d+x\\d+)");
   private static final Pattern REGEX_TARGET_DURATION = Pattern.compile(TAG_TARGET_DURATION
@@ -235,6 +237,11 @@ public final class HlsPlaylistParser implements ParsingLoadable.Parser<HlsPlayli
         }
       } else if (line.startsWith(TAG_STREAM_INF)) {
         int bitrate = parseIntAttr(line, REGEX_BANDWIDTH);
+        String averageBandwidthString = parseOptionalStringAttr(line, REGEX_AVERAGE_BANDWIDTH);
+        if (averageBandwidthString != null) {
+          // If available, the average bandwidth attribute is used as the variant's bitrate.
+          bitrate = Integer.parseInt(averageBandwidthString);
+        }
         String codecs = parseOptionalStringAttr(line, REGEX_CODECS);
         String resolutionString = parseOptionalStringAttr(line, REGEX_RESOLUTION);
         noClosedCaptions |= line.contains(ATTR_CLOSED_CAPTIONS_NONE);
@@ -406,14 +413,6 @@ public final class HlsPlaylistParser implements ParsingLoadable.Parser<HlsPlayli
         initializationSegment, segments);
   }
 
-  private static String parseStringAttr(String line, Pattern pattern) throws ParserException {
-    Matcher matcher = pattern.matcher(line);
-    if (matcher.find() && matcher.groupCount() == 1) {
-      return matcher.group(1);
-    }
-    throw new ParserException("Couldn't match " + pattern.pattern() + " in " + line);
-  }
-
   private static int parseIntAttr(String line, Pattern pattern) throws ParserException {
     return Integer.parseInt(parseStringAttr(line, pattern));
   }
@@ -424,10 +423,15 @@ public final class HlsPlaylistParser implements ParsingLoadable.Parser<HlsPlayli
 
   private static String parseOptionalStringAttr(String line, Pattern pattern) {
     Matcher matcher = pattern.matcher(line);
-    if (matcher.find()) {
+    return matcher.find() ? matcher.group(1) : null;
+  }
+
+  private static String parseStringAttr(String line, Pattern pattern) throws ParserException {
+    Matcher matcher = pattern.matcher(line);
+    if (matcher.find() && matcher.groupCount() == 1) {
       return matcher.group(1);
     }
-    return null;
+    throw new ParserException("Couldn't match " + pattern.pattern() + " in " + line);
   }
 
   private static boolean parseBooleanAttribute(String line, Pattern pattern, boolean defaultValue) {
