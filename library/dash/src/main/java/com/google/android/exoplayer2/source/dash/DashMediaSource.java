ac96317c72cf71b507d10568f21a001a3558b602
olly <olly@google.com>
2017-06-07 08:01:28 -0700

    Update handled schemes for timing element resolution.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=158269487

--- library/dash/src/main/java/com/google/android/exoplayer2/source/dash/DashMediaSource.java
+++ library/dash/src/main/java/com/google/android/exoplayer2/source/dash/DashMediaSource.java
@@ -410,12 +410,14 @@ public final class DashMediaSource implements MediaSource {
 
   private void resolveUtcTimingElement(UtcTimingElement timingElement) {
     String scheme = timingElement.schemeIdUri;
-    if (Util.areEqual(scheme, "urn:mpeg:dash:utc:direct:2012")) {
+    if (Util.areEqual(scheme, "urn:mpeg:dash:utc:direct:2014")
+        || Util.areEqual(scheme, "urn:mpeg:dash:utc:direct:2012")) {
       resolveUtcTimingElementDirect(timingElement);
-    } else if (Util.areEqual(scheme, "urn:mpeg:dash:utc:http-iso:2014")) {
+    } else if (Util.areEqual(scheme, "urn:mpeg:dash:utc:http-iso:2014")
+        || Util.areEqual(scheme, "urn:mpeg:dash:utc:http-iso:2012")) {
       resolveUtcTimingElementHttp(timingElement, new Iso8601Parser());
-    } else if (Util.areEqual(scheme, "urn:mpeg:dash:utc:http-xsdate:2012")
-        || Util.areEqual(scheme, "urn:mpeg:dash:utc:http-xsdate:2014")) {
+    } else if (Util.areEqual(scheme, "urn:mpeg:dash:utc:http-xsdate:2014")
+        || Util.areEqual(scheme, "urn:mpeg:dash:utc:http-xsdate:2012")) {
       resolveUtcTimingElementHttp(timingElement, new XsDateTimeParser());
     } else {
       // Unsupported scheme.
