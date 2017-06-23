a7100b055a377d808aa42dc92e96a7393ac18577
Oliver Woodman <olly@google.com>
2017-06-23 17:20:51 +0100

    Fix ContentDataSource and enhance tests to validate read data

--- library/core/src/androidTest/java/com/google/android/exoplayer2/upstream/AssetDataSourceTest.java
+++ library/core/src/androidTest/java/com/google/android/exoplayer2/upstream/AssetDataSourceTest.java
@@ -1,21 +1,62 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.android.exoplayer2.upstream;
 
 import android.content.Context;
 import android.net.Uri;
 import android.test.InstrumentationTestCase;
+import android.test.MoreAsserts;
+import com.google.android.exoplayer2.testutil.TestUtil;
 
-import static com.google.android.exoplayer2.upstream.AndroidDataSourceConstants.SAMPLE_MP4_BYTES;
-import static com.google.android.exoplayer2.upstream.AndroidDataSourceConstants.SAMPLE_MP4_PATH;
+/**
+ * Unit tests for {@link AssetDataSource}.
+ */
+public final class AssetDataSourceTest extends InstrumentationTestCase {
 
-public class AssetDataSourceTest extends InstrumentationTestCase {
+  private static final String DATA_PATH = "binary/1024_incrementing_bytes.mp3";
+  private static final long DATA_LENGTH = 1024;
 
-  public void testAssetDataSource() throws Exception {
-    final Context context = getInstrumentation().getContext();
+  public void testReadFileUri() throws Exception {
+    Context context = getInstrumentation().getContext();
     AssetDataSource dataSource = new AssetDataSource(context);
-    Uri assetUri = Uri.parse("file:///android_asset" + SAMPLE_MP4_PATH);
+    Uri assetUri = Uri.parse("file:///android_asset/" + DATA_PATH);
     DataSpec dataSpec = new DataSpec(assetUri);
-    long sourceLengthBytes = dataSource.open(dataSpec);
-
-    assertEquals(SAMPLE_MP4_BYTES, sourceLengthBytes);
+    try {
+      long length = dataSource.open(dataSpec);
+      assertEquals(DATA_LENGTH, length);
+      byte[] readData = TestUtil.readToEnd(dataSource);
+      MoreAsserts.assertEquals(TestUtil.getByteArray(getInstrumentation(), DATA_PATH), readData);
+    } finally {
+      dataSource.close();
+    }
   }
+
+  public void testReadAssetUri() throws Exception {
+    Context context = getInstrumentation().getContext();
+    AssetDataSource dataSource = new AssetDataSource(context);
+    Uri assetUri = Uri.parse("asset:///" + DATA_PATH);
+    DataSpec dataSpec = new DataSpec(assetUri);
+    try {
+      long length = dataSource.open(dataSpec);
+      assertEquals(DATA_LENGTH, length);
+      byte[] readData = TestUtil.readToEnd(dataSource);
+      MoreAsserts.assertEquals(TestUtil.getByteArray(getInstrumentation(), DATA_PATH), readData);
+    } finally {
+      dataSource.close();
+    }
+  }
+
 }
