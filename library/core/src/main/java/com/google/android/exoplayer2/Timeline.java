52fd8be3465a80430c6a0a9713cd619bac54e093
andrewlewis <andrewlewis@google.com>
2017-06-27 07:46:33 -0700

    Move getPeriodPosition methods to Timeline
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=160273929

--- library/core/src/main/java/com/google/android/exoplayer2/Timeline.java
+++ library/core/src/main/java/com/google/android/exoplayer2/Timeline.java
@@ -15,6 +15,9 @@
  */
 package com.google.android.exoplayer2;
 
+import android.util.Pair;
+import com.google.android.exoplayer2.util.Assertions;
+
 /**
  * A representation of media currently available for playback.
  * <p>
@@ -519,6 +522,50 @@ public abstract class Timeline {
    */
   public final Period getPeriod(int periodIndex, Period period) {
     return getPeriod(periodIndex, period, false);
+  }
+
+  /**
+   * Calls {@link #getPeriodPosition(Window, Period, int, long, long)} with a zero default position
+   * projection.
+   */
+  public final Pair<Integer, Long> getPeriodPosition(Window window, Period period, int windowIndex,
+      long windowPositionUs) {
+    return getPeriodPosition(window, period, windowIndex, windowPositionUs, 0);
+  }
+
+  /**
+   * Converts (windowIndex, windowPositionUs) to the corresponding (periodIndex, periodPositionUs).
+   *
+   * @param window A {@link Window} that may be overwritten.
+   * @param period A {@link Period} that may be overwritten.
+   * @param windowIndex The window index.
+   * @param windowPositionUs The window time, or {@link C#TIME_UNSET} to use the window's default
+   *     start position.
+   * @param defaultPositionProjectionUs If {@code windowPositionUs} is {@link C#TIME_UNSET}, the
+   *     duration into the future by which the window's position should be projected.
+   * @return The corresponding (periodIndex, periodPositionUs), or null if {@code #windowPositionUs}
+   *     is {@link C#TIME_UNSET}, {@code defaultPositionProjectionUs} is non-zero, and the window's
+   *     position could not be projected by {@code defaultPositionProjectionUs}.
+   */
+  public final Pair<Integer, Long> getPeriodPosition(Window window, Period period, int windowIndex,
+      long windowPositionUs, long defaultPositionProjectionUs) {
+    Assertions.checkIndex(windowIndex, 0, getWindowCount());
+    getWindow(windowIndex, window, false, defaultPositionProjectionUs);
+    if (windowPositionUs == C.TIME_UNSET) {
+      windowPositionUs = window.getDefaultPositionUs();
+      if (windowPositionUs == C.TIME_UNSET) {
+        return null;
+      }
+    }
+    int periodIndex = window.firstPeriodIndex;
+    long periodPositionUs = window.getPositionInFirstPeriodUs() + windowPositionUs;
+    long periodDurationUs = getPeriod(periodIndex, period).getDurationUs();
+    while (periodDurationUs != C.TIME_UNSET && periodPositionUs >= periodDurationUs
+        && periodIndex < window.lastPeriodIndex) {
+      periodPositionUs -= periodDurationUs;
+      periodDurationUs = getPeriod(++periodIndex, period).getDurationUs();
+    }
+    return Pair.create(periodIndex, periodPositionUs);
   }
 
   /**
