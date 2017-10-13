cad88512f5c1aae39d90a8f75cf6e2bbb8faf983
olly <olly@google.com>
2017-10-13 11:38:47 -0700

    Only parse common-encryption sinf boxes
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=172124807

--- library/core/src/main/java/com/google/android/exoplayer2/extractor/mp4/AtomParsers.java
+++ library/core/src/main/java/com/google/android/exoplayer2/extractor/mp4/AtomParsers.java
@@ -1064,8 +1064,8 @@ import java.util.List;
       Assertions.checkArgument(childAtomSize > 0, "childAtomSize should be positive");
       int childAtomType = parent.readInt();
       if (childAtomType == Atom.TYPE_sinf) {
-        Pair<Integer, TrackEncryptionBox> result = parseSinfFromParent(parent, childPosition,
-            childAtomSize);
+        Pair<Integer, TrackEncryptionBox> result = parseCommonEncryptionSinfFromParent(parent,
+            childPosition, childAtomSize);
         if (result != null) {
           return result;
         }
@@ -1075,8 +1075,8 @@ import java.util.List;
     return null;
   }
 
-  private static Pair<Integer, TrackEncryptionBox> parseSinfFromParent(ParsableByteArray parent,
-      int position, int size) {
+  /* package */ static Pair<Integer, TrackEncryptionBox> parseCommonEncryptionSinfFromParent(
+      ParsableByteArray parent, int position, int size) {
     int childPosition = position + Atom.HEADER_SIZE;
     int schemeInformationBoxPosition = C.POSITION_UNSET;
     int schemeInformationBoxSize = 0;
@@ -1090,7 +1090,7 @@ import java.util.List;
         dataFormat = parent.readInt();
       } else if (childAtomType == Atom.TYPE_schm) {
         parent.skipBytes(4);
-        // scheme_type field. Defined in ISO/IEC 23001-7:2016, section 4.1.
+        // Common encryption scheme_type values are defined in ISO/IEC 23001-7:2016, section 4.1.
         schemeType = parent.readString(4);
       } else if (childAtomType == Atom.TYPE_schi) {
         schemeInformationBoxPosition = childPosition;
@@ -1099,7 +1099,8 @@ import java.util.List;
       childPosition += childAtomSize;
     }
 
-    if (schemeType != null) {
+    if (C.CENC_TYPE_cenc.equals(schemeType) || C.CENC_TYPE_cbc1.equals(schemeType)
+        || C.CENC_TYPE_cens.equals(schemeType) || C.CENC_TYPE_cbcs.equals(schemeType)) {
       Assertions.checkArgument(dataFormat != null, "frma atom is mandatory");
       Assertions.checkArgument(schemeInformationBoxPosition != C.POSITION_UNSET,
           "schi atom is mandatory");
