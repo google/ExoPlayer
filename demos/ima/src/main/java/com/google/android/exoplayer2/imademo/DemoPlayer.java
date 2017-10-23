b9854e769b25ddf5e121b38d10a9d9ae7114dd25
olly <olly@google.com>
2017-10-23 11:41:11 -0700

    Clean up IMA demo
    Also do some minor cleanup in other demo apps.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=173146425

--- demos/ima/src/main/java/com/google/android/exoplayer2/imademo/DemoPlayer.java
+++ demos/ima/src/main/java/com/google/android/exoplayer2/imademo/DemoPlayer.java
@@ -17,14 +17,15 @@ package com.google.android.exoplayer2.imademo;
 
 import android.content.Context;
 import android.net.Uri;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.ext.ima.ImaAdsLoader;
-import com.google.android.exoplayer2.ext.ima.ImaAdsMediaSource;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ads.AdsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
@@ -37,61 +38,69 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
 /**
- * This class deals with ExoPlayer, the IMA plugin, and all video playback.
+ * Manages the {@link ExoPlayer}, the IMA plugin and all video playback.
  */
-class DemoPlayer {
+/* package */ final class DemoPlayer {
 
-    private final ImaAdsLoader mAdsLoader;
-    private SimpleExoPlayer mPlayer;
-    private long mContentPosition;
+  private final ImaAdsLoader adsLoader;
 
-    DemoPlayer(Context context) {
-        String adTag = context.getString(R.string.ad_tag_url);
-        mAdsLoader = new ImaAdsLoader(context, Uri.parse(adTag));
+  private SimpleExoPlayer player;
+  private long contentPosition;
+
+  public DemoPlayer(Context context) {
+    String adTag = context.getString(R.string.ad_tag_url);
+    adsLoader = new ImaAdsLoader(context, Uri.parse(adTag));
+  }
+
+  public void init(Context context, SimpleExoPlayerView simpleExoPlayerView) {
+    // Create a default track selector.
+    BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+    TrackSelection.Factory videoTrackSelectionFactory =
+        new AdaptiveTrackSelection.Factory(bandwidthMeter);
+    TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+    // Create a player instance.
+    player = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+
+    // Bind the player to the view.
+    simpleExoPlayerView.setPlayer(player);
+
+    // Produces DataSource instances through which media data is loaded.
+    DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context,
+        Util.getUserAgent(context, context.getString(R.string.application_name)));
+
+    // Produces Extractor instances for parsing the content media (i.e. not the ad).
+    ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+    // This is the MediaSource representing the content media (i.e. not the ad).
+    String contentUrl = context.getString(R.string.content_url);
+    MediaSource contentMediaSource = new ExtractorMediaSource(
+        Uri.parse(contentUrl), dataSourceFactory, extractorsFactory, null, null);
+
+    // Compose the content media source into a new AdsMediaSource with both ads and content.
+    MediaSource mediaSourceWithAds = new AdsMediaSource(contentMediaSource, dataSourceFactory,
+        adsLoader, simpleExoPlayerView.getOverlayFrameLayout());
+
+    // Prepare the player with the source.
+    player.seekTo(contentPosition);
+    player.prepare(mediaSourceWithAds);
+    player.setPlayWhenReady(true);
+  }
+
+  public void reset() {
+    if (player != null) {
+      contentPosition = player.getContentPosition();
+      player.release();
+      player = null;
     }
+  }
 
-    void init(Context context, SimpleExoPlayerView simpleExoPlayerView) {
-        // Create a default track selector.
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoTrackSelectionFactory =
-                new AdaptiveTrackSelection.Factory(bandwidthMeter);
-        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-
-        // Create a simple ExoPlayer instance.
-        mPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
-
-        // Bind the player to the view.
-        simpleExoPlayerView.setPlayer(mPlayer);
-
-        // Produces DataSource instances through which media data is loaded.
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context,
-                Util.getUserAgent(context, context.getString(R.string.app_name)));
-        // Produces Extractor instances for parsing the media data.
-        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-        // This is the MediaSource representing the non-ad, content media to be played.
-        String contentUrl = context.getString(R.string.content_url);
-        MediaSource contentMediaSource = new ExtractorMediaSource(
-                Uri.parse(contentUrl), dataSourceFactory, extractorsFactory, null, null);
-        // Compose the content media source into a new ImaAdMediaSource with both ads and content.
-        MediaSource mediaSourceWithAds = new ImaAdsMediaSource(
-                contentMediaSource,
-                dataSourceFactory,
-                mAdsLoader,
-                simpleExoPlayerView.getOverlayFrameLayout());
-        // Prepare the player with the source.
-        mPlayer.seekTo(mContentPosition);
-        mPlayer.prepare(mediaSourceWithAds);
-        mPlayer.setPlayWhenReady(true);
+  public void release() {
+    if (player != null) {
+      player.release();
+      player = null;
     }
+    adsLoader.release();
+  }
 
-    void reset() {
-        if (mPlayer != null) {
-            mContentPosition = mPlayer.getContentPosition();
-            mPlayer.release();
-        }
-    }
-
-    void release() {
-        mAdsLoader.release();
-    }
 }
