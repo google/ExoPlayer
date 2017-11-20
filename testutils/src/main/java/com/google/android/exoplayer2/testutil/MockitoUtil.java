42dfbb9fcd144176353170526aa8ba38501c5071
tonihei <tonihei@google.com>
2017-11-20 04:20:52 -0800

    Move MockitoUtils to testutils and use it for all Mockito set-ups.
    In particular this allows to have the workaround for
    https://code.google.com/p/dexmaker/issues/detail?id=2 in one place only.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=176340526

--- testutils/src/main/java/com/google/android/exoplayer2/testutil/MockitoUtil.java
+++ testutils/src/main/java/com/google/android/exoplayer2/testutil/MockitoUtil.java
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.android.exoplayer2.source.dash;
+package com.google.android.exoplayer2.testutil;
 
+import android.content.Context;
 import android.test.InstrumentationTestCase;
 import org.mockito.MockitoAnnotations;
 
@@ -25,12 +26,27 @@ public final class MockitoUtil {
 
   /**
    * Sets up Mockito for an instrumentation test.
+   *
+   * @param instrumentationTestCase The instrumentation test case class.
    */
   public static void setUpMockito(InstrumentationTestCase instrumentationTestCase) {
     // Workaround for https://code.google.com/p/dexmaker/issues/detail?id=2.
     System.setProperty("dexmaker.dexcache",
         instrumentationTestCase.getInstrumentation().getTargetContext().getCacheDir().getPath());
     MockitoAnnotations.initMocks(instrumentationTestCase);
+  }
+
+  /**
+   * Sets up Mockito for a JUnit4 test.
+   *
+   * @param targetContext The target context. Usually obtained from
+   *     {@code InstrumentationRegistry.getTargetContext()}
+   * @param testClass The JUnit4 test class.
+   */
+  public static void setUpMockito(Context targetContext, Object testClass) {
+    // Workaround for https://code.google.com/p/dexmaker/issues/detail?id=2.
+    System.setProperty("dexmaker.dexcache", targetContext.getCacheDir().getPath());
+    MockitoAnnotations.initMocks(testClass);
   }
 
   private MockitoUtil() {}
