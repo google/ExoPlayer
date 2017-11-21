b688a562508e74721841aaaa770e9dc47bd378dd
olly <olly@google.com>
2017-11-21 04:46:52 -0800

    Make ExtractorMediaSource timeline dynamic until duration is set
    We (eventually - albeit possibly infinitely far in the future)
    expect a timeline update with a window of known duration. This
    also stops live radio stream playbacks transitioning to ended
    state when their tracks are disabled.
    
    As part of this fix, I found an issue where getPeriodPosition
    could return null even when defaultPositionProjectionUs is 0,
    which is not as documented.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=176492024

--- library/core/src/main/java/com/google/android/exoplayer2/source/SingleSampleMediaSource.java
+++ library/core/src/main/java/com/google/android/exoplayer2/source/SingleSampleMediaSource.java
@@ -110,7 +110,7 @@ public final class SingleSampleMediaSource implements MediaSource {
     this.eventListener = eventListener;
     this.eventSourceId = eventSourceId;
     this.treatLoadErrorsAsEndOfStream = treatLoadErrorsAsEndOfStream;
-    timeline = new SinglePeriodTimeline(durationUs, true);
+    timeline = new SinglePeriodTimeline(durationUs, true, false);
   }
 
   // MediaSource implementation.
