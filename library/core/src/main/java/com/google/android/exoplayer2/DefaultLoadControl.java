601a87c727d07a148a571a3c1aea8413ed421187
andrewlewis <andrewlewis@google.com>
2017-11-21 08:58:02 -0800

    Remove DefaultLoadControl buffer time state
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=176515168

--- library/core/src/main/java/com/google/android/exoplayer2/DefaultLoadControl.java
+++ library/core/src/main/java/com/google/android/exoplayer2/DefaultLoadControl.java
@@ -51,10 +51,6 @@ public class DefaultLoadControl implements LoadControl {
    */
   public static final int DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS  = 5000;
 
-  private static final int ABOVE_HIGH_WATERMARK = 0;
-  private static final int BETWEEN_WATERMARKS = 1;
-  private static final int BELOW_LOW_WATERMARK = 2;
-
   private final DefaultAllocator allocator;
 
   private final long minBufferUs;
@@ -182,11 +178,11 @@ public class DefaultLoadControl implements LoadControl {
 
   @Override
   public boolean shouldContinueLoading(long bufferedDurationUs, float playbackSpeed) {
-    int bufferTimeState = getBufferTimeState(bufferedDurationUs);
     boolean targetBufferSizeReached = allocator.getTotalBytesAllocated() >= targetBufferSize;
     boolean wasBuffering = isBuffering;
-    isBuffering = bufferTimeState == BELOW_LOW_WATERMARK
-        || (bufferTimeState == BETWEEN_WATERMARKS && isBuffering && !targetBufferSizeReached);
+    isBuffering = bufferedDurationUs < minBufferUs // below low watermark
+        || (bufferedDurationUs <= maxBufferUs // between watermarks
+            && isBuffering && !targetBufferSizeReached);
     if (priorityTaskManager != null && isBuffering != wasBuffering) {
       if (isBuffering) {
         priorityTaskManager.add(C.PRIORITY_PLAYBACK);
@@ -195,11 +191,6 @@ public class DefaultLoadControl implements LoadControl {
       }
     }
     return isBuffering;
-  }
-
-  private int getBufferTimeState(long bufferedDurationUs) {
-    return bufferedDurationUs > maxBufferUs ? ABOVE_HIGH_WATERMARK
-        : (bufferedDurationUs < minBufferUs ? BELOW_LOW_WATERMARK : BETWEEN_WATERMARKS);
   }
 
   private void reset(boolean resetAllocator) {
