0aca6b152dbd5352e4e4c51a54df0cff959de25b
andrewlewis <andrewlewis@google.com>
2017-11-24 10:00:44 -0800

    Switch from currentTimeMillis to elapsedRealtime
    currentTimeMillis is not guaranteed to be monotonic and elapsedRealtime is
    recommend for interval timing.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=176853118

--- library/core/src/main/java/com/google/android/exoplayer2/util/ConditionVariable.java
+++ library/core/src/main/java/com/google/android/exoplayer2/util/ConditionVariable.java
@@ -60,18 +60,18 @@ public final class ConditionVariable {
   }
 
   /**
-   * Blocks until the condition is opened or until timeout milliseconds have passed.
+   * Blocks until the condition is opened or until {@code timeout} milliseconds have passed.
    *
    * @param timeout The maximum time to wait in milliseconds.
-   * @return true If the condition was opened, false if the call returns because of the timeout.
+   * @return True if the condition was opened, false if the call returns because of the timeout.
    * @throws InterruptedException If the thread is interrupted.
    */
   public synchronized boolean block(long timeout) throws InterruptedException {
-    long now = System.currentTimeMillis();
+    long now = android.os.SystemClock.elapsedRealtime();
     long end = now + timeout;
     while (!isOpen && now < end) {
       wait(end - now);
-      now = System.currentTimeMillis();
+      now = android.os.SystemClock.elapsedRealtime();
     }
     return isOpen;
   }
