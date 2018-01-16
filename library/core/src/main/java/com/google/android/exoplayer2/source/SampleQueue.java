a4114f59b6897a28dd10cff2f11311dc04fc9363
olly <olly@google.com>
2018-01-16 07:59:51 -0800

    Seek at chunk level when seeking to chunk start positions
    This avoids issues that can arise due to slight discrepancies between
    chunk start times (obtained from the manifest of segment index) and
    the timestamps of the samples contained within those chunks.
    
    Issue: #2882
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=182054959

--- library/core/src/main/java/com/google/android/exoplayer2/source/SampleQueue.java
+++ library/core/src/main/java/com/google/android/exoplayer2/source/SampleQueue.java
@@ -294,6 +294,18 @@ public final class SampleQueue implements TrackOutput {
   }
 
   /**
+   * Attempts to set the read position to the specified sample index.
+   *
+   * @param sampleIndex The sample index.
+   * @return Whether the read position was set successfully. False is returned if the specified
+   *     index is smaller than the index of the first sample in the queue, or larger than the index
+   *     of the next sample that will be written.
+   */
+  public boolean setReadPosition(int sampleIndex) {
+    return metadataQueue.setReadPosition(sampleIndex);
+  }
+
+  /**
    * Attempts to read from the queue.
    *
    * @param formatHolder A {@link FormatHolder} to populate in the case of reading a format.
