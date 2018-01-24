b4afe6d70acfb6e73b417050f08e51989c11101d
aquilescanta <aquilescanta@google.com>
2018-01-24 06:17:18 -0800

    Implement a best-effort DRM session acquisition approach
    Try to delay failure for as long as possible. That is, propagate
    DRM session failures only after an encrypted buffer arrives or clear
    sample playback without session is not allowed.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=183076348

--- extensions/vp9/src/main/java/com/google/android/exoplayer2/ext/vp9/LibvpxVideoRenderer.java
+++ extensions/vp9/src/main/java/com/google/android/exoplayer2/ext/vp9/LibvpxVideoRenderer.java
@@ -647,10 +647,12 @@ public class LibvpxVideoRenderer extends BaseRenderer {
       if (mediaCrypto == null) {
         DrmSessionException drmError = drmSession.getError();
         if (drmError != null) {
-          throw ExoPlaybackException.createForRenderer(drmError, getIndex());
+          // Continue for now. We may be able to avoid failure if the session recovers, or if a new
+          // input format causes the session to be replaced before it's used.
+        } else {
+          // The drm session isn't open yet.
+          return;
         }
-        // The drm session isn't open yet.
-        return;
       }
     }
 
