c03f326274a9ce8051f9fd918d3d35b996d04e50
andrewlewis <andrewlewis@google.com>
2018-02-02 05:52:30 -0800

    Update internal opus build
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=184274139

--- extensions/opus/src/main/java/com/google/android/exoplayer2/ext/opus/OpusLibrary.java
+++ extensions/opus/src/main/java/com/google/android/exoplayer2/ext/opus/OpusLibrary.java
@@ -27,7 +27,7 @@ public final class OpusLibrary {
     ExoPlayerLibraryInfo.registerModule("goog.exo.opus");
   }
 
-  private static final LibraryLoader LOADER = new LibraryLoader("opus", "opusJNI");
+  private static final LibraryLoader LOADER = new LibraryLoader("opusJNI");
 
   private OpusLibrary() {}
 
