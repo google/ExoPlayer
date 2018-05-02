c466fabb1c267a102b0559a56fccbf4066b1323d
olly <olly@google.com>
2018-05-02 02:25:44 -0700

    Use integers for HLS rendition keys
    Currently it's possible to copy a master playlist with a URL
    that was never present in the original. This change prevents
    this, and also moves the key more in line with DASH/SS.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=195065286

--- library/hls/src/main/java/com/google/android/exoplayer2/source/hls/playlist/HlsMasterPlaylist.java
+++ library/hls/src/main/java/com/google/android/exoplayer2/source/hls/playlist/HlsMasterPlaylist.java
@@ -119,9 +119,9 @@ public final class HlsMasterPlaylist extends HlsPlaylist<HlsMasterPlaylist> {
     return new HlsMasterPlaylist(
         baseUri,
         tags,
-        copyRenditionsList(variants, renditionKeys),
-        copyRenditionsList(audios, renditionKeys),
-        copyRenditionsList(subtitles, renditionKeys),
+        copyRenditionsList(variants, RenditionKey.GROUP_VARIANTS, renditionKeys),
+        copyRenditionsList(audios, RenditionKey.GROUP_AUDIOS, renditionKeys),
+        copyRenditionsList(subtitles, RenditionKey.GROUP_SUBTITLES, renditionKeys),
         muxedAudioFormat,
         muxedCaptionFormats);
   }
@@ -140,13 +140,13 @@ public final class HlsMasterPlaylist extends HlsPlaylist<HlsMasterPlaylist> {
   }
 
   private static List<HlsUrl> copyRenditionsList(
-      List<HlsUrl> renditions, List<RenditionKey> renditionKeys) {
+      List<HlsUrl> renditions, int renditionGroup, List<RenditionKey> renditionKeys) {
     List<HlsUrl> copiedRenditions = new ArrayList<>(renditionKeys.size());
     for (int i = 0; i < renditions.size(); i++) {
       HlsUrl rendition = renditions.get(i);
       for (int j = 0; j < renditionKeys.size(); j++) {
         RenditionKey renditionKey = renditionKeys.get(j);
-        if (renditionKey.url.equals(rendition.url)) {
+        if (renditionKey.renditionGroup == renditionGroup && renditionKey.trackIndex == i) {
           copiedRenditions.add(rendition);
           break;
         }
