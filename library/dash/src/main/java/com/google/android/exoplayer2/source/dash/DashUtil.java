23f4c2a548ce320e1e252da1130cef12724b6c5a
tonihei <tonihei@google.com>
2018-05-03 04:04:07 -0700

    Turn on automatic nullness check for DASH.
    Also add a build rule to simplify the test setup with a custom blacklist.
    In addition, the test path pattern are made less specific to capture all the
    static nullness tests in the /main/ directories.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=195230334

--- library/dash/src/main/java/com/google/android/exoplayer2/source/dash/DashUtil.java
+++ library/dash/src/main/java/com/google/android/exoplayer2/source/dash/DashUtil.java
@@ -179,8 +179,10 @@ public final class DashUtil {
 
   private static ChunkExtractorWrapper newWrappedExtractor(int trackType, Format format) {
     String mimeType = format.containerMimeType;
-    boolean isWebm = mimeType.startsWith(MimeTypes.VIDEO_WEBM)
-        || mimeType.startsWith(MimeTypes.AUDIO_WEBM);
+    boolean isWebm =
+        mimeType != null
+            && (mimeType.startsWith(MimeTypes.VIDEO_WEBM)
+                || mimeType.startsWith(MimeTypes.AUDIO_WEBM));
     Extractor extractor = isWebm ? new MatroskaExtractor() : new FragmentedMp4Extractor();
     return new ChunkExtractorWrapper(extractor, trackType, format);
   }
