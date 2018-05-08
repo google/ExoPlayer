f030769f48721978c3f092ae793bd3b28e32584d
Andrew Lewis <andrewlewis@google.com>
2018-05-08 08:08:00 -0700

    Implement AudioPlayerService

--- audio-app/src/main/java/com/google/android/exoplayer/audiodemo/AudioPlayerService.java
+++ audio-app/src/main/java/com/google/android/exoplayer/audiodemo/AudioPlayerService.java
@@ -15,24 +15,36 @@
  */
 package com.google.android.exoplayer.audiodemo;
 
+import static com.google.android.exoplayer.audiodemo.C.PLAYBACK_CHANNEL_ID;
+import static com.google.android.exoplayer.audiodemo.C.PLAYBACK_NOTIFICATION_ID;
 import static com.google.android.exoplayer.audiodemo.Samples.SAMPLES;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerNotificationManager;
+import com.google.android.exoplayer2.ui.PlayerNotificationManager.BitmapCallback;
+import com.google.android.exoplayer2.ui.PlayerNotificationManager.MediaDescriptionAdapter;
+import com.google.android.exoplayer2.ui.PlayerNotificationManager.NotificationListener;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
 public class AudioPlayerService extends Service {
 
   private SimpleExoPlayer player;
+  private PlayerNotificationManager playerNotificationManager;
 
   @Override
   public void onCreate() {
@@ -42,14 +54,63 @@ public class AudioPlayerService extends Service {
     player = ExoPlayerFactory.newSimpleInstance(context, new DefaultTrackSelector());
     DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(
         context, Util.getUserAgent(context, getString(R.string.application_name)));
-    MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
-        .createMediaSource(SAMPLES[0].uri);
-    player.prepare(mediaSource);
+    ConcatenatingMediaSource concatenatingMediaSource = new ConcatenatingMediaSource();
+    for (Samples.Sample sample : SAMPLES) {
+      MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+          .createMediaSource(sample.uri);
+      concatenatingMediaSource.addMediaSource(mediaSource);
+    }
+    player.prepare(concatenatingMediaSource);
     player.setPlayWhenReady(true);
+
+    playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
+        context,
+        PLAYBACK_CHANNEL_ID,
+        R.string.playback_channel_name,
+        PLAYBACK_NOTIFICATION_ID,
+        new MediaDescriptionAdapter() {
+          @Override
+          public String getCurrentContentTitle(Player player) {
+            return SAMPLES[player.getCurrentWindowIndex()].title;
+          }
+
+          @Nullable
+          @Override
+          public PendingIntent createCurrentContentIntent(Player player) {
+            return null;
+          }
+
+          @Nullable
+          @Override
+          public String getCurrentContentText(Player player) {
+            return SAMPLES[player.getCurrentWindowIndex()].description;
+          }
+
+          @Nullable
+          @Override
+          public Bitmap getCurrentLargeIcon(Player player, BitmapCallback callback) {
+            return Samples.getBitmap(
+                context, SAMPLES[player.getCurrentWindowIndex()].bitmapResource);
+          }
+        }
+    );
+    playerNotificationManager.setNotificationListener(new NotificationListener() {
+      @Override
+      public void onNotificationStarted(int notificationId, Notification notification) {
+        startForeground(notificationId, notification);
+      }
+
+      @Override
+      public void onNotificationCancelled(int notificationId) {
+        stopSelf();
+      }
+    });
+    playerNotificationManager.setPlayer(player);
   }
 
   @Override
   public void onDestroy() {
+    playerNotificationManager.setPlayer(null);
     player.release();
     player = null;
 
