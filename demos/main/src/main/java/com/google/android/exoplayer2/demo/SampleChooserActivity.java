94ffbee7e536d119f21074fe8ef07f65fa30fad7
olly <olly@google.com>
2018-05-07 17:59:52 -0700

    Fix demo app playlist playbacks
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=195752969

--- demos/main/src/main/java/com/google/android/exoplayer2/demo/SampleChooserActivity.java
+++ demos/main/src/main/java/com/google/android/exoplayer2/demo/SampleChooserActivity.java
@@ -548,10 +548,10 @@ public class SampleChooserActivity extends Activity
 
     @Override
     public Intent buildIntent(Context context) {
-      Uri[] uris = new Uri[children.length];
+      String[] uris = new String[children.length];
       String[] extensions = new String[children.length];
       for (int i = 0; i < children.length; i++) {
-        uris[i] = children[i].uri;
+        uris[i] = children[i].uri.toString();
         extensions[i] = children[i].extension;
       }
       return super.buildIntent(context)
