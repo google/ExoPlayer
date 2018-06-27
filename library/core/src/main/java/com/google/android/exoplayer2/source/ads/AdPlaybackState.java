4a8bd911cf98f967ec25605f19f6d92290ff5c1d
Keith Thompson <keithjoethompson@gmail.com>
2018-06-27 16:19:22 +0100

    Add withSkippedAd method to AdPlaybackState. Currently it is only possible to skip entire ad groups but not individual ads within a given ad group.

--- library/core/src/main/java/com/google/android/exoplayer2/source/ads/AdPlaybackState.java
+++ library/core/src/main/java/com/google/android/exoplayer2/source/ads/AdPlaybackState.java
@@ -344,6 +344,14 @@ public final class AdPlaybackState {
     return new AdPlaybackState(adGroupTimesUs, adGroups, adResumePositionUs, contentDurationUs);
   }
 
+  /** Returns an instance with the specified ad marked as skipped. */
+  @CheckResult
+  public AdPlaybackState withSkippedAd(int adGroupIndex, int adIndexInAdGroup) {
+    AdGroup[] adGroups = Arrays.copyOf(this.adGroups, this.adGroups.length);
+    adGroups[adGroupIndex] = adGroups[adGroupIndex].withAdState(AD_STATE_SKIPPED, adIndexInAdGroup);
+    return new AdPlaybackState(adGroupTimesUs, adGroups, adResumePositionUs, contentDurationUs);
+  }
+
   /** Returns an instance with the specified ad marked as having a load error. */
   @CheckResult
   public AdPlaybackState withAdLoadError(int adGroupIndex, int adIndexInAdGroup) {
