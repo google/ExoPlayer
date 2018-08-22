afebd60ee48f7b22775f0541b8f5c34ab5301c47
aquilescanta <aquilescanta@google.com>
2018-08-22 03:39:57 -0700

    Do not seek to the start of live Transport Streams after preparation
    Issue:#4666
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=209742008

--- library/core/src/main/java/com/google/android/exoplayer2/extractor/ts/TsExtractor.java
+++ library/core/src/main/java/com/google/android/exoplayer2/extractor/ts/TsExtractor.java
@@ -243,8 +243,8 @@ public final class TsExtractor implements Extractor {
   @Override
   public @ReadResult int read(ExtractorInput input, PositionHolder seekPosition)
       throws IOException, InterruptedException {
+    long inputLength = input.getLength();
     if (tracksEnded) {
-      long inputLength = input.getLength();
       boolean canReadDuration = inputLength != C.LENGTH_UNSET && mode != MODE_HLS;
       if (canReadDuration && !durationReader.isDurationReadFinished()) {
         return durationReader.readDuration(input, seekPosition, pcrPid);
@@ -324,10 +324,10 @@ public final class TsExtractor implements Extractor {
       payloadReader.consume(tsPacketBuffer, payloadUnitStartIndicator);
       tsPacketBuffer.setLimit(limit);
     }
-    if (mode != MODE_HLS && !wereTracksEnded && tracksEnded) {
-      // We have read all tracks from all PMTs in this stream. Now seek to the beginning and read
-      // again to make sure we output all media, including any contained in packets prior to those
-      // containing the track information.
+    if (mode != MODE_HLS && !wereTracksEnded && tracksEnded && inputLength != C.LENGTH_UNSET) {
+      // We have read all tracks from all PMTs in this non-live stream. Now seek to the beginning
+      // and read again to make sure we output all media, including any contained in packets prior
+      // to those containing the track information.
       pendingSeekToStart = true;
     }
 
