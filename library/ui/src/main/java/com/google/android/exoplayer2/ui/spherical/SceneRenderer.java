5335b258de8b2bec4d6bf84288323fc4d200183b
eguven <eguven@google.com>
2018-08-31 07:44:12 -0700

    Make SceneRenderer implement VideoFrameMetadataListener and CameraMotionListener
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=211084127

--- library/ui/src/main/java/com/google/android/exoplayer2/ui/spherical/SceneRenderer.java
+++ library/ui/src/main/java/com/google/android/exoplayer2/ui/spherical/SceneRenderer.java
@@ -21,48 +21,67 @@ import android.graphics.SurfaceTexture;
 import android.opengl.GLES20;
 import android.opengl.Matrix;
 import android.support.annotation.Nullable;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.ui.spherical.ProjectionRenderer.EyeType;
 import com.google.android.exoplayer2.util.Assertions;
 import com.google.android.exoplayer2.util.TimedValueQueue;
+import com.google.android.exoplayer2.video.VideoFrameMetadataListener;
+import com.google.android.exoplayer2.video.spherical.CameraMotionListener;
 import com.google.android.exoplayer2.video.spherical.FrameRotationQueue;
 import com.google.android.exoplayer2.video.spherical.Projection;
+import com.google.android.exoplayer2.video.spherical.ProjectionDecoder;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
-/**
- * Renders a GL Scene.
- *
- * <p>All methods should be called only on the GL thread unless GL thread is stopped.
- */
-/*package*/ final class SceneRenderer {
+/** Renders a GL Scene. */
+/*package*/ class SceneRenderer implements VideoFrameMetadataListener, CameraMotionListener {
 
   private final AtomicBoolean frameAvailable;
+  private final AtomicBoolean resetRotationAtNextFrame;
   private final ProjectionRenderer projectionRenderer;
   private final FrameRotationQueue frameRotationQueue;
   private final TimedValueQueue<Long> sampleTimestampQueue;
+  private final TimedValueQueue<Projection> projectionQueue;
   private final float[] rotationMatrix;
   private final float[] tempMatrix;
 
+  // Used by GL thread only
   private int textureId;
   private @MonotonicNonNull SurfaceTexture surfaceTexture;
-  private @Nullable Projection pendingProjection;
-  private long pendingProjectionTimeNs;
-  private long lastFrameTimestamp;
-  private boolean resetRotationAtNextFrame;
 
-  public SceneRenderer(
-      Projection projection,
-      FrameRotationQueue frameRotationQueue,
-      TimedValueQueue<Long> sampleTimestampQueue) {
-    this.frameRotationQueue = frameRotationQueue;
-    this.sampleTimestampQueue = sampleTimestampQueue;
+  // Used by other threads only
+  private volatile @C.StreamType int defaultStereoMode;
+  private @C.StreamType int lastStereoMode;
+  private @Nullable byte[] lastProjectionData;
+
+  // Methods called on any thread.
+
+  public SceneRenderer() {
     frameAvailable = new AtomicBoolean();
+    resetRotationAtNextFrame = new AtomicBoolean(true);
     projectionRenderer = new ProjectionRenderer();
-    projectionRenderer.setProjection(projection);
+    frameRotationQueue = new FrameRotationQueue();
+    sampleTimestampQueue = new TimedValueQueue<>();
+    projectionQueue = new TimedValueQueue<>();
     rotationMatrix = new float[16];
     tempMatrix = new float[16];
-    resetRotation();
+    defaultStereoMode = C.STEREO_MODE_MONO;
+    lastStereoMode = Format.NO_VALUE;
   }
+
+  /**
+   * Sets the default stereo mode. If the played video doesn't contain a stereo mode the default one
+   * is used.
+   *
+   * @param stereoMode A {@link C.StereoMode} value.
+   */
+  public void setDefaultStereoMode(@C.StereoMode int stereoMode) {
+    defaultStereoMode = stereoMode;
+  }
+
+  // Methods called on GL thread.
 
   /** Initializes the renderer. */
   public SurfaceTexture init() {
@@ -79,21 +98,11 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
     return surfaceTexture;
   }
 
-  public void resetRotation() {
-    resetRotationAtNextFrame = true;
-  }
-
-  /** Sets a {@link Projection} to be used to display video. */
-  public void setProjection(Projection projection, long timeNs) {
-    pendingProjection = projection;
-    pendingProjectionTimeNs = timeNs;
-  }
-
   /**
    * Draws the scene with a given eye pose and type.
    *
    * @param viewProjectionMatrix 16 element GL matrix.
-   * @param eyeType an {@link EyeType} value
+   * @param eyeType An {@link EyeType} value
    */
   public void drawFrame(float[] viewProjectionMatrix, int eyeType) {
     // glClear isn't strictly necessary when rendering fully spherical panoramas, but it can improve
@@ -104,20 +113,73 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
     if (frameAvailable.compareAndSet(true, false)) {
       Assertions.checkNotNull(surfaceTexture).updateTexImage();
       checkGlError();
-      if (resetRotationAtNextFrame) {
+      if (resetRotationAtNextFrame.compareAndSet(true, false)) {
         Matrix.setIdentityM(rotationMatrix, 0);
       }
-      lastFrameTimestamp = surfaceTexture.getTimestamp();
-      Long sampleTimestamp = sampleTimestampQueue.poll(lastFrameTimestamp);
-      if (sampleTimestamp != null) {
-        frameRotationQueue.pollRotationMatrix(rotationMatrix, sampleTimestamp);
+      long lastFrameTimestampNs = surfaceTexture.getTimestamp();
+      Long sampleTimestampUs = sampleTimestampQueue.poll(lastFrameTimestampNs);
+      if (sampleTimestampUs != null) {
+        frameRotationQueue.pollRotationMatrix(rotationMatrix, sampleTimestampUs);
       }
-    }
-    if (pendingProjection != null && pendingProjectionTimeNs <= lastFrameTimestamp) {
-      projectionRenderer.setProjection(pendingProjection);
-      pendingProjection = null;
+      Projection projection = projectionQueue.pollFloor(lastFrameTimestampNs);
+      if (projection != null) {
+        projectionRenderer.setProjection(projection);
+      }
     }
     Matrix.multiplyMM(tempMatrix, 0, viewProjectionMatrix, 0, rotationMatrix, 0);
     projectionRenderer.draw(textureId, tempMatrix, eyeType);
+  }
+
+  // Methods called on playback thread.
+
+  // VideoFrameMetadataListener implementation.
+
+  @Override
+  public void onVideoFrameAboutToBeRendered(
+      long presentationTimeUs, long releaseTimeNs, Format format) {
+    sampleTimestampQueue.add(releaseTimeNs, presentationTimeUs);
+    setProjection(format.projectionData, format.stereoMode, releaseTimeNs);
+  }
+
+  // CameraMotionListener implementation.
+
+  @Override
+  public void onCameraMotion(long timeUs, float[] rotation) {
+    frameRotationQueue.setRotation(timeUs, rotation);
+  }
+
+  @Override
+  public void onCameraMotionReset() {
+    sampleTimestampQueue.clear();
+    frameRotationQueue.reset();
+    resetRotationAtNextFrame.set(true);
+  }
+
+  /**
+   * Sets projection data and stereo mode of the media to be played.
+   *
+   * @param projectionData Contains the projection data to be rendered.
+   * @param stereoMode A {@link C.StereoMode} value.
+   * @param timeNs When then new projection should be used.
+   */
+  private void setProjection(
+      @Nullable byte[] projectionData, @C.StereoMode int stereoMode, long timeNs) {
+    byte[] oldProjectionData = lastProjectionData;
+    int oldStereoMode = lastStereoMode;
+    lastProjectionData = projectionData;
+    lastStereoMode = stereoMode == Format.NO_VALUE ? defaultStereoMode : stereoMode;
+    if (oldStereoMode == lastStereoMode && Arrays.equals(oldProjectionData, lastProjectionData)) {
+      return;
+    }
+
+    Projection projectionFromData = null;
+    if (lastProjectionData != null) {
+      projectionFromData = ProjectionDecoder.decode(lastProjectionData, lastStereoMode);
+    }
+    Projection projection =
+        projectionFromData != null && ProjectionRenderer.isSupported(projectionFromData)
+            ? projectionFromData
+            : Projection.createEquirectangular(lastStereoMode);
+    projectionQueue.add(timeNs, projection);
   }
 }
