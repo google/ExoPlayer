caa46d49e5a549aff26a2c9841e1a1b57676435a
aquilescanta <aquilescanta@google.com>
2018-09-14 09:21:01 -0700

    Explicitly document empty range removal in Util.removeRange.
    Also prevent an unnecessary allocation when the removed range is empty.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=212993427

--- library/core/src/main/java/com/google/android/exoplayer2/util/Util.java
+++ library/core/src/main/java/com/google/android/exoplayer2/util/Util.java
@@ -253,12 +253,21 @@ public final class Util {
   /**
    * Removes an indexed range from a List.
    *
+   * <p>Does nothing if the provided range is valid and {@code fromIndex == toIndex}.
+   *
    * @param list The List to remove the range from.
    * @param fromIndex The first index to be removed (inclusive).
    * @param toIndex The last index to be removed (exclusive).
+   * @throws IllegalArgumentException If {@code fromIndex} &lt; 0, {@code toIndex} &gt; {@code
+   *     list.size()}, or {@code fromIndex} &gt; {@code toIndex}.
    */
   public static <T> void removeRange(List<T> list, int fromIndex, int toIndex) {
-    list.subList(fromIndex, toIndex).clear();
+    if (fromIndex < 0 || toIndex > list.size() || fromIndex > toIndex) {
+      throw new IllegalArgumentException();
+    } else if (fromIndex != toIndex) {
+      // Checking index inequality prevents an unnecessary allocation.
+      list.subList(fromIndex, toIndex).clear();
+    }
   }
 
   /**
