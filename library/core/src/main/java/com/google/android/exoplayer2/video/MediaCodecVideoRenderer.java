8844694dfd0a033472b98c00e8cbaf023f69d596
olly <olly@google.com>
2018-11-05 10:11:27 -0800

    Automated g4 rollback of changelist 219585084.
    *** Reason for rollback ***
    
    Rolling forward again as [] should fix issue that prompted the rollback
    
    *** Original change description ***
    
    Automated g4 rollback of changelist 219130576.
    
    *** Original change description ***
    
    Re-enable codec re-use
    
    Issue: #2826
    
    ***
    
    ***
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=220124362

--- library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
@@ -283,8 +283,12 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
   @Override
   protected void onEnabled(boolean joining) throws ExoPlaybackException {
     super.onEnabled(joining);
+    int oldTunnelingAudioSessionId = tunnelingAudioSessionId;
     tunnelingAudioSessionId = getConfiguration().tunnelingAudioSessionId;
     tunneling = tunnelingAudioSessionId != C.AUDIO_SESSION_ID_UNSET;
+    if (tunnelingAudioSessionId != oldTunnelingAudioSessionId) {
+      releaseCodec();
+    }
     eventDispatcher.enabled(decoderCounters);
     frameReleaseTimeHelper.enable();
   }
