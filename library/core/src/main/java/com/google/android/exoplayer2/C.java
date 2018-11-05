a8d1f04f92e15b2832ba810bc4fb993312f23073
andrewlewis <andrewlewis@google.com>
2018-11-05 10:54:24 -0800

    Document error case for generateAudioSessionIdV21
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=220132865

--- library/core/src/main/java/com/google/android/exoplayer2/C.java
+++ library/core/src/main/java/com/google/android/exoplayer2/C.java
@@ -978,7 +978,10 @@ public final class C {
   }
 
   /**
-   * Returns a newly generated {@link android.media.AudioTrack} session identifier.
+   * Returns a newly generated audio session identifier, or {@link AudioManager#ERROR} if an error
+   * occurred in which case audio playback may fail.
+   *
+   * @see AudioManager#generateAudioSessionId()
    */
   @TargetApi(21)
   public static int generateAudioSessionIdV21(Context context) {
