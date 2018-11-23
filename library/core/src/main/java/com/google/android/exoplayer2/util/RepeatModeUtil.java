121d2108c473a920a2a45a7993b62f42d13f230d
olly <olly@google.com>
2018-11-23 16:55:26 +0000

    Add Lint.IfChange/ThenChange for repeat modes
    PiperOrigin-RevId: 222630411

--- library/core/src/main/java/com/google/android/exoplayer2/util/RepeatModeUtil.java
+++ library/core/src/main/java/com/google/android/exoplayer2/util/RepeatModeUtil.java
@@ -26,6 +26,7 @@ import java.lang.annotation.RetentionPolicy;
  */
 public final class RepeatModeUtil {
 
+  // LINT.IfChange
   /**
    * Set of repeat toggle modes. Can be combined using bit-wise operations. Possible flag values are
    * {@link #REPEAT_TOGGLE_MODE_NONE}, {@link #REPEAT_TOGGLE_MODE_ONE} and {@link
@@ -47,6 +48,7 @@ public final class RepeatModeUtil {
   public static final int REPEAT_TOGGLE_MODE_ONE = 1;
   /** "Repeat All" button enabled. */
   public static final int REPEAT_TOGGLE_MODE_ALL = 1 << 1; // 2
+  // LINT.ThenChange(../../../../../../../../../ui/src/main/res/values/attrs.xml)
 
   private RepeatModeUtil() {
     // Prevent instantiation.
