5909cbd681ce32f5f706e994571fa4b6924beee6
aquilescanta <aquilescanta@google.com>
2018-12-06 11:56:27 +0000

    Revert usage of MediaItems for Cast demo sample list
    PiperOrigin-RevId: 224308973

--- demos/cast/src/main/java/com/google/android/exoplayer2/castdemo/DemoUtil.java
+++ demos/cast/src/main/java/com/google/android/exoplayer2/castdemo/DemoUtil.java
@@ -15,7 +15,6 @@
  */
 package com.google.android.exoplayer2.castdemo;
 
-import com.google.android.exoplayer2.ext.cast.MediaItem;
 import com.google.android.exoplayer2.util.MimeTypes;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,50 +23,64 @@ import java.util.List;
 /** Utility methods and constants for the Cast demo application. */
 /* package */ final class DemoUtil {
 
+  /** Represents a media sample. */
+  public static final class Sample {
+
+    /** The uri of the media content. */
+    public final String uri;
+    /** The name of the sample. */
+    public final String name;
+    /** The mime type of the sample media content. */
+    public final String mimeType;
+
+    /**
+     * @param uri See {@link #uri}.
+     * @param name See {@link #name}.
+     * @param mimeType See {@link #mimeType}.
+     */
+    public Sample(String uri, String name, String mimeType) {
+      this.uri = uri;
+      this.name = name;
+      this.mimeType = mimeType;
+    }
+
+    @Override
+    public String toString() {
+      return name;
+    }
+  }
+
   public static final String MIME_TYPE_DASH = MimeTypes.APPLICATION_MPD;
   public static final String MIME_TYPE_HLS = MimeTypes.APPLICATION_M3U8;
   public static final String MIME_TYPE_SS = MimeTypes.APPLICATION_SS;
   public static final String MIME_TYPE_VIDEO_MP4 = MimeTypes.VIDEO_MP4;
 
   /** The list of samples available in the cast demo app. */
-  public static final List<MediaItem> SAMPLES;
+  public static final List<Sample> SAMPLES;
 
   static {
     // App samples.
-    ArrayList<MediaItem> samples = new ArrayList<>();
-    MediaItem.Builder sampleBuilder = new MediaItem.Builder();
+    ArrayList<Sample> samples = new ArrayList<>();
 
     samples.add(
-        sampleBuilder
-            .setTitle("DASH (clear,MP4,H264)")
-            .setMimeType(MIME_TYPE_DASH)
-            .setMedia("https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd")
-            .buildAndClear());
-
+        new Sample(
+            "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd",
+            "DASH (clear,MP4,H264)",
+            MIME_TYPE_DASH));
     samples.add(
-        sampleBuilder
-            .setTitle("Tears of Steel (HLS)")
-            .setMimeType(MIME_TYPE_HLS)
-            .setMedia(
-                "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/"
-                    + "hls/TearsOfSteel.m3u8")
-            .buildAndClear());
-
+        new Sample(
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/"
+                + "hls/TearsOfSteel.m3u8",
+            "Tears of Steel (HLS)",
+            MIME_TYPE_HLS));
     samples.add(
-        sampleBuilder
-            .setTitle("HLS Basic (TS)")
-            .setMimeType(MIME_TYPE_HLS)
-            .setMedia(
-                "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3"
-                    + "/bipbop_4x3_variant.m3u8")
-            .buildAndClear());
-
+        new Sample(
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3"
+                + "/bipbop_4x3_variant.m3u8",
+            "HLS Basic (TS)",
+            MIME_TYPE_HLS));
     samples.add(
-        sampleBuilder
-            .setTitle("Dizzy (MP4)")
-            .setMimeType(MIME_TYPE_VIDEO_MP4)
-            .setMedia("https://html5demos.com/assets/dizzy.mp4")
-            .buildAndClear());
+        new Sample("https://html5demos.com/assets/dizzy.mp4", "Dizzy (MP4)", MIME_TYPE_VIDEO_MP4));
     SAMPLES = Collections.unmodifiableList(samples);
   }
 
