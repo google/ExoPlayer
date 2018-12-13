445b158a76810f1da5eca1899a76268d96bba86b
olly <olly@google.com>
2018-12-13 16:17:14 +0000

    Clarify that the shutter can prevent flicker
    PiperOrigin-RevId: 225374071

--- library/ui/src/main/java/com/google/android/exoplayer2/ui/PlayerView.java
+++ library/ui/src/main/java/com/google/android/exoplayer2/ui/PlayerView.java
@@ -186,8 +186,9 @@ import java.util.List;
  *         <li>Type: {@link AspectRatioFrameLayout}
  *       </ul>
  *   <li><b>{@code exo_shutter}</b> - A view that's made visible when video should be hidden. This
- *       view is typically an opaque view that covers the video surface view, thereby obscuring it
- *       when visible.
+ *       view is typically an opaque view that covers the video surface, thereby obscuring it when
+ *       visible. Obscuring the surface in this way also helps to prevent flicker at the start of
+ *       playback when {@code surface_type="surface_view"}
  *       <ul>
  *         <li>Type: {@link View}
  *       </ul>
