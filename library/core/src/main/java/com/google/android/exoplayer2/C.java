b795e68c5a43c727f0b2c255d7a21590c594b04b
tonihei <tonihei@google.com>
2019-01-15 14:18:29 +0000

    Add buffer flag for last sample to improve buffered position calculation.
    The buffered position is currently based on the mimimum queued timestamp of
    all AV tracks. If the tracks have unequal lengths, one track continues loading
    without bounds as the "buffered position" will always stay at the shorter
    track's duration.
    
    This change adds an optional buffer flag to mark the last sample of the
    stream. This is set in the Mp4Extractor only so far. ExtractorMediaSource
    uses this flag to ignore AV streams in the buffered duration calculation if
    they already finished loading.
    
    Issue:#3670
    PiperOrigin-RevId: 229359899

--- library/core/src/main/java/com/google/android/exoplayer2/C.java
+++ library/core/src/main/java/com/google/android/exoplayer2/C.java
@@ -460,8 +460,8 @@ public final class C {
 
   /**
    * Flags which can apply to a buffer containing a media sample. Possible flag values are {@link
-   * #BUFFER_FLAG_KEY_FRAME}, {@link #BUFFER_FLAG_END_OF_STREAM}, {@link #BUFFER_FLAG_ENCRYPTED} and
-   * {@link #BUFFER_FLAG_DECODE_ONLY}.
+   * #BUFFER_FLAG_KEY_FRAME}, {@link #BUFFER_FLAG_END_OF_STREAM}, {@link #BUFFER_FLAG_LAST_SAMPLE},
+   * {@link #BUFFER_FLAG_ENCRYPTED} and {@link #BUFFER_FLAG_DECODE_ONLY}.
    */
   @Documented
   @Retention(RetentionPolicy.SOURCE)
@@ -470,6 +470,7 @@ public final class C {
       value = {
         BUFFER_FLAG_KEY_FRAME,
         BUFFER_FLAG_END_OF_STREAM,
+        BUFFER_FLAG_LAST_SAMPLE,
         BUFFER_FLAG_ENCRYPTED,
         BUFFER_FLAG_DECODE_ONLY
       })
@@ -482,6 +483,8 @@ public final class C {
    * Flag for empty buffers that signal that the end of the stream was reached.
    */
   public static final int BUFFER_FLAG_END_OF_STREAM = MediaCodec.BUFFER_FLAG_END_OF_STREAM;
+  /** Indicates that a buffer is known to contain the last media sample of the stream. */
+  public static final int BUFFER_FLAG_LAST_SAMPLE = 1 << 29; // 0x20000000
   /** Indicates that a buffer is (at least partially) encrypted. */
   public static final int BUFFER_FLAG_ENCRYPTED = 1 << 30; // 0x40000000
   /** Indicates that a buffer should be decoded but not rendered. */
