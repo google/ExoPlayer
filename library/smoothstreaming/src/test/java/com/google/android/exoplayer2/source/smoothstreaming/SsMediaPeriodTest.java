e925649079a4af56a39050be4baadeb0c9859b86
tonihei <tonihei@google.com>
2019-01-29 12:57:22 +0000

    Change getStreamKeys to take a list of TrackSelections.
    Converting a single track selection to stream keys is only possible if the output
    is independent from other track selections being made.
    
    This is not the case for DASH and HLS embedded track groups which should select the
    already selected primary track if possible (and thus needs to know  whether a primary
    track group is selected).
    
    Also, update the test method to take a period index.
    
    PiperOrigin-RevId: 231385490

--- library/smoothstreaming/src/test/java/com/google/android/exoplayer2/source/smoothstreaming/SsMediaPeriodTest.java
+++ library/smoothstreaming/src/test/java/com/google/android/exoplayer2/source/smoothstreaming/SsMediaPeriodTest.java
@@ -61,7 +61,7 @@ public class SsMediaPeriodTest {
             createStreamElement(
                 /* name= */ "text", C.TRACK_TYPE_TEXT, createTextFormat(/* language= */ "eng")));
     FilterableManifestMediaPeriodFactory<SsManifest> mediaPeriodFactory =
-        manifest ->
+        (manifest, periodIndex) ->
             new SsMediaPeriod(
                 manifest,
                 mock(SsChunkSource.Factory.class),
@@ -77,7 +77,7 @@ public class SsMediaPeriodTest {
                 mock(Allocator.class));
 
     MediaPeriodAsserts.assertGetStreamKeysAndManifestFilterIntegration(
-        mediaPeriodFactory, testManifest);
+        mediaPeriodFactory, testManifest, /* periodIndex= */ 0);
   }
 
   private static Format createVideoFormat(int bitrate) {
