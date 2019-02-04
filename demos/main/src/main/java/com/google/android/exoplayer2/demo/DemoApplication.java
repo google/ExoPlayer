a5d64463c781092f60664d9b86d9e0867dec0561
eguven <eguven@google.com>
2019-02-04 17:03:12 +0000

    Make DownloadTracker use DownloadIndex
    DownloadTracker will stop updating DownloadIndex when DownloadManager
    starts using the same DownloadIndex.
    
    PiperOrigin-RevId: 232306803

--- demos/main/src/main/java/com/google/android/exoplayer2/demo/DemoApplication.java
+++ demos/main/src/main/java/com/google/android/exoplayer2/demo/DemoApplication.java
@@ -18,7 +18,11 @@ package com.google.android.exoplayer2.demo;
 import android.app.Application;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.database.ExoDatabaseProvider;
+import com.google.android.exoplayer2.offline.ActionFile;
+import com.google.android.exoplayer2.offline.DefaultDownloadIndex;
 import com.google.android.exoplayer2.offline.DefaultDownloaderFactory;
+import com.google.android.exoplayer2.offline.DownloadIndexUtil;
 import com.google.android.exoplayer2.offline.DownloadManager;
 import com.google.android.exoplayer2.offline.DownloaderConstructorHelper;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -31,14 +35,17 @@ import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
 import com.google.android.exoplayer2.upstream.cache.CacheDataSourceFactory;
 import com.google.android.exoplayer2.upstream.cache.NoOpCacheEvictor;
 import com.google.android.exoplayer2.upstream.cache.SimpleCache;
+import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.Util;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Placeholder application to facilitate overriding Application methods for debugging and testing.
  */
 public class DemoApplication extends Application {
 
+  private static final String TAG = "DemoApplication";
   private static final String DOWNLOAD_ACTION_FILE = "actions";
   private static final String DOWNLOAD_TRACKER_ACTION_FILE = "tracked_actions";
   private static final String DOWNLOAD_CONTENT_DIRECTORY = "downloads";
@@ -97,6 +104,16 @@ public class DemoApplication extends Application {
 
   private synchronized void initDownloadManager() {
     if (downloadManager == null) {
+      DefaultDownloadIndex downloadIndex = new DefaultDownloadIndex(new ExoDatabaseProvider(this));
+      File actionFile = new File(getDownloadDirectory(), DOWNLOAD_TRACKER_ACTION_FILE);
+      if (actionFile.exists()) {
+        try {
+          DownloadIndexUtil.upgradeActionFile(new ActionFile(actionFile), downloadIndex, null);
+        } catch (IOException e) {
+          Log.e(TAG, "Upgrading action file failed", e);
+        }
+        actionFile.delete();
+      }
       DownloaderConstructorHelper downloaderConstructorHelper =
           new DownloaderConstructorHelper(getDownloadCache(), buildHttpDataSourceFactory());
       downloadManager =
@@ -108,10 +125,7 @@ public class DemoApplication extends Application {
               DownloadManager.DEFAULT_MIN_RETRY_COUNT,
               DownloadManager.DEFAULT_REQUIREMENTS);
       downloadTracker =
-          new DownloadTracker(
-              /* context= */ this,
-              buildDataSourceFactory(),
-              new File(getDownloadDirectory(), DOWNLOAD_TRACKER_ACTION_FILE));
+          new DownloadTracker(/* context= */ this, buildDataSourceFactory(), downloadIndex);
       downloadManager.addListener(downloadTracker);
     }
   }
