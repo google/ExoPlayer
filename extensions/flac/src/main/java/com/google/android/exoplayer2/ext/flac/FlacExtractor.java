d1cf92e0a0d9d7d776fbd79ceb097c1e8156bbab
olly <olly@google.com>
2019-02-11 15:53:57 +0000

    Add tests to ensure DefaultRenderersFactory reflection works
    Also add explicit Lint.IfChange/ThenChange rules to make it
    clearer what needs to be done when changing one of the code
    paths that's accessed via reflection.
    
    PiperOrigin-RevId: 233406709

--- extensions/flac/src/main/java/com/google/android/exoplayer2/ext/flac/FlacExtractor.java
+++ extensions/flac/src/main/java/com/google/android/exoplayer2/ext/flac/FlacExtractor.java
@@ -94,7 +94,7 @@ public final class FlacExtractor implements Extractor {
 
   /** Constructs an instance with flags = 0. */
   public FlacExtractor() {
-    this(0);
+    this(/* flags= */ 0);
   }
 
   /**
