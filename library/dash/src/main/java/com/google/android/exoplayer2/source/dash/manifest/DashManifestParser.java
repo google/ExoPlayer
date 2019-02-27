99ceeeff2597e674c604a72396551e14280f0e71
Arnold Szabo <szaboaarnold@gmail.com>
2019-02-28 00:11:04 +0200

    Remove old parseRole method that looked only for main

--- library/dash/src/main/java/com/google/android/exoplayer2/source/dash/manifest/DashManifestParser.java
+++ library/dash/src/main/java/com/google/android/exoplayer2/source/dash/manifest/DashManifestParser.java
@@ -470,24 +470,6 @@ public class DashManifestParser extends DefaultHandler
   }
 
   /**
-   * Parses a Role element.
-   *
-   * @param xpp The parser from which to read.
-   * @throws XmlPullParserException If an error occurs parsing the element.
-   * @throws IOException If an error occurs reading the element.
-   * @return {@link C.SelectionFlags} parsed from the element.
-   */
-  protected int parseRole(XmlPullParser xpp) throws XmlPullParserException, IOException {
-    String schemeIdUri = parseString(xpp, "schemeIdUri", null);
-    String value = parseString(xpp, "value", null);
-    do {
-      xpp.next();
-    } while (!XmlPullParserUtil.isEndTag(xpp, "Role"));
-    return "urn:mpeg:dash:role:2011".equals(schemeIdUri) && "main".equals(value)
-        ? C.SELECTION_FLAG_DEFAULT : 0;
-  }
-
-  /**
    * Parses children of AdaptationSet elements not specifically parsed elsewhere.
    *
    * @param xpp The XmpPullParser from which the AdaptationSet child should be parsed.
