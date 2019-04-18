32a1b4280ab19e38bdf524392a230f50df5259d2
olly <olly@google.com>
2019-04-18 18:48:09 +0100

    Rename start/stopDownloads to resume/pauseDownloads
    PiperOrigin-RevId: 244216620

--- library/dash/src/test/java/com/google/android/exoplayer2/source/dash/offline/DownloadManagerDashTest.java
+++ library/dash/src/test/java/com/google/android/exoplayer2/source/dash/offline/DownloadManagerDashTest.java
@@ -267,7 +267,7 @@ public class DownloadManagerDashTest {
           downloadManagerListener =
               new TestDownloadManagerListener(
                   downloadManager, dummyMainThread, /* timeout= */ 3000);
-          downloadManager.startDownloads();
+          downloadManager.resumeDownloads();
         });
   }
 
