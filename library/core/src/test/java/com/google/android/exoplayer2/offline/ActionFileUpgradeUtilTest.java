f7f6489f573189f84d8c3f9b0b9ab0797f648d08
eguven <eguven@google.com>
2019-04-23 17:09:05 +0100

    Add option to add entries in an ActionFile to DownloadIndex as completed
    PiperOrigin-RevId: 244864742

--- library/core/src/test/java/com/google/android/exoplayer2/offline/ActionFileUpgradeUtilTest.java
+++ library/core/src/test/java/com/google/android/exoplayer2/offline/ActionFileUpgradeUtilTest.java
@@ -88,7 +88,11 @@ public class ActionFileUpgradeUtilTest {
             new byte[] {5, 4, 3, 2, 1});
 
     ActionFileUpgradeUtil.upgradeAndDelete(
-        tempFile, /* downloadIdProvider= */ null, downloadIndex, /* deleteOnFailure= */ true);
+        tempFile,
+        /* downloadIdProvider= */ null,
+        downloadIndex,
+        /* deleteOnFailure= */ true,
+        /* addNewDownloadsAsCompleted= */ false);
 
     assertDownloadIndexContainsRequest(expectedRequest1, Download.STATE_QUEUED);
     assertDownloadIndexContainsRequest(expectedRequest2, Download.STATE_QUEUED);
@@ -108,7 +112,8 @@ public class ActionFileUpgradeUtilTest {
             /* customCacheKey= */ "key123",
             data);
 
-    ActionFileUpgradeUtil.mergeRequest(request, downloadIndex);
+    ActionFileUpgradeUtil.mergeRequest(
+        request, downloadIndex, /* addNewDownloadAsCompleted= */ false);
 
     assertDownloadIndexContainsRequest(request, Download.STATE_QUEUED);
   }
@@ -135,8 +140,10 @@ public class ActionFileUpgradeUtilTest {
             asList(streamKey2),
             /* customCacheKey= */ "key123",
             new byte[] {5, 4, 3, 2, 1});
-    ActionFileUpgradeUtil.mergeRequest(request1, downloadIndex);
-    ActionFileUpgradeUtil.mergeRequest(request2, downloadIndex);
+    ActionFileUpgradeUtil.mergeRequest(
+        request1, downloadIndex, /* addNewDownloadAsCompleted= */ false);
+    ActionFileUpgradeUtil.mergeRequest(
+        request2, downloadIndex, /* addNewDownloadAsCompleted= */ false);
 
     Download download = downloadIndex.getDownload(request2.id);
     assertThat(download).isNotNull();
@@ -146,6 +153,42 @@ public class ActionFileUpgradeUtilTest {
     assertThat(download.request.uri).isEqualTo(request2.uri);
     assertThat(download.request.streamKeys).containsExactly(streamKey1, streamKey2);
     assertThat(download.state).isEqualTo(Download.STATE_QUEUED);
+  }
+
+  @Test
+  public void mergeRequest_addNewDownloadAsCompleted() throws IOException {
+    StreamKey streamKey1 =
+        new StreamKey(/* periodIndex= */ 3, /* groupIndex= */ 4, /* trackIndex= */ 5);
+    StreamKey streamKey2 =
+        new StreamKey(/* periodIndex= */ 0, /* groupIndex= */ 1, /* trackIndex= */ 2);
+    DownloadRequest request1 =
+        new DownloadRequest(
+            "id1",
+            TYPE_PROGRESSIVE,
+            Uri.parse("https://www.test.com/download1"),
+            asList(streamKey1),
+            /* customCacheKey= */ "key123",
+            new byte[] {1, 2, 3, 4});
+    DownloadRequest request2 =
+        new DownloadRequest(
+            "id2",
+            TYPE_PROGRESSIVE,
+            Uri.parse("https://www.test.com/download2"),
+            asList(streamKey2),
+            /* customCacheKey= */ "key123",
+            new byte[] {5, 4, 3, 2, 1});
+    ActionFileUpgradeUtil.mergeRequest(
+        request1, downloadIndex, /* addNewDownloadAsCompleted= */ false);
+
+    // Merging existing download, keeps it queued.
+    ActionFileUpgradeUtil.mergeRequest(
+        request1, downloadIndex, /* addNewDownloadAsCompleted= */ true);
+    assertThat(downloadIndex.getDownload(request1.id).state).isEqualTo(Download.STATE_QUEUED);
+
+    // New download is merged as completed.
+    ActionFileUpgradeUtil.mergeRequest(
+        request2, downloadIndex, /* addNewDownloadAsCompleted= */ true);
+    assertThat(downloadIndex.getDownload(request2.id).state).isEqualTo(Download.STATE_COMPLETED);
   }
 
   private void assertDownloadIndexContainsRequest(DownloadRequest request, int state)
