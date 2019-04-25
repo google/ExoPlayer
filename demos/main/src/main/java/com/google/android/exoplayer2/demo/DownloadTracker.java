dae5177951dc22706d3a210daee6e2408cc01919
eguven <eguven@google.com>
2019-04-25 16:52:35 +0100

    Add DownloadHelper.createMediaSource utility method
    PiperOrigin-RevId: 245243488

--- demos/main/src/main/java/com/google/android/exoplayer2/demo/DownloadTracker.java
+++ demos/main/src/main/java/com/google/android/exoplayer2/demo/DownloadTracker.java
@@ -30,15 +30,12 @@ import com.google.android.exoplayer2.offline.DownloadIndex;
 import com.google.android.exoplayer2.offline.DownloadManager;
 import com.google.android.exoplayer2.offline.DownloadRequest;
 import com.google.android.exoplayer2.offline.DownloadService;
-import com.google.android.exoplayer2.offline.StreamKey;
 import com.google.android.exoplayer2.trackselection.MappingTrackSelector.MappedTrackInfo;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.Util;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /** Tracks media that has been downloaded. */
@@ -86,11 +83,9 @@ public class DownloadTracker {
   }
 
   @SuppressWarnings("unchecked")
-  public List<StreamKey> getOfflineStreamKeys(Uri uri) {
+  public DownloadRequest getDownloadRequest(Uri uri) {
     Download download = downloads.get(uri);
-    return download != null && download.state != Download.STATE_FAILED
-        ? download.request.streamKeys
-        : Collections.emptyList();
+    return download != null && download.state != Download.STATE_FAILED ? download.request : null;
   }
 
   public void toggleDownload(
