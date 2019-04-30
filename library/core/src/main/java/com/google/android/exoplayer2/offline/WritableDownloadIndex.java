4a5b8e17de84d9ae34af3f0964147f9a2bffcd49
olly <olly@google.com>
2019-04-30 12:25:04 +0100

    DownloadManager improvements
    - Do requirements TODO
    - Add useful helper method to retrieve not met requirements
    - Fix WritableDownloadIndex Javadoc
    
    PiperOrigin-RevId: 245922903

--- library/core/src/main/java/com/google/android/exoplayer2/offline/WritableDownloadIndex.java
+++ library/core/src/main/java/com/google/android/exoplayer2/offline/WritableDownloadIndex.java
@@ -17,43 +17,43 @@ package com.google.android.exoplayer2.offline;
 
 import java.io.IOException;
 
-/** An writable index of {@link Download Downloads}. */
+/** A writable index of {@link Download Downloads}. */
 public interface WritableDownloadIndex extends DownloadIndex {
 
   /**
    * Adds or replaces a {@link Download}.
    *
    * @param download The {@link Download} to be added.
-   * @throws throws IOException If an error occurs setting the state.
+   * @throws IOException If an error occurs setting the state.
    */
   void putDownload(Download download) throws IOException;
 
   /**
-   * Removes the {@link Download} with the given {@code id}.
+   * Removes the download with the given ID. Does nothing if a download with the given ID does not
+   * exist.
    *
-   * @param id ID of a {@link Download}.
-   * @throws throws IOException If an error occurs removing the state.
+   * @param id The ID of the download to remove.
+   * @throws IOException If an error occurs removing the state.
    */
   void removeDownload(String id) throws IOException;
+
   /**
    * Sets the stop reason of the downloads in a terminal state ({@link Download#STATE_COMPLETED},
    * {@link Download#STATE_FAILED}).
    *
    * @param stopReason The stop reason.
-   * @throws throws IOException If an error occurs updating the state.
+   * @throws IOException If an error occurs updating the state.
    */
   void setStopReason(int stopReason) throws IOException;
 
   /**
-   * Sets the stop reason of the download with the given {@code id} in a terminal state ({@link
-   * Download#STATE_COMPLETED}, {@link Download#STATE_FAILED}).
+   * Sets the stop reason of the download with the given ID in a terminal state ({@link
+   * Download#STATE_COMPLETED}, {@link Download#STATE_FAILED}). Does nothing if a download with the
+   * given ID does not exist, or if it's not in a terminal state.
    *
-   * <p>If there's no {@link Download} with the given {@code id} or it isn't in a terminal state,
-   * then nothing happens.
-   *
-   * @param id ID of a {@link Download}.
+   * @param id The ID of the download to update.
    * @param stopReason The stop reason.
-   * @throws throws IOException If an error occurs updating the state.
+   * @throws IOException If an error occurs updating the state.
    */
   void setStopReason(String id, int stopReason) throws IOException;
 }
