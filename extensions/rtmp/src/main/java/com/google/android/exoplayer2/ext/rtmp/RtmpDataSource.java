8669d6dc102f7a8c99e8fed6f5d29b973fa8503a
olly <olly@google.com>
2019-05-21 20:04:42 +0100

    Fix missing import
    PiperOrigin-RevId: 249298093

--- extensions/rtmp/src/main/java/com/google/android/exoplayer2/ext/rtmp/RtmpDataSource.java
+++ extensions/rtmp/src/main/java/com/google/android/exoplayer2/ext/rtmp/RtmpDataSource.java
@@ -18,6 +18,7 @@ package com.google.android.exoplayer2.ext.rtmp;
 import static com.google.android.exoplayer2.util.Util.castNonNull;
 
 import android.net.Uri;
+import androidx.annotation.Nullable;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayerLibraryInfo;
 import com.google.android.exoplayer2.upstream.BaseDataSource;
