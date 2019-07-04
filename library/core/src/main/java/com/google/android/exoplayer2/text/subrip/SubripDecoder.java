d74020a1fc6a42036a8dc8891a2a58547587c021
olly <olly@google.com>
2019-07-04 20:02:20 +0100

    Remove more low hanging fruit from nullness blacklist
    PiperOrigin-RevId: 256573352

--- library/core/src/main/java/com/google/android/exoplayer2/text/subrip/SubripDecoder.java
+++ library/core/src/main/java/com/google/android/exoplayer2/text/subrip/SubripDecoder.java
@@ -111,11 +111,13 @@ public final class SubripDecoder extends SimpleSubtitleDecoder {
       // Read and parse the text and tags.
       textBuilder.setLength(0);
       tags.clear();
-      while (!TextUtils.isEmpty(currentLine = subripData.readLine())) {
+      currentLine = subripData.readLine();
+      while (!TextUtils.isEmpty(currentLine)) {
         if (textBuilder.length() > 0) {
           textBuilder.append("<br>");
         }
         textBuilder.append(processLine(currentLine, tags));
+        currentLine = subripData.readLine();
       }
 
       Spanned text = Html.fromHtml(textBuilder.toString());
@@ -132,7 +134,7 @@ public final class SubripDecoder extends SimpleSubtitleDecoder {
       cues.add(buildCue(text, alignmentTag));
 
       if (haveEndTimecode) {
-        cues.add(null);
+        cues.add(Cue.EMPTY);
       }
     }
 
