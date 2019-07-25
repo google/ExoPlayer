596be3b71ba1e37fe02dd82b13d3042d97711ff9
olly <olly@google.com>
2019-07-25 22:32:04 +0100

    Cast: Simplify MediaItem/Sample to a single MediaItem class
    PiperOrigin-RevId: 260021990

--- demos/cast/src/main/java/com/google/android/exoplayer2/castdemo/DemoUtil.java
+++ demos/cast/src/main/java/com/google/android/exoplayer2/castdemo/DemoUtil.java
@@ -15,70 +15,17 @@
  */
 package com.google.android.exoplayer2.castdemo;
 
-import androidx.annotation.Nullable;
+import android.net.Uri;
 import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ext.cast.MediaItem;
+import com.google.android.exoplayer2.ext.cast.MediaItem.DrmConfiguration;
 import com.google.android.exoplayer2.util.MimeTypes;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 /** Utility methods and constants for the Cast demo application. */
 /* package */ final class DemoUtil {
-
-  /** Represents a media sample. */
-  public static final class Sample {
-
-    /** The URI of the media content. */
-    public final String uri;
-    /** The name of the sample. */
-    public final String name;
-    /** The mime type of the sample media content. */
-    public final String mimeType;
-    /** Data to configure DRM license acquisition. May be null if content is not DRM-protected. */
-    @Nullable public final DrmConfiguration drmConfiguration;
-
-    public Sample(String uri, String name, String mimeType) {
-      this(uri, name, mimeType, /* drmConfiguration= */ null);
-    }
-
-    public Sample(
-        String uri, String name, String mimeType, @Nullable DrmConfiguration drmConfiguration) {
-      this.uri = uri;
-      this.name = name;
-      this.mimeType = mimeType;
-      this.drmConfiguration = drmConfiguration;
-    }
-
-    @Override
-    public String toString() {
-      return name;
-    }
-  }
-
-  /** Holds information required to play DRM-protected content. */
-  public static final class DrmConfiguration {
-
-    /** The {@link UUID} of the DRM scheme that protects the content. */
-    public final UUID drmSchemeUuid;
-    /**
-     * The URI from which players should obtain DRM licenses. May be null if the license server URI
-     * is provided as part of the media.
-     */
-    @Nullable public final String licenseServerUri;
-    /** HTTP request headers to include the in DRM license requests. */
-    public final Map<String, String> httpRequestHeaders;
-
-    public DrmConfiguration(
-        UUID drmSchemeUuid,
-        @Nullable String licenseServerUri,
-        Map<String, String> httpRequestHeaders) {
-      this.drmSchemeUuid = drmSchemeUuid;
-      this.licenseServerUri = licenseServerUri;
-      this.httpRequestHeaders = httpRequestHeaders;
-    }
-  }
 
   public static final String MIME_TYPE_DASH = MimeTypes.APPLICATION_MPD;
   public static final String MIME_TYPE_HLS = MimeTypes.APPLICATION_M3U8;
@@ -86,26 +33,31 @@ import java.util.UUID;
   public static final String MIME_TYPE_VIDEO_MP4 = MimeTypes.VIDEO_MP4;
 
   /** The list of samples available in the cast demo app. */
-  public static final List<Sample> SAMPLES;
+  public static final List<MediaItem> SAMPLES;
 
   static {
     // App samples.
-    ArrayList<Sample> samples = new ArrayList<>();
+    ArrayList<MediaItem> samples = new ArrayList<>();
 
     // Clear content.
     samples.add(
-        new Sample(
-            "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd",
-            "Clear DASH: Tears",
-            MIME_TYPE_DASH));
+        new MediaItem.Builder()
+            .setUri("https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd")
+            .setTitle("Clear DASH: Tears")
+            .setMimeType(MIME_TYPE_DASH)
+            .build());
     samples.add(
-        new Sample(
-            "https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8",
-            "Clear HLS: Angel one",
-            MIME_TYPE_HLS));
+        new MediaItem.Builder()
+            .setUri("https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8")
+            .setTitle("Clear HLS: Angel one")
+            .setMimeType(MIME_TYPE_HLS)
+            .build());
     samples.add(
-        new Sample(
-            "https://html5demos.com/assets/dizzy.mp4", "Clear MP4: Dizzy", MIME_TYPE_VIDEO_MP4));
+        new MediaItem.Builder()
+            .setUri("https://html5demos.com/assets/dizzy.mp4")
+            .setTitle("Clear MP4: Dizzy")
+            .setMimeType(MIME_TYPE_VIDEO_MP4)
+            .build());
 
     SAMPLES = Collections.unmodifiableList(samples);
   }
--- extensions/cast/src/test/java/com/google/android/exoplayer2/ext/cast/MediaItemTest.java
+++ extensions/cast/src/test/java/com/google/android/exoplayer2/ext/cast/MediaItemTest.java
@@ -21,9 +21,7 @@ import android.net.Uri;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.util.MimeTypes;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -36,7 +34,7 @@ public class MediaItemTest {
     MediaItem.Builder builder = new MediaItem.Builder();
     MediaItem item1 =
         builder
-            .setMedia("http://example.com")
+            .setUri(Uri.parse("http://example.com"))
             .setTitle("title")
             .setMimeType(MimeTypes.AUDIO_MP4)
             .build();
@@ -45,18 +43,19 @@ public class MediaItemTest {
   }
 
   @Test
-  public void buildMediaItem_assertDefaultValues() {
-    assertDefaultValues(new MediaItem.Builder().build());
-  }
-
-  @Test
   public void equals_withEqualDrmSchemes_returnsTrue() {
     MediaItem.Builder builder1 = new MediaItem.Builder();
     MediaItem mediaItem1 =
-        builder1.setMedia("www.google.com").setDrmSchemes(createDummyDrmSchemes(1)).build();
+        builder1
+            .setUri(Uri.parse("www.google.com"))
+            .setDrmConfiguration(buildDrmConfiguration(1))
+            .build();
     MediaItem.Builder builder2 = new MediaItem.Builder();
     MediaItem mediaItem2 =
-        builder2.setMedia("www.google.com").setDrmSchemes(createDummyDrmSchemes(1)).build();
+        builder2
+            .setUri(Uri.parse("www.google.com"))
+            .setDrmConfiguration(buildDrmConfiguration(1))
+            .build();
     assertThat(mediaItem1).isEqualTo(mediaItem2);
   }
 
@@ -64,33 +63,24 @@ public class MediaItemTest {
   public void equals_withDifferentDrmRequestHeaders_returnsFalse() {
     MediaItem.Builder builder1 = new MediaItem.Builder();
     MediaItem mediaItem1 =
-        builder1.setMedia("www.google.com").setDrmSchemes(createDummyDrmSchemes(1)).build();
+        builder1
+            .setUri(Uri.parse("www.google.com"))
+            .setDrmConfiguration(buildDrmConfiguration(1))
+            .build();
     MediaItem.Builder builder2 = new MediaItem.Builder();
     MediaItem mediaItem2 =
-        builder2.setMedia("www.google.com").setDrmSchemes(createDummyDrmSchemes(2)).build();
+        builder2
+            .setUri(Uri.parse("www.google.com"))
+            .setDrmConfiguration(buildDrmConfiguration(2))
+            .build();
     assertThat(mediaItem1).isNotEqualTo(mediaItem2);
   }
 
-  private static void assertDefaultValues(MediaItem item) {
-    assertThat(item.title).isEmpty();
-    assertThat(item.media.uri).isEqualTo(Uri.EMPTY);
-    assertThat(item.drmSchemes).isEmpty();
-    assertThat(item.mimeType).isEmpty();
-  }
-
-  private static List<MediaItem.DrmScheme> createDummyDrmSchemes(int seed) {
-    HashMap<String, String> requestHeaders1 = new HashMap<>();
-    requestHeaders1.put("key1", "value1");
-    requestHeaders1.put("key2", "value1");
-    MediaItem.UriBundle uriBundle1 =
-        new MediaItem.UriBundle(Uri.parse("www.uri1.com"), requestHeaders1);
-    MediaItem.DrmScheme drmScheme1 = new MediaItem.DrmScheme(C.WIDEVINE_UUID, uriBundle1);
-    HashMap<String, String> requestHeaders2 = new HashMap<>();
-    requestHeaders2.put("key3", "value3");
-    requestHeaders2.put("key4", "valueWithSeed" + seed);
-    MediaItem.UriBundle uriBundle2 =
-        new MediaItem.UriBundle(Uri.parse("www.uri2.com"), requestHeaders2);
-    MediaItem.DrmScheme drmScheme2 = new MediaItem.DrmScheme(C.PLAYREADY_UUID, uriBundle2);
-    return Arrays.asList(drmScheme1, drmScheme2);
+  private static MediaItem.DrmConfiguration buildDrmConfiguration(int seed) {
+    HashMap<String, String> requestHeaders = new HashMap<>();
+    requestHeaders.put("key1", "value1");
+    requestHeaders.put("key2", "value2" + seed);
+    return new MediaItem.DrmConfiguration(
+        C.WIDEVINE_UUID, Uri.parse("www.uri1.com"), requestHeaders);
   }
 }
