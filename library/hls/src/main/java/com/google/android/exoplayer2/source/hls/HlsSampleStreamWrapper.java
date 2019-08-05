d1ac2727a6e1d2928d203791c6453908e77038e5
tonihei <tonihei@google.com>
2019-08-05 16:57:44 +0100

    Update stale TrackSelections in chunk sources when keeping the streams.
    If we keep streams in chunk sources after selecting new tracks, we also keep
    a reference to a stale disabled TrackSelection object. Fix this by updating
    the TrackSelection object when keeping the stream. The static part of the
    selection (i.e. the subset of selected tracks) stays the same in all cases.
    
    Issue:#6256
    PiperOrigin-RevId: 261696082

--- library/hls/src/main/java/com/google/android/exoplayer2/source/hls/HlsSampleStreamWrapper.java
+++ library/hls/src/main/java/com/google/android/exoplayer2/source/hls/HlsSampleStreamWrapper.java
@@ -292,14 +292,17 @@ import java.util.Map;
     TrackSelection primaryTrackSelection = oldPrimaryTrackSelection;
     // Select new tracks.
     for (int i = 0; i < selections.length; i++) {
-      if (streams[i] == null && selections[i] != null) {
+      TrackSelection selection = selections[i];
+      if (selection == null) {
+        continue;
+      }
+      int trackGroupIndex = trackGroups.indexOf(selection.getTrackGroup());
+      if (trackGroupIndex == primaryTrackGroupIndex) {
+        primaryTrackSelection = selection;
+        chunkSource.setTrackSelection(selection);
+      }
+      if (streams[i] == null) {
         enabledTrackGroupCount++;
-        TrackSelection selection = selections[i];
-        int trackGroupIndex = trackGroups.indexOf(selection.getTrackGroup());
-        if (trackGroupIndex == primaryTrackGroupIndex) {
-          primaryTrackSelection = selection;
-          chunkSource.selectTracks(selection);
-        }
         streams[i] = new HlsSampleStream(this, trackGroupIndex);
         streamResetFlags[i] = true;
         if (trackGroupToSampleQueueIndex != null) {
