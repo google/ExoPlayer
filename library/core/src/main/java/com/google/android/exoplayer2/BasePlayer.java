7d8bee799bd899b44b7457e3fed530c472426863
tonihei <tonihei@google.com>
2019-09-30 18:01:22 +0100

    Add convenience Player.isCurrentWindowLive method.
    PiperOrigin-RevId: 272005632

--- library/core/src/main/java/com/google/android/exoplayer2/BasePlayer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/BasePlayer.java
@@ -128,6 +128,12 @@ public abstract class BasePlayer implements Player {
   }
 
   @Override
+  public final boolean isCurrentWindowLive() {
+    Timeline timeline = getCurrentTimeline();
+    return !timeline.isEmpty() && timeline.getWindow(getCurrentWindowIndex(), window).isLive;
+  }
+
+  @Override
   public final boolean isCurrentWindowSeekable() {
     Timeline timeline = getCurrentTimeline();
     return !timeline.isEmpty() && timeline.getWindow(getCurrentWindowIndex(), window).isSeekable;
