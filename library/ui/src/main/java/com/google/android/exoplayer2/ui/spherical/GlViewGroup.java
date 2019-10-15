5e538a2a28a43df077993be438f4a0c06de7ee3a
olly <olly@google.com>
2019-10-15 19:09:09 +0100

    Clean up GvrPlayerActivity
    PiperOrigin-RevId: 274845045

--- library/ui/src/main/java/com/google/android/exoplayer2/ui/spherical/GlViewGroup.java
+++ library/ui/src/main/java/com/google/android/exoplayer2/ui/spherical/GlViewGroup.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.android.exoplayer2.ui.spherical;
 
 import android.content.Context;
