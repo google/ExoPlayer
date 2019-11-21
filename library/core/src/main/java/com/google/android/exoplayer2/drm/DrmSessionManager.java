06092c0860c21113eda0c6ec95bccb2c1ba2a37b
olly <olly@google.com>
2019-11-21 16:50:24 +0000

    Simplify playback of clear samples without keys
    - Move property to DrmSession; it feels like a more natural place
      for it to go (and provides greater flexibility).
    - Change flags to a boolean.
    
    PiperOrigin-RevId: 281758729

--- library/core/src/main/java/com/google/android/exoplayer2/drm/DrmSessionManager.java
+++ library/core/src/main/java/com/google/android/exoplayer2/drm/DrmSessionManager.java
@@ -16,13 +16,9 @@
 package com.google.android.exoplayer2.drm;
 
 import android.os.Looper;
-import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.drm.DrmInitData.SchemeData;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
 /**
  * Manages a DRM session.
@@ -58,26 +54,6 @@ public interface DrmSessionManager<T extends ExoMediaCrypto> {
           return null;
         }
       };
-
-  /** Flags that control the handling of DRM protected content. */
-  @Documented
-  @Retention(RetentionPolicy.SOURCE)
-  @IntDef(
-      flag = true,
-      value = {FLAG_PLAY_CLEAR_SAMPLES_WITHOUT_KEYS})
-  @interface Flags {}
-
-  /**
-   * When this flag is set, clear samples of an encrypted region may be rendered when no keys are
-   * available.
-   *
-   * <p>Encrypted media may contain clear (un-encrypted) regions. For example a media file may start
-   * with a short clear region so as to allow playback to begin in parallel with key acquisition.
-   * When this flag is set, consumers of sample data are permitted to access the clear regions of
-   * encrypted media files when the associated {@link DrmSession} has not yet obtained the keys
-   * necessary for the encrypted regions of the media.
-   */
-  int FLAG_PLAY_CLEAR_SAMPLES_WITHOUT_KEYS = 1;
 
   /**
    * Acquires any required resources.
@@ -135,12 +111,6 @@ public interface DrmSessionManager<T extends ExoMediaCrypto> {
    * @return The DRM session.
    */
   DrmSession<T> acquireSession(Looper playbackLooper, DrmInitData drmInitData);
-
-  /** Returns flags that control the handling of DRM protected content. */
-  @Flags
-  default int getFlags() {
-    return 0;
-  }
 
   /**
    * Returns the {@link ExoMediaCrypto} type returned by sessions acquired using the given {@link
