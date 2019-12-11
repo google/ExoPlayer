511a0d4586fae6df8646ccd0482510b58e2a76f9
andrewlewis <andrewlewis@google.com>
2019-12-11 11:12:01 +0000

    Inline ENABLE_PRELOADING
    ImaAdsLoader relies on preloading being enabled (it doesn't
    work without it) so we may as well remove the constant to
    avoid potential confusion.
    
    PiperOrigin-RevId: 284951356

--- extensions/ima/src/main/java/com/google/android/exoplayer2/ext/ima/ImaAdsLoader.java
+++ extensions/ima/src/main/java/com/google/android/exoplayer2/ext/ima/ImaAdsLoader.java
@@ -272,11 +272,6 @@ public final class ImaAdsLoader
   private static final boolean DEBUG = false;
   private static final String TAG = "ImaAdsLoader";
 
-  /**
-   * Whether to enable preloading of ads in {@link AdsRenderingSettings}.
-   */
-  private static final boolean ENABLE_PRELOADING = true;
-
   private static final String IMA_SDK_SETTINGS_PLAYER_TYPE = "google/exo.ext.ima";
   private static final String IMA_SDK_SETTINGS_PLAYER_VERSION = ExoPlayerLibraryInfo.VERSION;
 
@@ -1053,7 +1048,7 @@ public final class ImaAdsLoader
 
   private void initializeAdsManager() {
     AdsRenderingSettings adsRenderingSettings = imaFactory.createAdsRenderingSettings();
-    adsRenderingSettings.setEnablePreloading(ENABLE_PRELOADING);
+    adsRenderingSettings.setEnablePreloading(true);
     adsRenderingSettings.setMimeTypes(supportedMimeTypes);
     if (mediaLoadTimeoutMs != TIMEOUT_UNSET) {
       adsRenderingSettings.setLoadVideoTimeout(mediaLoadTimeoutMs);
