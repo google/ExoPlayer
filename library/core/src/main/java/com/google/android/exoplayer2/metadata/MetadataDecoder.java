7715731b28bcbe3512647de5eafb0b37fdc5c724
ibaker <ibaker@google.com>
2019-12-13 13:54:01 +0000

    Tighten the documentation of MetadataInputDecoder
    Document that limit() is respected, but position() and arrayOffset()
    are assumed to be zero.
    
    PiperOrigin-RevId: 285384613

--- library/core/src/main/java/com/google/android/exoplayer2/metadata/MetadataDecoder.java
+++ library/core/src/main/java/com/google/android/exoplayer2/metadata/MetadataDecoder.java
@@ -16,6 +16,7 @@
 package com.google.android.exoplayer2.metadata;
 
 import androidx.annotation.Nullable;
+import java.nio.ByteBuffer;
 
 /**
  * Decodes metadata from binary data.
@@ -24,6 +25,9 @@ public interface MetadataDecoder {
 
   /**
    * Decodes a {@link Metadata} element from the provided input buffer.
+   *
+   * <p>Respects {@link ByteBuffer#limit()} of {@code inputBuffer.data}, but assumes {@link
+   * ByteBuffer#position()} and {@link ByteBuffer#arrayOffset()} are both zero.
    *
    * @param inputBuffer The input buffer to decode.
    * @return The decoded metadata object, or null if the metadata could not be decoded.
