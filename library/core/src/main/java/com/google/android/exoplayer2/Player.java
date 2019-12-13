81291f1f00cdd2a06ad9f15df5b6c17f002ce9ad
andrewlewis <andrewlewis@google.com>
2019-12-13 16:23:48 +0000

    Fix formatting in Player.java
    This is in preparation for adding an internal-only change near these
    lines.
    
    PiperOrigin-RevId: 285403671

--- library/core/src/main/java/com/google/android/exoplayer2/Player.java
+++ library/core/src/main/java/com/google/android/exoplayer2/Player.java
@@ -42,20 +42,20 @@ import java.lang.annotation.RetentionPolicy;
 /**
  * A media player interface defining traditional high-level functionality, such as the ability to
  * play, pause, seek and query properties of the currently playing media.
- * <p>
- * Some important properties of media players that implement this interface are:
+ *
+ * <p>Some important properties of media players that implement this interface are:
+ *
  * <ul>
- *     <li>They can provide a {@link Timeline} representing the structure of the media being played,
- *     which can be obtained by calling {@link #getCurrentTimeline()}.</li>
- *     <li>They can provide a {@link TrackGroupArray} defining the currently available tracks,
- *     which can be obtained by calling {@link #getCurrentTrackGroups()}.</li>
- *     <li>They contain a number of renderers, each of which is able to render tracks of a single
- *     type (e.g. audio, video or text). The number of renderers and their respective track types
- *     can be obtained by calling {@link #getRendererCount()} and {@link #getRendererType(int)}.
- *     </li>
- *     <li>They can provide a {@link TrackSelectionArray} defining which of the currently available
- *     tracks are selected to be rendered by each renderer. This can be obtained by calling
- *     {@link #getCurrentTrackSelections()}}.</li>
+ *   <li>They can provide a {@link Timeline} representing the structure of the media being played,
+ *       which can be obtained by calling {@link #getCurrentTimeline()}.
+ *   <li>They can provide a {@link TrackGroupArray} defining the currently available tracks, which
+ *       can be obtained by calling {@link #getCurrentTrackGroups()}.
+ *   <li>They contain a number of renderers, each of which is able to render tracks of a single type
+ *       (e.g. audio, video or text). The number of renderers and their respective track types can
+ *       be obtained by calling {@link #getRendererCount()} and {@link #getRendererType(int)}.
+ *   <li>They can provide a {@link TrackSelectionArray} defining which of the currently available
+ *       tracks are selected to be rendered by each renderer. This can be obtained by calling {@link
+ *       #getCurrentTrackSelections()}}.
  * </ul>
  */
 public interface Player {
