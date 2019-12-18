6847fbf49e9d433ff7ec1632b0688d821a94efc5
olly <olly@google.com>
2019-12-18 16:35:43 +0000

    Read arrays directly from Parcel
    PiperOrigin-RevId: 286197990

--- library/core/src/test/java/com/google/android/exoplayer2/metadata/MetadataRendererTest.java
+++ library/core/src/test/java/com/google/android/exoplayer2/metadata/MetadataRendererTest.java
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 package com.google.android.exoplayer2.metadata;
 
