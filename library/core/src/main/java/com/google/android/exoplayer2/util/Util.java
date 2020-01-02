98e7276e3900cfc3ed7233c80ab5304ce89a6f6d
olly <olly@google.com>
2020-01-02 14:44:16 +0000

    Support twos codec in MP4
    Issue: #5789
    PiperOrigin-RevId: 287828559

--- library/core/src/main/java/com/google/android/exoplayer2/util/Util.java
+++ library/core/src/main/java/com/google/android/exoplayer2/util/Util.java
@@ -1355,6 +1355,7 @@ public final class Util {
   public static boolean isEncodingLinearPcm(@C.Encoding int encoding) {
     return encoding == C.ENCODING_PCM_8BIT
         || encoding == C.ENCODING_PCM_16BIT
+        || encoding == C.ENCODING_PCM_16BIT_BIG_ENDIAN
         || encoding == C.ENCODING_PCM_24BIT
         || encoding == C.ENCODING_PCM_32BIT
         || encoding == C.ENCODING_PCM_FLOAT;
@@ -1423,6 +1424,7 @@ public final class Util {
       case C.ENCODING_PCM_8BIT:
         return channelCount;
       case C.ENCODING_PCM_16BIT:
+      case C.ENCODING_PCM_16BIT_BIG_ENDIAN:
         return channelCount * 2;
       case C.ENCODING_PCM_24BIT:
         return channelCount * 3;
