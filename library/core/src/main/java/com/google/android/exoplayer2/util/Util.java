d609c3b6669938ee7c4017d4faeb2ff5a29010c7
tonihei <tonihei@google.com>
2020-01-06 12:54:05 +0000

    Support 5G in network type detection
    PiperOrigin-RevId: 288280500

--- library/core/src/main/java/com/google/android/exoplayer2/util/Util.java
+++ library/core/src/main/java/com/google/android/exoplayer2/util/Util.java
@@ -2099,6 +2099,8 @@ public final class Util {
         return C.NETWORK_TYPE_3G;
       case TelephonyManager.NETWORK_TYPE_LTE:
         return C.NETWORK_TYPE_4G;
+      case TelephonyManager.NETWORK_TYPE_NR:
+        return C.NETWORK_TYPE_5G;
       case TelephonyManager.NETWORK_TYPE_IWLAN:
         return C.NETWORK_TYPE_WIFI;
       case TelephonyManager.NETWORK_TYPE_GSM:
