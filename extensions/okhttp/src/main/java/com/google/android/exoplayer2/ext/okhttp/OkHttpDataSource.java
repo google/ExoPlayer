1e4f8da038ef09192a4e1beb4b0301c3fa848f6b
Fillmore, Christopher <christopher.fillmore@intl.att.com>
2020-01-14 13:48:14 -0500

    Update OkHttpDataSource with response body on bad HTTP status
    This change supplies the response body in OkHttpDataSource to
    InvalidResponseCodeException.
    
    Issue #6853

--- extensions/okhttp/src/main/java/com/google/android/exoplayer2/ext/okhttp/OkHttpDataSource.java
+++ extensions/okhttp/src/main/java/com/google/android/exoplayer2/ext/okhttp/OkHttpDataSource.java
@@ -230,10 +230,19 @@ public class OkHttpDataSource extends BaseDataSource implements HttpDataSource {
 
     // Check for a valid response code.
     if (!response.isSuccessful()) {
+      byte[] errorResponseBody;
+      try {
+        errorResponseBody = Util.toByteArray(responseByteStream);
+      } catch (IOException e) {
+        throw new HttpDataSourceException(
+            "Error reading response body of unsuccessful response " + dataSpec.uri,
+            e, dataSpec, HttpDataSourceException.TYPE_OPEN);
+      }
       Map<String, List<String>> headers = response.headers().toMultimap();
       closeConnectionQuietly();
       InvalidResponseCodeException exception =
-          new InvalidResponseCodeException(responseCode, response.message(), headers, dataSpec);
+          new InvalidResponseCodeException(responseCode, response.message(), headers, dataSpec,
+              errorResponseBody);
       if (responseCode == 416) {
         exception.initCause(new DataSourceException(DataSourceException.POSITION_OUT_OF_RANGE));
       }
