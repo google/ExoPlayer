96c648c2d6d24e4d15e622cd4262e13c31653d55
samrobinson <samrobinson@google.com>
2020-01-15 11:44:31 +0000

    Improve customisability of FakeSampleStream.
    PiperOrigin-RevId: 289829592

--- library/core/src/test/java/com/google/android/exoplayer2/metadata/MetadataRendererTest.java
+++ library/core/src/test/java/com/google/android/exoplayer2/metadata/MetadataRendererTest.java
@@ -27,10 +27,12 @@ import com.google.android.exoplayer2.metadata.emsg.EventMessageEncoder;
 import com.google.android.exoplayer2.metadata.id3.TextInformationFrame;
 import com.google.android.exoplayer2.metadata.scte35.TimeSignalCommand;
 import com.google.android.exoplayer2.testutil.FakeSampleStream;
+import com.google.android.exoplayer2.testutil.FakeSampleStream.FakeSampleStreamItem;
 import com.google.android.exoplayer2.testutil.TestUtil;
 import com.google.android.exoplayer2.util.Assertions;
 import com.google.android.exoplayer2.util.MimeTypes;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
@@ -141,7 +143,11 @@ public class MetadataRendererTest {
     MetadataRenderer renderer = new MetadataRenderer(metadata::add, /* outputLooper= */ null);
     renderer.replaceStream(
         new Format[] {EMSG_FORMAT},
-        new FakeSampleStream(EMSG_FORMAT, /* eventDispatcher= */ null, input),
+        new FakeSampleStream(
+            EMSG_FORMAT,
+            /* eventDispatcher= */ null,
+            Arrays.asList(new FakeSampleStreamItem(input)),
+            0),
         /* offsetUs= */ 0L);
     renderer.render(/* positionUs= */ 0, /* elapsedRealtimeUs= */ 0); // Read the format
     renderer.render(/* positionUs= */ 0, /* elapsedRealtimeUs= */ 0); // Read the data
