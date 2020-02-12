d79ee72d20ea7323feddca8444fa3b16f459a921
kimvde <kimvde@google.com>
2020-02-12 09:22:25 +0000

    Share FLAC extractor tests between both extractors
    PiperOrigin-RevId: 294614958

--- extensions/flac/src/androidTest/java/com/google/android/exoplayer2/ext/flac/FlacExtractorTest.java
+++ extensions/flac/src/androidTest/java/com/google/android/exoplayer2/ext/flac/FlacExtractorTest.java
@@ -36,16 +36,92 @@ public class FlacExtractorTest {
   }
 
   @Test
-  public void testExtractFlacSample() throws Exception {
+  public void testSample() throws Exception {
     ExtractorAsserts.assertBehavior(
-        FlacExtractor::new, "flac/ext-bear.flac", ApplicationProvider.getApplicationContext());
+        FlacExtractor::new,
+        /* file= */ "flac/bear.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_native");
   }
 
   @Test
-  public void testExtractFlacSampleWithId3Header() throws Exception {
+  public void testSampleWithId3HeaderAndId3Enabled() throws Exception {
     ExtractorAsserts.assertBehavior(
         FlacExtractor::new,
-        "flac/ext-bear_with_id3.flac",
-        ApplicationProvider.getApplicationContext());
+        /* file= */ "flac/bear_with_id3.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_with_id3_enabled_native");
+  }
+
+  @Test
+  public void testSampleWithId3HeaderAndId3Disabled() throws Exception {
+    ExtractorAsserts.assertBehavior(
+        () -> new FlacExtractor(FlacExtractor.FLAG_DISABLE_ID3_METADATA),
+        /* file= */ "flac/bear_with_id3.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_with_id3_disabled_native");
+  }
+
+  @Test
+  public void testSampleUnseekable() throws Exception {
+    ExtractorAsserts.assertBehavior(
+        FlacExtractor::new,
+        /* file= */ "flac/bear_no_seek_table_no_num_samples.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_no_seek_table_no_num_samples_native");
+  }
+
+  @Test
+  public void testSampleWithVorbisComments() throws Exception {
+    ExtractorAsserts.assertBehavior(
+        FlacExtractor::new,
+        /* file= */ "flac/bear_with_vorbis_comments.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_with_vorbis_comments_native");
+  }
+
+  @Test
+  public void testSampleWithPicture() throws Exception {
+    ExtractorAsserts.assertBehavior(
+        FlacExtractor::new,
+        /* file= */ "flac/bear_with_picture.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_with_picture_native");
+  }
+
+  @Test
+  public void testOneMetadataBlock() throws Exception {
+    ExtractorAsserts.assertBehavior(
+        FlacExtractor::new,
+        /* file= */ "flac/bear_one_metadata_block.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_one_metadata_block_native");
+  }
+
+  @Test
+  public void testNoMinMaxFrameSize() throws Exception {
+    ExtractorAsserts.assertBehavior(
+        FlacExtractor::new,
+        /* file= */ "flac/bear_no_min_max_frame_size.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_no_min_max_frame_size_native");
+  }
+
+  @Test
+  public void testNoNumSamples() throws Exception {
+    ExtractorAsserts.assertBehavior(
+        FlacExtractor::new,
+        /* file= */ "flac/bear_no_num_samples.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_no_num_samples_native");
+  }
+
+  @Test
+  public void testUncommonSampleRate() throws Exception {
+    ExtractorAsserts.assertBehavior(
+        FlacExtractor::new,
+        /* file= */ "flac/bear_uncommon_sample_rate.flac",
+        ApplicationProvider.getApplicationContext(),
+        /* dumpFilesPrefix= */ "flac/bear_uncommon_sample_rate_native");
   }
 }
