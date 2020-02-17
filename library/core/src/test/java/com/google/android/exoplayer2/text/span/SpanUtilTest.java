2835427b1a430a6c863eddfa8ff1e586e4f08473
ibaker <ibaker@google.com>
2020-02-17 13:57:17 +0000

    Move SpanUtil into text.span package
    Seems like it belongs here.
    
    PiperOrigin-RevId: 295571820

--- library/core/src/test/java/com/google/android/exoplayer2/text/span/SpanUtilTest.java
+++ library/core/src/test/java/com/google/android/exoplayer2/text/span/SpanUtilTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.android.exoplayer2.text;
+package com.google.android.exoplayer2.text.span;
 
 import static com.google.common.truth.Truth.assertThat;
 
