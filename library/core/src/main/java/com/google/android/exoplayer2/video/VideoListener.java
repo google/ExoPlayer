14aa56d62bc8f961bf878ff6e73917e9046d7cac
bachinger <bachinger@google.com>
2020-03-17 21:44:22 +0000

    correct the javadoc of onRenderedFirstFrame in VideoListener
    Issue: #7097
    PiperOrigin-RevId: 301456951

--- library/core/src/main/java/com/google/android/exoplayer2/video/VideoListener.java
+++ library/core/src/main/java/com/google/android/exoplayer2/video/VideoListener.java
@@ -52,7 +52,7 @@ public interface VideoListener {
 
   /**
    * Called when a frame is rendered for the first time since setting the surface, and when a frame
-   * is rendered for the first time since a video track was selected.
+   * is rendered for the first time since the renderer was reset.
    */
   default void onRenderedFirstFrame() {}
 }
