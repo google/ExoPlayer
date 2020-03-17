77d52d16da2156c348e63863ac8ea61aa0a55256
olly <olly@google.com>
2020-03-17 12:05:17 +0000

    Make FfmpegLibrary H264/HEVC aware
    Issue: #2159
    PiperOrigin-RevId: 301351495

--- extensions/ffmpeg/src/main/java/com/google/android/exoplayer2/ext/ffmpeg/FfmpegLibrary.java
+++ extensions/ffmpeg/src/main/java/com/google/android/exoplayer2/ext/ffmpeg/FfmpegLibrary.java
@@ -118,6 +118,10 @@ public final class FfmpegLibrary {
         return "pcm_mulaw";
       case MimeTypes.AUDIO_ALAW:
         return "pcm_alaw";
+      case MimeTypes.VIDEO_H264:
+        return "h264";
+      case MimeTypes.VIDEO_H265:
+        return "hevc";
       default:
         return null;
     }
