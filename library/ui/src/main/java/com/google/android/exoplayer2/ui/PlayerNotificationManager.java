a2896c6cba7d86a5a228d5eb6bb9338c4da9e6b5
jaewan <jaewan@google.com>
2020-03-30 14:59:57 +0100

    Allow developers to specify CharSequence for Notification strings
    CharSequence is used by Notification builders and allows to set Spannable text.
    It's the base interface of the String class, so apps wouldn't be break after
    the update.
    
    PiperOrigin-RevId: 303731890

--- library/ui/src/main/java/com/google/android/exoplayer2/ui/PlayerNotificationManager.java
+++ library/ui/src/main/java/com/google/android/exoplayer2/ui/PlayerNotificationManager.java
@@ -139,7 +139,7 @@ public class PlayerNotificationManager {
      *
      * @param player The {@link Player} for which a notification is being built.
      */
-    String getCurrentContentTitle(Player player);
+    CharSequence getCurrentContentTitle(Player player);
 
     /**
      * Creates a content intent for the current media item.
@@ -159,7 +159,7 @@ public class PlayerNotificationManager {
      * @param player The {@link Player} for which a notification is being built.
      */
     @Nullable
-    String getCurrentContentText(Player player);
+    CharSequence getCurrentContentText(Player player);
 
     /**
      * Gets the content sub text for the current media item.
@@ -169,7 +169,7 @@ public class PlayerNotificationManager {
      * @param player The {@link Player} for which a notification is being built.
      */
     @Nullable
-    default String getCurrentSubText(Player player) {
+    default CharSequence getCurrentSubText(Player player) {
       return null;
     }
 
