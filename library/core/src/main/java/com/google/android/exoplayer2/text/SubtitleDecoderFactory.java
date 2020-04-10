77b20452512e77a81bb21ae068f92ced2d86c994
sneelavara <sneelavara@gmail.com>
2020-04-09 18:45:44 -0700

    Pass the Clock interface into the constructor

--- library/core/src/main/java/com/google/android/exoplayer2/text/SubtitleDecoderFactory.java
+++ library/core/src/main/java/com/google/android/exoplayer2/text/SubtitleDecoderFactory.java
@@ -27,6 +27,7 @@ import com.google.android.exoplayer2.text.ttml.TtmlDecoder;
 import com.google.android.exoplayer2.text.tx3g.Tx3gDecoder;
 import com.google.android.exoplayer2.text.webvtt.Mp4WebvttDecoder;
 import com.google.android.exoplayer2.text.webvtt.WebvttDecoder;
+import com.google.android.exoplayer2.util.Clock;
 import com.google.android.exoplayer2.util.MimeTypes;
 
 /**
@@ -108,7 +109,8 @@ public interface SubtitleDecoderFactory {
                 return new Tx3gDecoder(format.initializationData);
               case MimeTypes.APPLICATION_CEA608:
               case MimeTypes.APPLICATION_MP4CEA608:
-                return new Cea608Decoder(mimeType, format.accessibilityChannel);
+                return new Cea608Decoder(mimeType, format.accessibilityChannel
+                      16000L, Clock.DEFAULT);
               case MimeTypes.APPLICATION_CEA708:
                 return new Cea708Decoder(format.accessibilityChannel, format.initializationData);
               case MimeTypes.APPLICATION_DVBSUBS:
