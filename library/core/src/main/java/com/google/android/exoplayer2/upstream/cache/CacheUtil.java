4c8bade49b5210fdb08728e1f942ed329b20f1ff
olly <olly@google.com>
2020-04-17 17:22:39 +0100

    Better tie injection of Cache/CacheDataSource/CacheKeyFactory
    PiperOrigin-RevId: 307056661

--- library/core/src/main/java/com/google/android/exoplayer2/upstream/cache/CacheUtil.java
+++ library/core/src/main/java/com/google/android/exoplayer2/upstream/cache/CacheUtil.java
@@ -125,8 +125,6 @@ public final class CacheUtil {
       throws IOException, InterruptedException {
     cache(
         dataSpec,
-        cache,
-        /* cacheKeyFactory= */ null,
         new CacheDataSource(cache, upstream),
         new byte[DEFAULT_BUFFER_SIZE_BYTES],
         /* priorityTaskManager= */ null,
@@ -149,9 +147,7 @@ public final class CacheUtil {
    * <p>This method may be slow and shouldn't normally be called on the main thread.
    *
    * @param dataSpec Defines the data to be cached.
-   * @param cache A {@link Cache} to store the data.
-   * @param cacheKeyFactory An optional factory for cache keys.
-   * @param dataSource A {@link CacheDataSource} that works on the {@code cache}.
+   * @param dataSource A {@link CacheDataSource} to be used for caching the data.
    * @param buffer The buffer to be used while caching.
    * @param priorityTaskManager If not null it's used to check whether it is allowed to proceed with
    *     caching.
@@ -166,8 +162,6 @@ public final class CacheUtil {
   @WorkerThread
   public static void cache(
       DataSpec dataSpec,
-      Cache cache,
-      @Nullable CacheKeyFactory cacheKeyFactory,
       CacheDataSource dataSource,
       byte[] buffer,
       @Nullable PriorityTaskManager priorityTaskManager,
@@ -179,6 +173,8 @@ public final class CacheUtil {
     Assertions.checkNotNull(dataSource);
     Assertions.checkNotNull(buffer);
 
+    Cache cache = dataSource.getCache();
+    CacheKeyFactory cacheKeyFactory = dataSource.getCacheKeyFactory();
     String key = buildCacheKey(dataSpec, cacheKeyFactory);
     long bytesLeft;
     @Nullable ProgressNotifier progressNotifier = null;
