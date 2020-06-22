fe9d14bd4763018f412ca8fb187024b5412b0a02
andrewlewis <andrewlewis@google.com>
2020-06-22 08:45:35 +0100

    Remove unnecessary null check
    PiperOrigin-RevId: 317604812

--- library/dash/src/main/java/com/google/android/exoplayer2/source/dash/EventSampleStream.java
+++ library/dash/src/main/java/com/google/android/exoplayer2/source/dash/EventSampleStream.java
@@ -113,15 +113,11 @@ import java.io.IOException;
     }
     int sampleIndex = currentIndex++;
     byte[] serializedEvent = eventMessageEncoder.encode(eventStream.events[sampleIndex]);
-    if (serializedEvent != null) {
-      buffer.ensureSpaceForWrite(serializedEvent.length);
-      buffer.data.put(serializedEvent);
-      buffer.timeUs = eventTimesUs[sampleIndex];
-      buffer.setFlags(C.BUFFER_FLAG_KEY_FRAME);
-      return C.RESULT_BUFFER_READ;
-    } else {
-      return C.RESULT_NOTHING_READ;
-    }
+    buffer.ensureSpaceForWrite(serializedEvent.length);
+    buffer.data.put(serializedEvent);
+    buffer.timeUs = eventTimesUs[sampleIndex];
+    buffer.setFlags(C.BUFFER_FLAG_KEY_FRAME);
+    return C.RESULT_BUFFER_READ;
   }
 
   @Override
