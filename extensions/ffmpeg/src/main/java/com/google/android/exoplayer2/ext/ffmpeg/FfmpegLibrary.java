6c7884436abfc2ca2d72eb0634933204bba18ec6
Oliver Woodman <olly@google.com>
2020-09-07 20:48:28 +0100

    Remove unfinished FfmpegVideoRenderer from release

--- extensions/ffmpeg/src/main/java/com/google/android/exoplayer2/ext/ffmpeg/FfmpegLibrary.java
+++ extensions/ffmpeg/src/main/java/com/google/android/exoplayer2/ext/ffmpeg/FfmpegLibrary.java
@@ -45,7 +45,7 @@ public final class FfmpegLibrary {
   /**
    * Override the names of the FFmpeg native libraries. If an application wishes to call this
    * method, it must do so before calling any other method defined by this class, and before
-   * instantiating a {@link FfmpegAudioRenderer} or {@link FfmpegVideoRenderer} instance.
+   * instantiating a {@link FfmpegAudioRenderer} instance.
    *
    * @param libraries The names of the FFmpeg native libraries.
    */
