7dd4c736c3daa283ca829f9070ce30329524f613
olly <olly@google.com>
2020-09-07 21:17:29 +0100

    Fix extension renderer test names
    PiperOrigin-RevId: 330409635

--- extensions/flac/src/test/java/com/google/android/exoplayer2/ext/flac/DefaultRenderersFactoryTest.java
+++ extensions/flac/src/test/java/com/google/android/exoplayer2/ext/flac/DefaultRenderersFactoryTest.java
@@ -26,7 +26,7 @@ import org.junit.runner.RunWith;
 public final class DefaultRenderersFactoryTest {
 
   @Test
-  public void createRenderers_instantiatesVpxRenderer() {
+  public void createRenderers_instantiatesFlacRenderer() {
     DefaultRenderersFactoryAsserts.assertExtensionRendererCreated(
         LibflacAudioRenderer.class, C.TRACK_TYPE_AUDIO);
   }
