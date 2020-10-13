76b7f764374e8bb0569f20a952c97777f1117cfe
christosts <christosts@google.com>
2020-10-13 11:05:20 +0100

    Allow speed adjustments close to 1f in SonicAudioProcessor
    Issue: #4904
    PiperOrigin-RevId: 336841791

--- library/core/src/main/java/com/google/android/exoplayer2/audio/SonicAudioProcessor.java
+++ library/core/src/main/java/com/google/android/exoplayer2/audio/SonicAudioProcessor.java
@@ -33,7 +33,7 @@ public final class SonicAudioProcessor implements AudioProcessor {
   public static final int SAMPLE_RATE_NO_CHANGE = -1;
 
   /** The threshold below which the difference between two pitch/speed factors is negligible. */
-  private static final float CLOSE_THRESHOLD = 0.01f;
+  private static final float CLOSE_THRESHOLD = 0.0001f;
 
   /**
    * The minimum number of output bytes at which the speedup is calculated using the input/output
