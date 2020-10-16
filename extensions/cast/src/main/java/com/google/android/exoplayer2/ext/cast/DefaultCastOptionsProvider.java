e446f02eb9aa00bb2ea7a0e8ed007ff004772f92
aquilescanta <aquilescanta@google.com>
2020-10-16 17:09:34 +0100

    Remove references to the default Cast receiver
    The ExoPlayer receiver app id is a thin wrapper around the default receiver, so
    there are no reasons for users not to depend on it. By having ExoPlayer users
    refer to the ExoPlayer app id we can collect usage metrics.
    
    PiperOrigin-RevId: 337516767

--- extensions/cast/src/main/java/com/google/android/exoplayer2/ext/cast/DefaultCastOptionsProvider.java
+++ extensions/cast/src/main/java/com/google/android/exoplayer2/ext/cast/DefaultCastOptionsProvider.java
@@ -16,7 +16,6 @@
 package com.google.android.exoplayer2.ext.cast;
 
 import android.content.Context;
-import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
@@ -29,24 +28,12 @@ import java.util.List;
 public final class DefaultCastOptionsProvider implements OptionsProvider {
 
   /**
-   * App id of the Default Media Receiver app. Apps that do not require DRM support may use this
-   * receiver receiver app ID.
+   * App id that points to the Default Media Receiver app with basic DRM support.
    *
-   * <p>See https://developers.google.com/cast/docs/caf_receiver/#default_media_receiver.
+   * <p>Applications that require more complex DRM authentication should <a
+   * href="https://developers.google.com/cast/docs/web_receiver/streaming_protocols#drm">create a
+   * custom receiver application</a>.
    */
-  public static final String APP_ID_DEFAULT_RECEIVER =
-      CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID;
-
-  /**
-   * App id for receiver app with rudimentary support for DRM.
-   *
-   * <p>This app id is only suitable for ExoPlayer's Cast Demo app, and it is not intended for
-   * production use. In order to use DRM, custom receiver apps should be used. For environments that
-   * do not require DRM, the default receiver app should be used (see {@link
-   * #APP_ID_DEFAULT_RECEIVER}).
-   */
-  // TODO: Add a documentation resource link for DRM support in the receiver app [Internal ref:
-  // b/128603245].
   public static final String APP_ID_DEFAULT_RECEIVER_WITH_DRM = "A12D4273";
 
   @Override
