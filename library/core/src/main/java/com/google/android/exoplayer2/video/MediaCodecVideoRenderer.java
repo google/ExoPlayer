7fe68b52d56e2c34f48ecaa6ca7f1419edebe3a1
olly <olly@google.com>
2020-11-13 16:04:08 +0000

    Remove stray arguments from method
    PiperOrigin-RevId: 342263700

--- library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
@@ -815,8 +815,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
 
     boolean treatDroppedBuffersAsSkipped = joiningDeadlineMs != C.TIME_UNSET;
     if (shouldDropBuffersToKeyframe(earlyUs, elapsedRealtimeUs, isLastBuffer)
-        && maybeDropBuffersToKeyframe(
-            codec, bufferIndex, presentationTimeUs, positionUs, treatDroppedBuffersAsSkipped)) {
+        && maybeDropBuffersToKeyframe(positionUs, treatDroppedBuffersAsSkipped)) {
       return false;
     } else if (shouldDropOutputBuffer(earlyUs, elapsedRealtimeUs, isLastBuffer)) {
       if (treatDroppedBuffersAsSkipped) {
@@ -974,9 +973,6 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
    * position. If no such keyframe exists, as the playback position is inside the same group of
    * pictures as the buffer being processed, returns {@code false}. Returns {@code true} otherwise.
    *
-   * @param codec The codec that owns the output buffer.
-   * @param index The index of the output buffer to drop.
-   * @param presentationTimeUs The presentation time of the output buffer, in microseconds.
    * @param positionUs The current playback position, in microseconds.
    * @param treatDroppedBuffersAsSkipped Whether dropped buffers should be treated as intentionally
    *     skipped.
@@ -984,9 +980,6 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
    * @throws ExoPlaybackException If an error occurs flushing the codec.
    */
   protected boolean maybeDropBuffersToKeyframe(
-      MediaCodecAdapter codec,
-      int index,
-      long presentationTimeUs,
       long positionUs,
       boolean treatDroppedBuffersAsSkipped)
       throws ExoPlaybackException {
