93b3f43ed2f9633c37118330d530dcd68f9b0704
krocard <krocard@google.com>
2020-12-07 21:09:38 +0000

    Fix some extractor nullness checks
    Fix Matroska, Heif, FLAC, Ogg, Opus, Vorbis
    extractor nullness check.
    
    There should be no functional change.
    Every media that fail to be parsed should still fail.
    Every media that parsed successfully should still succeed.
    
    This refactor aims to push all nullness constraints up the call stack to clarify each API nullness contract. This ensures implementation and caller have to prove their respective contract close to where such logic is implemented. This also allows to fail early if an nullness contract is broken instead of deep in the call stack.
    
    For example, by adding a requirement that all implementation of `StreamReader.readHeaders` have to initialize `setupData.format` if the return false, each overriding method is forced to prove this next to the logic initializing it. This also means the runtime check might not be needed because the nullnessChecker can prove itself the contract holds.
    
    This is in contrast with adding a null check at the point of usage, which will not catch logic errors where they are produce, but later when they are perceived; making it harder to debug and catching the issue at run time instead of compile time.
    
    #exofixit
    
    PiperOrigin-RevId: 346163124

--- library/extractor/src/main/java/com/google/android/exoplayer2/extractor/ogg/VorbisReader.java
+++ library/extractor/src/main/java/com/google/android/exoplayer2/extractor/ogg/VorbisReader.java
@@ -15,6 +15,9 @@
  */
 package com.google.android.exoplayer2.extractor.ogg;
 
+import static com.google.android.exoplayer2.util.Assertions.checkNotNull;
+import static com.google.android.exoplayer2.util.Assertions.checkStateNotNull;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import com.google.android.exoplayer2.Format;
@@ -26,6 +29,7 @@ import com.google.android.exoplayer2.util.ParsableByteArray;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import org.checkerframework.checker.nullness.qual.EnsuresNonNullIf;
 
 /**
  * {@link StreamReader} to extract Vorbis data out of Ogg byte stream.
@@ -74,7 +78,7 @@ import java.util.Arrays;
     }
 
     // ... we need to decode the block size
-    int packetBlockSize = decodeBlockSize(packet.getData()[0], vorbisSetup);
+    int packetBlockSize = decodeBlockSize(packet.getData()[0], checkStateNotNull(vorbisSetup));
     // a packet contains samples produced from overlapping the previous and current frame data
     // (https://www.xiph.org/vorbis/doc/Vorbis_I_spec.html#x1-350001.3.2)
     int samplesInPacket = seenFirstAudioPacket ? (packetBlockSize + previousPacketBlockSize) / 4
@@ -89,9 +93,11 @@ import java.util.Arrays;
   }
 
   @Override
+  @EnsuresNonNullIf(expression = "#3.format", result = false)
   protected boolean readHeaders(ParsableByteArray packet, long position, SetupData setupData)
       throws IOException {
     if (vorbisSetup != null) {
+      checkNotNull(setupData.format);
       return false;
     }
 
@@ -99,6 +105,7 @@ import java.util.Arrays;
     if (vorbisSetup == null) {
       return true;
     }
+    VorbisSetup vorbisSetup = this.vorbisSetup;
 
     VorbisUtil.VorbisIdHeader idHeader = vorbisSetup.idHeader;
 
@@ -131,6 +138,8 @@ import java.util.Arrays;
       commentHeader = VorbisUtil.readVorbisCommentHeader(scratch);
       return null;
     }
+    VorbisUtil.VorbisIdHeader vorbisIdHeader = this.vorbisIdHeader;
+    VorbisUtil.CommentHeader commentHeader = this.commentHeader;
 
     // the third packet contains the setup header
     byte[] setupHeaderData = new byte[scratch.limit()];
