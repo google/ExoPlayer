afcd74144746059dbc5e377bd62841cbefeb7d66
Ian Baker <ibaker@google.com>
2021-01-25 17:58:10 +0000

    Remove FfmpegVideoRenderer from 2.13.0 release
    This component isn't ready to be released yet.

--- extensions/ffmpeg/src/main/java/com/google/android/exoplayer2/ext/ffmpeg/FfmpegLibrary.java
+++ extensions/ffmpeg/src/main/java/com/google/android/exoplayer2/ext/ffmpeg/FfmpegLibrary.java
@@ -44,7 +44,7 @@ public final class FfmpegLibrary {
   /**
    * Override the names of the FFmpeg native libraries. If an application wishes to call this
    * method, it must do so before calling any other method defined by this class, and before
-   * instantiating a {@link FfmpegAudioRenderer} or {@link FfmpegVideoRenderer} instance.
+   * instantiating a {@link FfmpegAudioRenderer} instance.
    *
    * @param libraries The names of the FFmpeg native libraries.
    */
@@ -144,10 +144,6 @@ public final class FfmpegLibrary {
         return "pcm_mulaw";
       case MimeTypes.AUDIO_ALAW:
         return "pcm_alaw";
-      case MimeTypes.VIDEO_H264:
-        return "h264";
-      case MimeTypes.VIDEO_H265:
-        return "hevc";
       default:
         return null;
     }
