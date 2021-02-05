01b6061bd2f9b4c45781db41e684bdcfe6c459b3
olly <olly@google.com>
2021-02-05 19:50:37 +0000

    Wire CodecException.isRecoverable to ExoPlaybackException
    PiperOrigin-RevId: 355896218

--- library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecRenderer.java
@@ -359,6 +359,7 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
   private boolean enableAsynchronousBufferQueueing;
   private boolean forceAsyncQueueingSynchronizationWorkaround;
   private boolean enableSynchronizeCodecInteractionsWithQueueing;
+  private boolean enableRecoverableCodecExceptionRetries;
   @Nullable private ExoPlaybackException pendingPlaybackException;
   protected DecoderCounters decoderCounters;
   private long outputStreamStartPositionUs;
@@ -463,6 +464,17 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
    */
   public void experimentalSetSynchronizeCodecInteractionsWithQueueingEnabled(boolean enabled) {
     enableSynchronizeCodecInteractionsWithQueueing = enabled;
+  }
+
+  /**
+   * Enable internal player retries for codec exceptions if the underlying platform indicates that
+   * they are recoverable.
+   *
+   * <p>This method is experimental, and will be renamed or removed in a future release. It should
+   * only be called before the renderer is used.
+   */
+  public void experimentalSetRecoverableCodecExceptionRetriesEnabled(boolean enabled) {
+    enableRecoverableCodecExceptionRetries = enabled;
   }
 
   @Override
@@ -836,7 +848,15 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
       decoderCounters.ensureUpdated();
     } catch (IllegalStateException e) {
       if (isMediaCodecException(e)) {
-        throw createRendererException(createDecoderException(e, getCodecInfo()), inputFormat);
+        boolean isRecoverable =
+            enableRecoverableCodecExceptionRetries
+                && Util.SDK_INT >= 21
+                && isRecoverableMediaCodecExceptionV21(e);
+        if (isRecoverable) {
+          releaseCodec();
+        }
+        throw createRendererException(
+            createDecoderException(e, getCodecInfo()), inputFormat, isRecoverable);
       }
       throw e;
     }
@@ -2261,6 +2281,14 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
   @RequiresApi(21)
   private static boolean isMediaCodecExceptionV21(IllegalStateException error) {
     return error instanceof MediaCodec.CodecException;
+  }
+
+  @RequiresApi(21)
+  private static boolean isRecoverableMediaCodecExceptionV21(IllegalStateException error) {
+    if (error instanceof MediaCodec.CodecException) {
+      return ((MediaCodec.CodecException) error).isRecoverable();
+    }
+    return false;
   }
 
   /**
