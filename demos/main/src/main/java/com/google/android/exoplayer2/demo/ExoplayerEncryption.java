5de05968ec454fb20f7d1c056542cb41cf720d58
nathan <nathans@scribd.com>
2021-03-03 16:23:17 -0800

    made toggle to turn on / off encryption

--- demos/main/src/main/java/com/google/android/exoplayer2/demo/ExoplayerEncryption.java
+++ demos/main/src/main/java/com/google/android/exoplayer2/demo/ExoplayerEncryption.java
@@ -10,14 +10,30 @@ import com.google.android.exoplayer2.util.Util;
 
 public class ExoplayerEncryption {
 
+  // Toggle this variable to enable / disable decryption. You may need to uninstall app after toggling
+  final private static boolean enableEncryption = true;
+
   private static final byte[] KEY = Util.getUtf8Bytes("testKey:12345678");
 
   public static DataSink.Factory dataSinkFactory(Cache downloadCache) {
-    return () -> new AesCipherDataSink(KEY, new CacheDataSink(downloadCache, Long.MAX_VALUE),
-        new byte[1024 * 32]);
+    return () -> {
+      if (enableEncryption) {
+        return new AesCipherDataSink(KEY, new CacheDataSink(downloadCache, Long.MAX_VALUE),
+            new byte[1024 * 32]);
+      } else {
+        return new CacheDataSink(downloadCache, Long.MAX_VALUE);
+      }
+    };
+
   }
 
   public static DataSource.Factory dataSourceFactory(DataSource.Factory upstream) {
-    return () -> new AesCipherDataSource(KEY, upstream.createDataSource());
+    return () -> {
+      if (enableEncryption) {
+        return new AesCipherDataSource(KEY, upstream.createDataSource());
+      } else {
+        return upstream.createDataSource();
+      }
+    };
   }
 }
