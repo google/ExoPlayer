8c0cc38d8baea00f9604438c5a81df71daf1150a
sediger <shemiah@enjoymoviesyourway.com>
2021-06-08 18:02:53 -0700

    Added option to ImaAdsLoader to skip all ads in group

--- extensions/ima/src/main/java/com/google/android/exoplayer2/ext/ima/ImaUtil.java
+++ extensions/ima/src/main/java/com/google/android/exoplayer2/ext/ima/ImaUtil.java
@@ -88,6 +88,7 @@ import java.util.Set;
     public final int mediaLoadTimeoutMs;
     public final boolean focusSkipButtonWhenAvailable;
     public final boolean playAdBeforeStartPosition;
+    public final boolean skipAdGroupOnSkipAd;
     public final int mediaBitrate;
     @Nullable public final Boolean enableContinuousPlayback;
     @Nullable public final List<String> adMediaMimeTypes;
@@ -105,6 +106,7 @@ import java.util.Set;
         int mediaLoadTimeoutMs,
         boolean focusSkipButtonWhenAvailable,
         boolean playAdBeforeStartPosition,
+        boolean skipAdGroupOnSkipAd,
         int mediaBitrate,
         @Nullable Boolean enableContinuousPlayback,
         @Nullable List<String> adMediaMimeTypes,
@@ -120,6 +122,7 @@ import java.util.Set;
       this.mediaLoadTimeoutMs = mediaLoadTimeoutMs;
       this.focusSkipButtonWhenAvailable = focusSkipButtonWhenAvailable;
       this.playAdBeforeStartPosition = playAdBeforeStartPosition;
+      this.skipAdGroupOnSkipAd = skipAdGroupOnSkipAd;
       this.mediaBitrate = mediaBitrate;
       this.enableContinuousPlayback = enableContinuousPlayback;
       this.adMediaMimeTypes = adMediaMimeTypes;
