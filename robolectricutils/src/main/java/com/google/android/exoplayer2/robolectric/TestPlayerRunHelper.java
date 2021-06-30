google/ExoPlayer

commit a4ad351fb15b60080ff53b345563e3f922464251

Enable offload tests
Add additional assertions to try to figure out why
the tests are flaky when run in the test harness.

Failure could not be reproduced locally even after 4000 run.

PiperOrigin-RevId: 362282251

robolectricutils/src/main/java/com/google/android/exoplayer2/robolectric/TestPlayerRunHelper.java
@@ -17,6 +17,7 @@
 package com.google.android.exoplayer2.robolectric;
 
 import static com.google.android.exoplayer2.robolectric.RobolectricUtil.runMainLooperUntil;
+import static com.google.common.truth.Truth.assertThat;
 
 import android.os.Looper;
 import com.google.android.exoplayer2.ExoPlaybackException;
@@ -257,7 +258,13 @@ public class TestPlayerRunHelper {
           }
         };
     player.addListener(listener);
-    runMainLooperUntil(receiverCallback::get);
+    runMainLooperUntil(
+        () -> { // Make sure progress is being made, see [internal: b/170387438#comment2]
+          assertThat(player.getPlayerError()).isNull();
+          assertThat(player.getPlayWhenReady()).isTrue();
+          assertThat(player.getPlaybackState()).isAnyOf(Player.STATE_BUFFERING, Player.STATE_READY);
+          return receiverCallback.get();
+        });
   }
 
   /**

library/core/src/test/java/com/google/android/exoplayer2/ExoPlayerTest.java
@@ -128,7 +128,6 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -8574,7 +8573,6 @@ public final class ExoPlayerTest {
     assertThat(runUntilReceiveOffloadSchedulingEnabledNewState(player)).isFalse();
   }
 
-  @Ignore // See [internal: b/170387438]
   @Test
   public void enableOffloadSchedulingWhileSleepingForOffload_isDisabled_isReported()
       throws Exception {
@@ -8592,7 +8590,6 @@ public final class ExoPlayerTest {
     assertThat(runUntilReceiveOffloadSchedulingEnabledNewState(player)).isFalse();
   }
 
-  @Ignore // See [internal: b/170387438]
   @Test
   public void enableOffloadScheduling_isEnable_playerSleeps() throws Exception {
     FakeSleepRenderer sleepRenderer = new FakeSleepRenderer(C.TRACK_TYPE_AUDIO);
@@ -8609,7 +8606,6 @@ public final class ExoPlayerTest {
     assertThat(player.experimentalIsSleepingForOffload()).isTrue();
   }
 
-  @Ignore // See [internal: b/170387438]
   @Test
   public void
       experimentalEnableOffloadSchedulingWhileSleepingForOffload_isDisabled_renderingResumes()
@@ -8630,7 +8626,6 @@ public final class ExoPlayerTest {
     runUntilPlaybackState(player, Player.STATE_ENDED);
   }
 
-  @Ignore // See [internal: b/170387438]
   @Test
   public void wakeupListenerWhileSleepingForOffload_isWokenUp_renderingResumes() throws Exception {
     FakeSleepRenderer sleepRenderer = new FakeSleepRenderer(C.TRACK_TYPE_AUDIO).sleepOnNextRender();

commit 5e2197d82e5076183732368d9385ab6f71d73e29

Remove extra test checks intended to debug flaky test.
Those test are no longer flaky since the test clock
was made deterministic.

The removed assertions were introduced in https://github.com/google/ExoPlayer/commit/a4ad351fb15b60080ff53b345563e3f922464251.

PiperOrigin-RevId: 380793961

robolectricutils/src/main/java/com/google/android/exoplayer2/robolectric/TestPlayerRunHelper.java
@@ -17,7 +17,6 @@
 package com.google.android.exoplayer2.robolectric;
 
 import static com.google.android.exoplayer2.robolectric.RobolectricUtil.runMainLooperUntil;
-import static com.google.common.truth.Truth.assertThat;
 
 import android.os.Looper;
 import com.google.android.exoplayer2.ExoPlaybackException;
@@ -263,13 +262,7 @@ public class TestPlayerRunHelper {
           }
         };
     player.addAudioOffloadListener(listener);
-    runMainLooperUntil(
-        () -> { // Make sure progress is being made, see [internal: b/170387438#comment2]
-          assertThat(player.getPlayerError()).isNull();
-          assertThat(player.getPlayWhenReady()).isTrue();
-          assertThat(player.getPlaybackState()).isAnyOf(Player.STATE_BUFFERING, Player.STATE_READY);
-          return receiverCallback.get();
-        });
+    runMainLooperUntil(receiverCallback::get);
   }
 
   /**