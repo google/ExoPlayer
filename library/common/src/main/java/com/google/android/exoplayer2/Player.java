17c6092335f8abe264298f4edc2ae13f141bec3d
ibaker <ibaker@google.com>
2021-07-02 18:17:37 +0100

    Clarify the thread requirements of a SurfaceView or SurfaceHolder
    Issue: #9005
    PiperOrigin-RevId: 382765045

--- library/common/src/main/java/com/google/android/exoplayer2/Player.java
+++ library/common/src/main/java/com/google/android/exoplayer2/Player.java
@@ -1761,6 +1761,9 @@ public interface Player {
    * Sets the {@link SurfaceHolder} that holds the {@link Surface} onto which video will be
    * rendered. The player will track the lifecycle of the surface automatically.
    *
+   * <p>The thread that calls the {@link SurfaceHolder.Callback} methods must be the thread
+   * associated with {@link #getApplicationLooper()}.
+   *
    * @param surfaceHolder The surface holder.
    */
   void setVideoSurfaceHolder(@Nullable SurfaceHolder surfaceHolder);
@@ -1777,6 +1780,9 @@ public interface Player {
    * Sets the {@link SurfaceView} onto which video will be rendered. The player will track the
    * lifecycle of the surface automatically.
    *
+   * <p>The thread that calls the {@link SurfaceHolder.Callback} methods must be the thread
+   * associated with {@link #getApplicationLooper()}.
+   *
    * @param surfaceView The surface view.
    */
   void setVideoSurfaceView(@Nullable SurfaceView surfaceView);
@@ -1792,6 +1798,9 @@ public interface Player {
   /**
    * Sets the {@link TextureView} onto which video will be rendered. The player will track the
    * lifecycle of the surface automatically.
+   *
+   * <p>The thread that calls the {@link TextureView.SurfaceTextureListener} methods must be the
+   * thread associated with {@link #getApplicationLooper()}.
    *
    * @param textureView The texture view.
    */
