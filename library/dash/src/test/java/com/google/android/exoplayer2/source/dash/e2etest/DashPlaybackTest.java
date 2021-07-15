7157f3c1190290ed65500aabf9987a5e70828930
olly <olly@google.com>
2021-07-15 14:24:27 +0100

    Move E2E DASH test into DASH package
    PiperOrigin-RevId: 384912341

--- library/dash/src/test/java/com/google/android/exoplayer2/source/dash/e2etest/DashPlaybackTest.java
+++ library/dash/src/test/java/com/google/android/exoplayer2/source/dash/e2etest/DashPlaybackTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.android.exoplayer2.e2etest;
+package com.google.android.exoplayer2.source.dash.e2etest;
 
 import static com.google.android.exoplayer2.util.Assertions.checkNotNull;
 
