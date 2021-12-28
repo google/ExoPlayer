b28f5ca4131f141448d53effe2305c323f625fdc
Paul Woitaschek <woitaschek@posteo.de>
2021-12-28 22:54:19 +0100

    Refactor unintuitive lateinit usage

--- demos/session/src/main/java/androidx/media3/demo/session/PlaybackService.kt
+++ demos/session/src/main/java/androidx/media3/demo/session/PlaybackService.kt
@@ -87,11 +87,10 @@ class PlaybackService : MediaLibraryService() {
       // Only accept query with pattern "play [Title]" or "[Title]"
       // Where [Title]: must be exactly matched
       // If no media with exact name found, play a random media instead
-      lateinit var mediaTitle: String
-      if (query.lowercase().startsWith("play ")) {
-        mediaTitle = query.subSequence(5, query.length).toString()
+      val mediaTitle = if (query.startsWith("play ", ignoreCase = true)) {
+        query.drop(5)
       } else {
-        mediaTitle = query
+        query
       }
 
       val item = MediaItemTree.getItemFromTitle(mediaTitle) ?: MediaItemTree.getRandomItem()
