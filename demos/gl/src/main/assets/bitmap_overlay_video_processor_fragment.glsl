308eaf55c653b849c0402badc5e6b04bd3a324db
huangdarwin <huangdarwin@google.com>
2022-01-14 15:01:20 +0000

    GL: Update BitmapOverlayVideoProcessor naming conventions.
    To be more readable and consistent with Transformer GL.
    
    Tested by running gl-demo with no crash.
    
    PiperOrigin-RevId: 421815519

--- demos/gl/src/main/assets/bitmap_overlay_video_processor_fragment.glsl
+++ demos/gl/src/main/assets/bitmap_overlay_video_processor_fragment.glsl
@@ -15,19 +15,19 @@
 #extension GL_OES_EGL_image_external : require
 precision mediump float;
 // External texture containing video decoder output.
-uniform samplerExternalOES tex_sampler_0;
+uniform samplerExternalOES uTexSampler0;
 // Texture containing the overlap bitmap.
-uniform sampler2D tex_sampler_1;
+uniform sampler2D uTexSampler1;
 // Horizontal scaling factor for the overlap bitmap.
-uniform float scaleX;
+uniform float uScaleX;
 // Vertical scaling factory for the overlap bitmap.
-uniform float scaleY;
-varying vec2 v_texcoord;
+uniform float uScaleY;
+varying vec2 vTexCoords;
 void main() {
-  vec4 videoColor = texture2D(tex_sampler_0, v_texcoord);
-  vec4 overlayColor = texture2D(tex_sampler_1,
-                                vec2(v_texcoord.x * scaleX,
-                                     v_texcoord.y * scaleY));
+  vec4 videoColor = texture2D(uTexSampler0, vTexCoords);
+  vec4 overlayColor = texture2D(uTexSampler1,
+                                vec2(vTexCoords.x * uScaleX,
+                                     vTexCoords.y * uScaleY));
   // Blend the video decoder output and the overlay bitmap.
   gl_FragColor = videoColor * (1.0 - overlayColor.a)
       + overlayColor * overlayColor.a;
