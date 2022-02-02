d3b46f1c701106502391b424bb4b2fa561a669cf
claincly <claincly@google.com>
2022-02-02 14:46:37 +0000

    Use constructor in default constructor.
    PiperOrigin-RevId: 425871885

--- library/transformer/src/main/java/com/google/android/exoplayer2/transformer/DefaultEncoderFactory.java
+++ library/transformer/src/main/java/com/google/android/exoplayer2/transformer/DefaultEncoderFactory.java
@@ -50,7 +50,7 @@ public final class DefaultEncoderFactory implements Codec.EncoderFactory {
 
   /** Creates a new instance using the {@link EncoderSelector#DEFAULT default encoder selector}. */
   public DefaultEncoderFactory() {
-    this.videoEncoderSelector = EncoderSelector.DEFAULT;
+    this(EncoderSelector.DEFAULT);
   }
 
   /** Creates a new instance. */
