363c75b69c165a4640eeddd0d12d394a31d35067
ibaker <ibaker@google.com>
2022-02-02 12:05:07 +0000

    Move DAI MediaSource.Factory from constructor parameter to setter
    This allows the same DefaultMediaSourceFactory instance to be used as
    the contentMediaSourceFactory inside
    ImaServerSideAdInsertionMediaSource.
    
    PiperOrigin-RevId: 425846609

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/source/DefaultMediaSourceFactory.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/source/DefaultMediaSourceFactory.java
@@ -106,7 +106,7 @@ public final class DefaultMediaSourceFactory implements MediaSourceFactory {
   private final DataSource.Factory dataSourceFactory;
   private final DelegateFactoryLoader delegateFactoryLoader;
 
-  @Nullable private final MediaSource.Factory serverSideDaiMediaSourceFactory;
+  @Nullable private MediaSource.Factory imaServerSideAdInsertionMediaSourceFactory;
   @Nullable private AdsLoader.Provider adsLoaderProvider;
   @Nullable private AdViewProvider adViewProvider;
   @Nullable private LoadErrorHandlingPolicy loadErrorHandlingPolicy;
@@ -135,10 +135,7 @@ public final class DefaultMediaSourceFactory implements MediaSourceFactory {
    */
   @UnstableApi
   public DefaultMediaSourceFactory(Context context, ExtractorsFactory extractorsFactory) {
-    this(
-        new DefaultDataSource.Factory(context),
-        extractorsFactory,
-        /* serverSideDaiMediaSourceFactory= */ null);
+    this(new DefaultDataSource.Factory(context), extractorsFactory);
   }
 
   /**
@@ -149,10 +146,7 @@ public final class DefaultMediaSourceFactory implements MediaSourceFactory {
    */
   @UnstableApi
   public DefaultMediaSourceFactory(DataSource.Factory dataSourceFactory) {
-    this(
-        dataSourceFactory,
-        new DefaultExtractorsFactory(),
-        /* serverSideDaiMediaSourceFactory= */ null);
+    this(dataSourceFactory, new DefaultExtractorsFactory());
   }
 
   /**
@@ -162,18 +156,11 @@ public final class DefaultMediaSourceFactory implements MediaSourceFactory {
    *     for requesting media data.
    * @param extractorsFactory An {@link ExtractorsFactory} used to extract progressive media from
    *     its container.
-   * @param serverSideDaiMediaSourceFactory A {@link MediaSource.Factory} for creating server side
-   *     inserted ad media sources.
    */
   @UnstableApi
   public DefaultMediaSourceFactory(
-      DataSource.Factory dataSourceFactory,
-      ExtractorsFactory extractorsFactory,
-      @Nullable MediaSource.Factory serverSideDaiMediaSourceFactory) {
+      DataSource.Factory dataSourceFactory, ExtractorsFactory extractorsFactory) {
     this.dataSourceFactory = dataSourceFactory;
-    // Temporary until factory registration is agreed upon.
-    this.serverSideDaiMediaSourceFactory = serverSideDaiMediaSourceFactory;
-
     delegateFactoryLoader = new DelegateFactoryLoader(dataSourceFactory, extractorsFactory);
     liveTargetOffsetMs = C.TIME_UNSET;
     liveMinOffsetMs = C.TIME_UNSET;
@@ -221,6 +208,26 @@ public final class DefaultMediaSourceFactory implements MediaSourceFactory {
    */
   public DefaultMediaSourceFactory setAdViewProvider(@Nullable AdViewProvider adViewProvider) {
     this.adViewProvider = adViewProvider;
+    return this;
+  }
+
+  /**
+   * Sets the {@link MediaSource.Factory} used to handle {@link MediaItem} instances containing <a
+   * href="https://support.google.com/admanager/answer/6147120">IMA Dynamic Ad Insertion URIs</a>.
+   *
+   * <p>In most cases this will be an {@code ImaServerSideAdInsertionMediaSource.Factory} from the
+   * IMA extension.
+   *
+   * <p>IMA DAI URIs are those with a scheme of {@code "imadai"}.
+   *
+   * @param imaServerSideAdInsertionMediaSourceFactory The {@link MediaSource.Factory} for IMA DAI
+   *     content, or {@code null} to remove a previously set {@link MediaSource.Factory}.
+   * @return This factory, for convenience.
+   */
+  @UnstableApi
+  public DefaultMediaSourceFactory setImaServerSideAdInsertionMediaSourceFactory(
+      @Nullable MediaSource.Factory imaServerSideAdInsertionMediaSourceFactory) {
+    this.imaServerSideAdInsertionMediaSourceFactory = imaServerSideAdInsertionMediaSourceFactory;
     return this;
   }
 
@@ -318,7 +325,7 @@ public final class DefaultMediaSourceFactory implements MediaSourceFactory {
     Assertions.checkNotNull(mediaItem.localConfiguration);
     @Nullable String scheme = mediaItem.localConfiguration.uri.getScheme();
     if (scheme != null && scheme.equals("imadai")) {
-      return checkNotNull(serverSideDaiMediaSourceFactory).createMediaSource(mediaItem);
+      return checkNotNull(imaServerSideAdInsertionMediaSourceFactory).createMediaSource(mediaItem);
     }
     @C.ContentType
     int type =
