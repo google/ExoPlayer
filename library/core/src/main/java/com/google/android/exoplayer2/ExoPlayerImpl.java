1103fee093a5e51b1dc4957a9fbee946c37cb78a
olly <olly@google.com>
2022-02-07 21:04:05 +0000

    Revert of b27349385b61efa3576bb78b59cb07c93fc3a167
    PiperOrigin-RevId: 426994559

--- library/core/src/main/java/com/google/android/exoplayer2/ExoPlayerImpl.java
+++ library/core/src/main/java/com/google/android/exoplayer2/ExoPlayerImpl.java
@@ -2771,7 +2771,6 @@ import java.util.concurrent.TimeoutException;
     @Override
     public void onCues(List<Cue> cues) {
       currentCues = cues;
-      analyticsCollector.onCues(cues);
       // TODO(internal b/187152483): Events should be dispatched via ListenerSet
       for (Listener listeners : listenerArraySet) {
         listeners.onCues(cues);
@@ -2880,7 +2879,6 @@ import java.util.concurrent.TimeoutException;
       DeviceInfo deviceInfo = createDeviceInfo(streamVolumeManager);
       if (!deviceInfo.equals(ExoPlayerImpl.this.deviceInfo)) {
         ExoPlayerImpl.this.deviceInfo = deviceInfo;
-        analyticsCollector.onDeviceInfoChanged(deviceInfo);
         // TODO(internal b/187152483): Events should be dispatched via ListenerSet
         for (Listener listener : listenerArraySet) {
           listener.onDeviceInfoChanged(deviceInfo);
@@ -2890,7 +2888,6 @@ import java.util.concurrent.TimeoutException;
 
     @Override
     public void onStreamVolumeChanged(int streamVolume, boolean streamMuted) {
-      analyticsCollector.onDeviceVolumeChanged(streamVolume, streamMuted);
       // TODO(internal b/187152483): Events should be dispatched via ListenerSet
       for (Listener listener : listenerArraySet) {
         listener.onDeviceVolumeChanged(streamVolume, streamMuted);
