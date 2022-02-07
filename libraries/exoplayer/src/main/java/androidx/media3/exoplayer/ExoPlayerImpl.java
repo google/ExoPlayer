a7e49d31f888709a4e4617bdf75fe6692e34272f
olly <olly@google.com>
2022-02-07 13:32:56 +0000

    Add missing calls to AnalyticsCollector
    PiperOrigin-RevId: 426892085

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayerImpl.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayerImpl.java
@@ -2781,6 +2781,7 @@ import java.util.concurrent.TimeoutException;
     @Override
     public void onCues(List<Cue> cues) {
       currentCues = cues;
+      analyticsCollector.onCues(cues);
       // TODO(internal b/187152483): Events should be dispatched via ListenerSet
       for (Listener listeners : listenerArraySet) {
         listeners.onCues(cues);
@@ -2889,6 +2890,7 @@ import java.util.concurrent.TimeoutException;
       DeviceInfo deviceInfo = createDeviceInfo(streamVolumeManager);
       if (!deviceInfo.equals(ExoPlayerImpl.this.deviceInfo)) {
         ExoPlayerImpl.this.deviceInfo = deviceInfo;
+        analyticsCollector.onDeviceInfoChanged(deviceInfo);
         // TODO(internal b/187152483): Events should be dispatched via ListenerSet
         for (Listener listener : listenerArraySet) {
           listener.onDeviceInfoChanged(deviceInfo);
@@ -2898,6 +2900,7 @@ import java.util.concurrent.TimeoutException;
 
     @Override
     public void onStreamVolumeChanged(int streamVolume, boolean streamMuted) {
+      analyticsCollector.onDeviceVolumeChanged(streamVolume, streamMuted);
       // TODO(internal b/187152483): Events should be dispatched via ListenerSet
       for (Listener listener : listenerArraySet) {
         listener.onDeviceVolumeChanged(streamVolume, streamMuted);
