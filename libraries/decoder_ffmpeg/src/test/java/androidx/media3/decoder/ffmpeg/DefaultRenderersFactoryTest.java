170c8d2f5ee93a586bac54196af360d406ccac35
Ian Baker <ibaker@google.com>
2022-02-18 17:12:59 +0000

    Remove FfmpegVideoRenderer from the 1.0 release branch
    This class is not ready to be released, and only exists in the main
    branch.

--- libraries/decoder_ffmpeg/src/test/java/androidx/media3/decoder/ffmpeg/DefaultRenderersFactoryTest.java
+++ libraries/decoder_ffmpeg/src/test/java/androidx/media3/decoder/ffmpeg/DefaultRenderersFactoryTest.java
@@ -22,8 +22,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * Unit test for {@link DefaultRenderersFactoryTest} with {@link FfmpegAudioRenderer} and {@link
- * FfmpegVideoRenderer}.
+ * Unit test for {@link DefaultRenderersFactoryTest} with {@link FfmpegAudioRenderer}.
  */
 @RunWith(AndroidJUnit4.class)
 public final class DefaultRenderersFactoryTest {
@@ -32,11 +31,5 @@ public final class DefaultRenderersFactoryTest {
   public void createRenderers_instantiatesFfmpegAudioRenderer() {
     DefaultRenderersFactoryAsserts.assertExtensionRendererCreated(
         FfmpegAudioRenderer.class, C.TRACK_TYPE_AUDIO);
-  }
-
-  @Test
-  public void createRenderers_instantiatesFfmpegVideoRenderer() {
-    DefaultRenderersFactoryAsserts.assertExtensionRendererCreated(
-        FfmpegVideoRenderer.class, C.TRACK_TYPE_VIDEO);
   }
 }
