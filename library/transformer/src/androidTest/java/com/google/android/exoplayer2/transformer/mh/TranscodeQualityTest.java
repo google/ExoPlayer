85ce0bc181626eff689ba9c84b90fc17b6642b32
claincly <claincly@google.com>
2022-02-25 16:16:09 +0000

    Add SSIM helper for transcoding quality measurements.
    We use SSIM to measure the transcoding quality between. SSIM is a widely used
    tool that compares the luma channel between two images, and generates a score
    from 0 to 1 that indicates "how similar" the two images are.
    
    In `SsimHelper`, we decode the two videos, extract matching frames and
    calculates the mean SSIM (SSIM averaged all matching frames) for both videos.
    Matching frames are referred to as "comparisonFrame" in the CL, which is
    selected based on the frame number and a user-set comparison interval.
    For instance, if the interval is 7, then every seventh frames are compared.
    
    We use MediaCodec/MediaExtractor to decode the video, and use ImageReader to
    extract the decoded frame.
    
    The SSIM calculation logic is a inspired by and modified from the CTS
    [MSSIMMatcher](https://cs.android.com/android/platform/superproject/+/master:cts/tests/tests/uirendering/src/android/uirendering/cts/bitmapcomparers/MSSIMComparer.java;l=1?q=mssimcom)
    that has some errors and extra features we don't need (like handling RGB
    images).
    
    Adds TranscodeQualityTest to ensure high quality transcoding.
    
    PiperOrigin-RevId: 430951206

---
+++ library/transformer/src/androidTest/java/com/google/android/exoplayer2/transformer/mh/TranscodeQualityTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.exoplayer2.transformer.mh;
+
+import static com.google.android.exoplayer2.transformer.AndroidTestUtil.runTransformer;
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.google.android.exoplayer2.transformer.AndroidTestUtil;
+import com.google.android.exoplayer2.transformer.SsimHelper;
+import com.google.android.exoplayer2.transformer.TestTransformationResult;
+import com.google.android.exoplayer2.transformer.TransformationRequest;
+import com.google.android.exoplayer2.transformer.Transformer;
+import com.google.android.exoplayer2.util.MimeTypes;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Checks transcoding quality. */
+@RunWith(AndroidJUnit4.class)
+public final class TranscodeQualityTest {
+  @Test
+  public void singleTranscode_ssimIsGreaterThan95Percent() throws Exception {
+    Context context = ApplicationProvider.getApplicationContext();
+    Transformer transformer =
+        new Transformer.Builder(context)
+            .setTransformationRequest(
+                new TransformationRequest.Builder()
+                    .setVideoMimeType(MimeTypes.VIDEO_H265)
+                    .setAudioMimeType(MimeTypes.AUDIO_AMR_NB)
+                    .build())
+            .build();
+
+    TestTransformationResult result =
+        runTransformer(
+            context,
+            /* testId= */ "singleTranscode_ssim",
+            transformer,
+            AndroidTestUtil.MP4_ASSET_URI_STRING,
+            /* timeoutSeconds= */ 120);
+
+    assertThat(
+            SsimHelper.calculate(
+                context,
+                AndroidTestUtil.MP4_ASSET_URI_STRING,
+                result.filePath,
+                SsimHelper.DEFAULT_COMPARISON_INTERVAL))
+        .isGreaterThan(0.95);
+  }
+}
