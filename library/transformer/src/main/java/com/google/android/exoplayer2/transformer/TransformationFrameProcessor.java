255007623d0930429f347736314a3f09c178f20d
hschlueter <hschlueter@google.com>
2022-03-14 12:52:16 +0000

    Clarify GlProgram parameter name.
    PiperOrigin-RevId: 434441008

--- library/transformer/src/main/java/com/google/android/exoplayer2/transformer/TransformationFrameProcessor.java
+++ library/transformer/src/main/java/com/google/android/exoplayer2/transformer/TransformationFrameProcessor.java
@@ -97,7 +97,7 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
     // TODO(b/205002913): check the loaded program is consistent with the attributes and uniforms
     //  expected in the code.
     glProgram = new GlProgram(context, VERTEX_SHADER_TRANSFORMATION_PATH, FRAGMENT_SHADER_PATH);
-    glProgram.setSamplerTexIdUniform("uTexSampler", inputTexId, /* unit= */ 0);
+    glProgram.setSamplerTexIdUniform("uTexSampler", inputTexId, /* texUnitIndex= */ 0);
     // Draw the frame on the entire normalized device coordinate space, from -1 to 1, for x and y.
     glProgram.setBufferAttribute(
         "aFramePosition", GlUtil.getNormalizedCoordinateBounds(), GlUtil.RECTANGLE_VERTICES_COUNT);
