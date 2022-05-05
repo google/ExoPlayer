ff1305cd1919966eedaa64a0d5ad7a0223b27d9d
hschlueter <hschlueter@google.com>
2022-05-05 19:24:44 +0100

    Remove redundant attribute from matrix transformation vertex shader.
    The texture coordinates can be derived from the frame coordinates.
    
    PiperOrigin-RevId: 446770538

--- library/transformer/src/main/java/com/google/android/exoplayer2/transformer/MatrixTransformationFrameProcessor.java
+++ library/transformer/src/main/java/com/google/android/exoplayer2/transformer/MatrixTransformationFrameProcessor.java
@@ -87,8 +87,6 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
     // Draw the frame on the entire normalized device coordinate space, from -1 to 1, for x and y.
     glProgram.setBufferAttribute(
         "aFramePosition", GlUtil.getNormalizedCoordinateBounds(), GlUtil.RECTANGLE_VERTICES_COUNT);
-    glProgram.setBufferAttribute(
-        "aTexSamplingCoord", GlUtil.getTextureCoordinateBounds(), GlUtil.RECTANGLE_VERTICES_COUNT);
   }
 
   @Override
