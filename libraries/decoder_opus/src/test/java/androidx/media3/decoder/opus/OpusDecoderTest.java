55a194c575ba40dc0e156815f07ec854dc85bd3d
bachinger <bachinger@google.com>
2022-05-31 14:41:24 +0000

    Ignore decoding test cases when library not available
    #minor-release
    
    PiperOrigin-RevId: 452043577

--- libraries/decoder_opus/src/test/java/androidx/media3/decoder/opus/OpusDecoderTest.java
+++ libraries/decoder_opus/src/test/java/androidx/media3/decoder/opus/OpusDecoderTest.java
@@ -16,6 +16,7 @@
 package androidx.media3.decoder.opus;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assume.assumeTrue;
 
 import androidx.annotation.Nullable;
 import androidx.media3.common.C;
@@ -26,7 +27,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.google.common.collect.ImmutableList;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -68,11 +68,6 @@ public final class OpusDecoderTest {
 
   private static final ImmutableList<byte[]> FULL_INITIALIZATION_DATA =
       ImmutableList.of(HEADER, CUSTOM_PRE_SKIP_BYTES, CUSTOM_SEEK_PRE_ROLL_BYTES);
-
-  @Before
-  public void setUp() {
-    assertThat(LOADER.isAvailable()).isTrue();
-  }
 
   @Test
   public void getChannelCount() {
@@ -120,6 +115,7 @@ public final class OpusDecoderTest {
 
   @Test
   public void decode_removesPreSkipFromOutput() throws OpusDecoderException {
+    assumeTrue(LOADER.isAvailable());
     OpusDecoder decoder =
         new OpusDecoder(
             /* numInputBuffers= */ 0,
@@ -139,6 +135,7 @@ public final class OpusDecoderTest {
   @Test
   public void decode_whenDiscardPaddingDisabled_returnsDiscardPadding()
       throws OpusDecoderException {
+    assumeTrue(LOADER.isAvailable());
     OpusDecoder decoder =
         new OpusDecoder(
             /* numInputBuffers= */ 0,
@@ -159,6 +156,7 @@ public final class OpusDecoderTest {
 
   @Test
   public void decode_whenDiscardPaddingEnabled_removesDiscardPadding() throws OpusDecoderException {
+    assumeTrue(LOADER.isAvailable());
     OpusDecoder decoder =
         new OpusDecoder(
             /* numInputBuffers= */ 0,
