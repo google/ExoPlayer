5349312f50ba0cf8d7000d6a70d440940c67ac4e
huangdarwin <huangdarwin@google.com>
2022-06-08 12:51:28 +0000

    Transformer GL: Split Presentation and Crop.
    This removes the prior restriction of needing to remember not to crop and set aspect ratio in the same Presentation.Builder, and makes each class a bit more targeted.
    
    This is partially made feasible by the past work to merge consecutive
    MatrixTransformations into a single MatrixTransformationFrameProcessor, which
    ensures that there's no loss in quality between successive MatrixTransformations.
    
    PiperOrigin-RevId: 453660582
    (cherry picked from commit c5b881e0899e713a7c632da48f1cd64b1394a221)

---
+++ libraries/transformer/src/test/java/androidx/media3/transformer/CropTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.media3.transformer;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.util.Size;
+import androidx.media3.common.util.GlUtil;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Unit tests for {@link Crop}.
+ *
+ * <p>See {@code CropPixelTest} for pixel tests testing {@link Crop}.
+ */
+@RunWith(AndroidJUnit4.class)
+public final class CropTest {
+  @Test
+  public void configure_noEdits_leavesFramesUnchanged() {
+    int inputWidth = 200;
+    int inputHeight = 150;
+    Crop crop = new Crop(/* left= */ -1, /* right= */ 1, /* bottom= */ -1, /* top= */ 1);
+
+    Size outputSize = crop.configure(inputWidth, inputHeight);
+
+    assertThat(outputSize.getWidth()).isEqualTo(inputWidth);
+    assertThat(outputSize.getHeight()).isEqualTo(inputHeight);
+  }
+
+  @Test
+  public void configure_setCrop_changesDimensions() {
+    int inputWidth = 300;
+    int inputHeight = 200;
+    float left = -.5f;
+    float right = .5f;
+    float bottom = .5f;
+    float top = 1f;
+    Crop crop = new Crop(left, right, bottom, top);
+
+    Size outputSize = crop.configure(inputWidth, inputHeight);
+
+    int expectedPostCropWidth = Math.round(inputWidth * (right - left) / GlUtil.LENGTH_NDC);
+    int expectedPostCropHeight = Math.round(inputHeight * (top - bottom) / GlUtil.LENGTH_NDC);
+    assertThat(outputSize.getWidth()).isEqualTo(expectedPostCropWidth);
+    assertThat(outputSize.getHeight()).isEqualTo(expectedPostCropHeight);
+  }
+}
