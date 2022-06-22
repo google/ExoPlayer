26ee3d32b2eb8cef7a19e227d863f79b6b57a7b6
hschlueter <hschlueter@google.com>
2022-06-22 17:16:54 +0100

    Use GlTextureProcessor to avoid redundant copy in MediaPipeProcessor.
    After this change GlEffects can use any GlTextureProcessor not just
    SingleFrameGlTextureProcessor.
    MediaPipeProcessor now implements GlTextureProcessor directly which
    allows it to reuse MediaPipe's output texture for its output texture
    and avoids an extra copy shader step.
    
    PiperOrigin-RevId: 456530718
    (cherry picked from commit 69ab79418ef21033ca25cebdf5a5e80752818ab5)

--- libraries/transformer/src/main/java/androidx/media3/transformer/GlEffect.java
+++ libraries/transformer/src/main/java/androidx/media3/transformer/GlEffect.java
@@ -19,17 +19,15 @@ import android.content.Context;
 import androidx.media3.common.util.UnstableApi;
 
 /**
- * Interface for a video frame effect with a {@link SingleFrameGlTextureProcessor} implementation.
+ * Interface for a video frame effect with a {@link GlTextureProcessor} implementation.
  *
  * <p>Implementations contain information specifying the effect and can be {@linkplain
- * #toGlTextureProcessor(Context) converted} to a {@link SingleFrameGlTextureProcessor} which
- * applies the effect.
+ * #toGlTextureProcessor(Context) converted} to a {@link GlTextureProcessor} which applies the
+ * effect.
  */
 @UnstableApi
 public interface GlEffect {
 
   /** Returns a {@link SingleFrameGlTextureProcessor} that applies the effect. */
-  // TODO(b/227625423): use GlTextureProcessor here once this interface exists.
-  SingleFrameGlTextureProcessor toGlTextureProcessor(Context context)
-      throws FrameProcessingException;
+  GlTextureProcessor toGlTextureProcessor(Context context) throws FrameProcessingException;
 }
