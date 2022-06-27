0b75c254ae24d1751064d2f2294ec9d7054056bb
tonihei <tonihei@google.com>
2022-06-27 10:04:19 +0100

    Rename shouldUseDummySurface to shouldUsePlaceholderSurface
    This was likely missed in https://github.com/google/ExoPlayer/commit/33373d0d0a159ad9c9c3590c838098c4c1530910.
    
    PiperOrigin-RevId: 457422574
    (cherry picked from commit aaa01529052bb299be41d5c56b2ea58abf738ce7)

--- library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
@@ -626,7 +626,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
         surface = placeholderSurface;
       } else {
         MediaCodecInfo codecInfo = getCodecInfo();
-        if (codecInfo != null && shouldUseDummySurface(codecInfo)) {
+        if (codecInfo != null && shouldUsePlaceholderSurface(codecInfo)) {
           placeholderSurface = PlaceholderSurface.newInstanceV17(context, codecInfo.secure);
           surface = placeholderSurface;
         }
@@ -672,7 +672,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
 
   @Override
   protected boolean shouldInitCodec(MediaCodecInfo codecInfo) {
-    return surface != null || shouldUseDummySurface(codecInfo);
+    return surface != null || shouldUsePlaceholderSurface(codecInfo);
   }
 
   @Override
@@ -703,7 +703,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
             deviceNeedsNoPostProcessWorkaround,
             tunneling ? tunnelingAudioSessionId : C.AUDIO_SESSION_ID_UNSET);
     if (surface == null) {
-      if (!shouldUseDummySurface(codecInfo)) {
+      if (!shouldUsePlaceholderSurface(codecInfo)) {
         throw new IllegalStateException();
       }
       if (placeholderSurface == null) {
@@ -1330,7 +1330,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
     maybeNotifyRenderedFirstFrame();
   }
 
-  private boolean shouldUseDummySurface(MediaCodecInfo codecInfo) {
+  private boolean shouldUsePlaceholderSurface(MediaCodecInfo codecInfo) {
     return Util.SDK_INT >= 23
         && !tunneling
         && !codecNeedsSetOutputSurfaceWorkaround(codecInfo.name)
