3db86e6e73745c1d5ffccb5b31116998662905a1
Steve Mayhew <mayhew.steve@gmail.com>
2022-06-30 14:58:28 -0700

    Allows discard of overlapping iFrame only chunks
    Consider two I-Frame only playlists, to allow adaption based on frame rate the two playlist have different I-Frame duration.
    
    The I-Frame only segment has a single sample (IDR), of sub-second duration (e.g. 0.0333s for 30fps source) but a
    duration equal to the time distance between the I-Frames.
    
    Playlist P1 -- 6 seconds between IDR samples, and Playlist P2 -- 4 seconds between IDR samples:
    
    ````
    +-----+-----+-----+
    |  1  |  2  |  3  |
    +-----+-----+-----+
    +---+---+---+---+---+
    | 1 | 2 | 3 | 4 | 5 |
    +---+---+---+---+---+
    ````
    
    If the `AdaptiveTrackSelection` changes from P2 after loading segments from P1 there appears to be a sample overlap that
    would trigger `shouldSpliceIn` to be set on the iFrame `MediaChunk` when no sample splicing is required.
    
    ````
    +-----+---
    |  1  |  2    --- loaded in P1
    +-----+---
            +---+---+---+
            | 3 | 4 | 5 |  -- switched to P2
            +---+---+---+
    ````
    
    Because the `startTimeUs` of P2 segment 3 is less then the `endTimeUs` of P1 segment 3 it appears samples would overlap,
    as there is only one sample in each segment this is not possible.   Setting this `shouldSpliceIn` flag incorrectly
    prevents pruning the buffered chunks.

--- library/hls/src/main/java/com/google/android/exoplayer2/source/hls/HlsMediaChunk.java
+++ library/hls/src/main/java/com/google/android/exoplayer2/source/hls/HlsMediaChunk.java
@@ -197,6 +197,7 @@ import org.checkerframework.checker.nullness.qual.RequiresNonNull;
    *     in the queue.
    * @param playlistUrl The URL of the playlist from which the new chunk will be obtained.
    * @param mediaPlaylist The {@link HlsMediaPlaylist} containing the new chunk.
+   * @param playlistFormat The {@link Format} of the playlist for the new chunk
    * @param segmentBaseHolder The {@link HlsChunkSource.SegmentBaseHolder} with information about
    *     the new chunk.
    * @param startOfPlaylistInPeriodUs The start time of the playlist in the period, in microseconds.
@@ -206,6 +207,7 @@ import org.checkerframework.checker.nullness.qual.RequiresNonNull;
       @Nullable HlsMediaChunk previousChunk,
       Uri playlistUrl,
       HlsMediaPlaylist mediaPlaylist,
+      Format playlistFormat,
       HlsChunkSource.SegmentBaseHolder segmentBaseHolder,
       long startOfPlaylistInPeriodUs) {
     if (previousChunk == null) {
@@ -221,8 +223,11 @@ import org.checkerframework.checker.nullness.qual.RequiresNonNull;
     // non-overlapping segments to avoid the splice.
     long segmentStartTimeInPeriodUs =
         startOfPlaylistInPeriodUs + segmentBaseHolder.segmentBase.relativeStartTimeUs;
+    boolean areBothChunksTrickplay = (previousChunk.trackFormat.roleFlags & C.ROLE_FLAG_TRICK_PLAY) != 0
+        && (playlistFormat.roleFlags & C.ROLE_FLAG_TRICK_PLAY) != 0;
+
     return !isIndependent(segmentBaseHolder, mediaPlaylist)
-        || segmentStartTimeInPeriodUs < previousChunk.endTimeUs;
+        || (segmentStartTimeInPeriodUs < previousChunk.endTimeUs && !areBothChunksTrickplay);
   }
 
   public static final String PRIV_TIMESTAMP_FRAME_OWNER =
