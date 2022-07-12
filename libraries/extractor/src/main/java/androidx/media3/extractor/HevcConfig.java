396b55a3411c4ff6489d233167599381801d0e65
rohks <rohks@google.com>
2022-07-12 16:53:03 +0000

    Ignore reserved bit in parsing NAL unit type
    `HevcConfig.parse` misreads reserved bit to determine NAL unit type. This is currently meant to be always set to 0, but could be given some kind of meaning in a future revision.
    
    Issue: google/ExoPlayer#10366
    PiperOrigin-RevId: 460487613
    (cherry picked from commit 704aa2531adac1a73cc284baa1a2f5e22f0c2a0b)

--- libraries/extractor/src/main/java/androidx/media3/extractor/HevcConfig.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/HevcConfig.java
@@ -46,7 +46,7 @@ public final class HevcConfig {
       int csdLength = 0;
       int csdStartPosition = data.getPosition();
       for (int i = 0; i < numberOfArrays; i++) {
-        data.skipBytes(1); // completeness (1), nal_unit_type (7)
+        data.skipBytes(1); // completeness (1), reserved (1), nal_unit_type (6)
         int numberOfNalUnits = data.readUnsignedShort();
         for (int j = 0; j < numberOfNalUnits; j++) {
           int nalUnitLength = data.readUnsignedShort();
@@ -64,7 +64,8 @@ public final class HevcConfig {
       float pixelWidthHeightRatio = 1;
       @Nullable String codecs = null;
       for (int i = 0; i < numberOfArrays; i++) {
-        int nalUnitType = data.readUnsignedByte() & 0x7F; // completeness (1), nal_unit_type (7)
+        int nalUnitType =
+            data.readUnsignedByte() & 0x3F; // completeness (1), reserved (1), nal_unit_type (6)
         int numberOfNalUnits = data.readUnsignedShort();
         for (int j = 0; j < numberOfNalUnits; j++) {
           int nalUnitLength = data.readUnsignedShort();
