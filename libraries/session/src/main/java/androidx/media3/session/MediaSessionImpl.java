9271572e950230a24da69a6ec5838dd838ea672d
olly <olly@google.com>
2022-07-15 10:34:53 +0000

    Add TODOs for registerReceiver calls without flag
    PiperOrigin-RevId: 461165173

--- libraries/session/src/main/java/androidx/media3/session/MediaSessionImpl.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaSessionImpl.java
@@ -210,6 +210,7 @@ import org.checkerframework.checker.initialization.qual.Initialized;
       broadcastReceiver = new MediaButtonReceiver();
       IntentFilter filter = new IntentFilter(Intent.ACTION_MEDIA_BUTTON);
       filter.addDataScheme(castNonNull(sessionUri.getScheme()));
+      // TODO(b/197817693): Explicitly indicate whether the receiver should be exported.
       context.registerReceiver(broadcastReceiver, filter);
     } else {
       // Has MediaSessionService to revive playback after it's dead.
