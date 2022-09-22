109ab70e00921ff772a4f0f5edfeb97a683297b6
leonwind <leonwind@google.com>
2022-09-22 10:04:42 +0000

    Add RGB Adjustments to transformer demo.
    PiperOrigin-RevId: 476049125
    (cherry picked from commit cfc0eef24ec4a24bc00b0a657f3dad07e9c29ade)

--- demos/transformer/src/main/java/com/google/android/exoplayer2/transformerdemo/TransformerActivity.java
+++ demos/transformer/src/main/java/com/google/android/exoplayer2/transformerdemo/TransformerActivity.java
@@ -40,6 +40,7 @@ import androidx.media3.common.Effect;
 import androidx.media3.effect.Contrast;
 import androidx.media3.effect.GlEffect;
 import androidx.media3.effect.GlTextureProcessor;
+import androidx.media3.effect.RgbAdjustment;
 import androidx.media3.effect.RgbFilter;
 import androidx.media3.effect.RgbMatrix;
 import com.google.android.exoplayer2.C;
@@ -336,9 +337,17 @@ public final class TransformerActivity extends AppCompatActivity {
           }
         }
         if (selectedEffects[3]) {
-          effects.add(new Contrast(bundle.getFloat(ConfigurationActivity.CONTRAST_VALUE)));
+          effects.add(
+              new RgbAdjustment.Builder()
+                  .setRedScale(bundle.getFloat(ConfigurationActivity.RGB_ADJUSTMENT_RED_SCALE))
+                  .setGreenScale(bundle.getFloat(ConfigurationActivity.RGB_ADJUSTMENT_GREEN_SCALE))
+                  .setBlueScale(bundle.getFloat(ConfigurationActivity.RGB_ADJUSTMENT_BLUE_SCALE))
+                  .build());
         }
         if (selectedEffects[4]) {
+          effects.add(new Contrast(bundle.getFloat(ConfigurationActivity.CONTRAST_VALUE)));
+        }
+        if (selectedEffects[5]) {
           effects.add(
               (GlEffect)
                   (Context context, boolean useHdr) ->
@@ -353,13 +362,13 @@ public final class TransformerActivity extends AppCompatActivity {
                               ConfigurationActivity.PERIODIC_VIGNETTE_OUTER_RADIUS),
                           bundle.getFloat(ConfigurationActivity.PERIODIC_VIGNETTE_OUTER_RADIUS)));
         }
-        if (selectedEffects[5]) {
+        if (selectedEffects[6]) {
           effects.add(MatrixTransformationFactory.createSpin3dEffect());
         }
-        if (selectedEffects[6]) {
+        if (selectedEffects[7]) {
           effects.add((GlEffect) BitmapOverlayProcessor::new);
         }
-        if (selectedEffects[7]) {
+        if (selectedEffects[8]) {
           effects.add(MatrixTransformationFactory.createZoomInTransition());
         }
         transformerBuilder.setVideoEffects(effects.build());
