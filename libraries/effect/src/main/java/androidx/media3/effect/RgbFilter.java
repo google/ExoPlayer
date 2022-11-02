40b5cba3c89398697d405d673bd09bf540dc3d88
ibaker <ibaker@google.com>
2022-11-02 12:21:56 +0000

    Fix Dackka/Metalava errors in the effects module
    Public methods may only refer to public types in their signature. This
    change ensures that by switching to a public supertype everywhere.
    
    PiperOrigin-RevId: 485568625

--- libraries/effect/src/main/java/androidx/media3/effect/RgbFilter.java
+++ libraries/effect/src/main/java/androidx/media3/effect/RgbFilter.java
@@ -91,7 +91,7 @@ public class RgbFilter implements RgbMatrix {
   }
 
   @Override
-  public MatrixTextureProcessor toGlTextureProcessor(Context context, boolean useHdr)
+  public SingleFrameGlTextureProcessor toGlTextureProcessor(Context context, boolean useHdr)
       throws FrameProcessingException {
     checkForConsistentHdrSetting(useHdr);
     return RgbMatrix.super.toGlTextureProcessor(context, useHdr);
