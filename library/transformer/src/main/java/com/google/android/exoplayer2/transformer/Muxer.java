194a32c0fbccdd87c66d1b59eca8c72406690c09
sheenachhabra <sheenachhabra@google.com>
2022-11-07 20:38:44 +0000

    Move muxer initialization off application thread
    Problem: We are initialising muxer as soon as we start the transformation. Now the startTransformation() method can be called from main thread, but muxer creation is an I/O operation and should be not be done on main thread.
    
    Solution: Added lazy initialisation of muxer object. The actual transformation happens on background thread so the muxer will be initialised lazily from background thread only.
    
    Another way was to provide an initialize() method on MuxerWrapper which will explicitly initialise muxer object but with this approach the caller need to call the initialise method before calling anything else. With current implementation the renderers are calling MuxerWrapper methods on various callbacks (Not sequentially) and also we are sharing same muxer with multiple renderers so It might become confusing for the caller on when to call the initialise() method. Also there are few methods on MuxerWrapper which dont really need muxer object. So in short it might make MuxerWrapper APIs more confusing.
    
    Validation: Verified the transformation from demo app.
    PiperOrigin-RevId: 486735787
    (cherry picked from commit b10b4e6d4694c7240073b81c3a09227042b58c21)

--- library/transformer/src/main/java/com/google/android/exoplayer2/transformer/Muxer.java
+++ library/transformer/src/main/java/com/google/android/exoplayer2/transformer/Muxer.java
@@ -20,7 +20,6 @@ import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.common.collect.ImmutableList;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 
 /**
@@ -55,9 +54,9 @@ public interface Muxer {
      *
      * @param path The path to the output file.
      * @throws IllegalArgumentException If the path is invalid.
-     * @throws IOException If an error occurs opening the output file for writing.
+     * @throws MuxerException If an error occurs opening the output file for writing.
      */
-    Muxer create(String path) throws IOException;
+    Muxer create(String path) throws MuxerException;
 
     /**
      * Returns a new muxer writing to a file descriptor.
@@ -67,9 +66,9 @@ public interface Muxer {
      *     muxer is released. It is the responsibility of the caller to close the
      *     ParcelFileDescriptor. This can be done after this method returns.
      * @throws IllegalArgumentException If the file descriptor is invalid.
-     * @throws IOException If an error occurs opening the output file descriptor for writing.
+     * @throws MuxerException If an error occurs opening the output file descriptor for writing.
      */
-    Muxer create(ParcelFileDescriptor parcelFileDescriptor) throws IOException;
+    Muxer create(ParcelFileDescriptor parcelFileDescriptor) throws MuxerException;
 
     /**
      * Returns the supported sample {@linkplain MimeTypes MIME types} for the given {@link
