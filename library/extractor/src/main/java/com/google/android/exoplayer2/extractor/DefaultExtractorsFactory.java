4853444f0dc90163c257d0e20962604510557df4
ibaker <ibaker@google.com>
2022-11-22 13:09:17 +0000

    Add `DefaultExtractorsFactory.setTsSubtitleFormats`
    ExoPlayer is unable to detect the presence of subtitle tracks in some
    MPEG-TS files that don't fully declare them. It's possible for a
    developer to provide the list instead, but doing so is quite awkward
    without this helper method. This is consistent for how
    `DefaultExtractorsFactory` allows other aspects of the delegate
    `Extractor` implementations to be customised.
    
    * Issue: google/ExoPlayer#10175
    * Issue: google/ExoPlayer#10505
    
    #minor-release
    
    PiperOrigin-RevId: 490214619

--- library/extractor/src/main/java/com/google/android/exoplayer2/extractor/DefaultExtractorsFactory.java
+++ library/extractor/src/main/java/com/google/android/exoplayer2/extractor/DefaultExtractorsFactory.java
@@ -21,6 +21,7 @@ import static com.google.android.exoplayer2.util.FileTypes.inferFileTypeFromUri;
 import android.net.Uri;
 import androidx.annotation.GuardedBy;
 import androidx.annotation.Nullable;
+import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.PlaybackException;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.extractor.amr.AmrExtractor;
@@ -43,6 +44,7 @@ import com.google.android.exoplayer2.extractor.ts.TsPayloadReader;
 import com.google.android.exoplayer2.extractor.wav.WavExtractor;
 import com.google.android.exoplayer2.util.FileTypes;
 import com.google.android.exoplayer2.util.TimestampAdjuster;
+import com.google.common.collect.ImmutableList;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -126,11 +128,13 @@ public final class DefaultExtractorsFactory implements ExtractorsFactory {
   private @Mp3Extractor.Flags int mp3Flags;
   private @TsExtractor.Mode int tsMode;
   private @DefaultTsPayloadReaderFactory.Flags int tsFlags;
+  private ImmutableList<Format> tsSubtitleFormats;
   private int tsTimestampSearchBytes;
 
   public DefaultExtractorsFactory() {
     tsMode = TsExtractor.MODE_SINGLE_PMT;
     tsTimestampSearchBytes = TsExtractor.DEFAULT_TIMESTAMP_SEARCH_BYTES;
+    tsSubtitleFormats = ImmutableList.of();
   }
 
   /**
@@ -302,6 +306,20 @@ public final class DefaultExtractorsFactory implements ExtractorsFactory {
   }
 
   /**
+   * Sets a list of subtitle formats to pass to the {@link DefaultTsPayloadReaderFactory} used by
+   * {@link TsExtractor} instances created by the factory.
+   *
+   * @see DefaultTsPayloadReaderFactory#DefaultTsPayloadReaderFactory(int, List)
+   * @param subtitleFormats The subtitle formats.
+   * @return The factory, for convenience.
+   */
+  @CanIgnoreReturnValue
+  public synchronized DefaultExtractorsFactory setTsSubtitleFormats(List<Format> subtitleFormats) {
+    tsSubtitleFormats = ImmutableList.copyOf(subtitleFormats);
+    return this;
+  }
+
+  /**
    * Sets the number of bytes searched to find a timestamp for {@link TsExtractor} instances created
    * by the factory.
    *
@@ -414,7 +432,12 @@ public final class DefaultExtractorsFactory implements ExtractorsFactory {
         extractors.add(new PsExtractor());
         break;
       case FileTypes.TS:
-        extractors.add(new TsExtractor(tsMode, tsFlags, tsTimestampSearchBytes));
+        extractors.add(
+            new TsExtractor(
+                tsMode,
+                new TimestampAdjuster(0),
+                new DefaultTsPayloadReaderFactory(tsFlags, tsSubtitleFormats),
+                tsTimestampSearchBytes));
         break;
       case FileTypes.WAV:
         extractors.add(new WavExtractor());
