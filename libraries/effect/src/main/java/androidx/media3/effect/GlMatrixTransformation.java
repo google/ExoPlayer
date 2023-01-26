a5d43b2194bf384c0534deb75f5e4fbae24aca27
huangdarwin <huangdarwin@google.com>
2023-01-26 11:20:12 +0000

    Effect: Clarify that padding is added in ScaleToFitTransformation.
    PiperOrigin-RevId: 504796380

--- libraries/effect/src/main/java/androidx/media3/effect/GlMatrixTransformation.java
+++ libraries/effect/src/main/java/androidx/media3/effect/GlMatrixTransformation.java
@@ -23,7 +23,7 @@ import androidx.media3.common.util.UnstableApi;
 import com.google.common.collect.ImmutableList;
 
 /**
- * Specifies a 4x4 transformation {@link Matrix} to apply in the vertex shader for each frame.
+ * Specifies a 4x4 transformation {@link Matrix} to apply in the vertex shader for each input frame.
  *
  * <p>The matrix is applied to points given in normalized device coordinates (-1 to 1 on x, y, and z
  * axes). Transformed pixels that are moved outside of the normal device coordinate range are
