506aa2c9996391eb83b3b3affcb2be1a6d1dc5a2
andrewlewis <andrewlewis@google.com>
2023-01-30 19:09:44 +0000

    Instantiate `SurfaceView` on main thread
    On some devices/builds it seems that the `SurfaceView` constructor tries to create a handler. Move instantiation to the main thread when getting the debug surface view so that handler creation succeeds.
    
    PiperOrigin-RevId: 505745387

--- demos/transformer/src/main/java/com/google/android/exoplayer2/transformerdemo/TransformerActivity.java
+++ demos/transformer/src/main/java/com/google/android/exoplayer2/transformerdemo/TransformerActivity.java
@@ -738,9 +738,9 @@ public final class TransformerActivity extends AppCompatActivity {
 
       // Update the UI on the main thread and wait for the output surface to be available.
       CountDownLatch surfaceCreatedCountDownLatch = new CountDownLatch(1);
-      SurfaceView surfaceView = new SurfaceView(/* context= */ TransformerActivity.this);
       runOnUiThread(
           () -> {
+            surfaceView = new SurfaceView(/* context= */ TransformerActivity.this);
             AspectRatioFrameLayout debugFrame = checkNotNull(TransformerActivity.this.debugFrame);
             debugFrame.addView(surfaceView);
             debugFrame.setAspectRatio((float) width / height);
@@ -772,7 +772,6 @@ public final class TransformerActivity extends AppCompatActivity {
         Thread.currentThread().interrupt();
         return null;
       }
-      this.surfaceView = surfaceView;
       return surfaceView;
     }
   }
