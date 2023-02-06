f630315af5bd3adcd3ebf5e008f785c6552eb48f
claincly <claincly@google.com>
2023-02-06 14:20:56 +0000

    Flush FrameProcessor when appropriate.
    PiperOrigin-RevId: 507461550

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/MediaCodecVideoRenderer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/MediaCodecVideoRenderer.java
@@ -1178,9 +1178,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
       if (frameProcessorManager.isEnabled()) {
         notifyFrameMetaDataListener = false;
         if (!frameProcessorManager.maybeRegisterFrame(format, presentationTimeUs, isLastBuffer)) {
-          // TODO(b/238302341): Handle FrameProcessor is unable to accept the force rendered buffer.
-          //  Treat the frame as dropped for now.
-          return true;
+          return false;
         }
       } else {
         notifyFrameMetaDataListener = true;
@@ -1454,6 +1452,9 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
           droppedSourceBufferCount, /* droppedDecoderBufferCount= */ buffersInCodecCount);
     }
     flushOrReinitializeCodec();
+    if (frameProcessorManager.isEnabled()) {
+      frameProcessorManager.flush();
+    }
     return true;
   }
 
