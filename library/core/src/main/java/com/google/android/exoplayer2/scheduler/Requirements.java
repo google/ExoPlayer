34b9824201314b4fa6442deac643635f2fa9e541
tonihei <tonihei@google.com>
2023-02-24 10:42:37 +0000

    Do not specify export flags for protected system broadcasts.
    Protected system broadcasts should not specify the export flag.
    Marking them as NOT_EXPORTED breaks sticky broadcasts in some
    cases.
    
    Issue: google/ExoPlayer#10970
    
    #minor-release
    
    PiperOrigin-RevId: 512020154

--- library/core/src/main/java/com/google/android/exoplayer2/scheduler/Requirements.java
+++ library/core/src/main/java/com/google/android/exoplayer2/scheduler/Requirements.java
@@ -182,8 +182,8 @@ public final class Requirements implements Parcelable {
   private boolean isDeviceCharging(Context context) {
     @Nullable
     Intent batteryStatus =
-        Util.registerReceiverNotExported(
-            context, /* receiver= */ null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        context.registerReceiver(
+            /* receiver= */ null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     if (batteryStatus == null) {
       return false;
     }
@@ -201,8 +201,8 @@ public final class Requirements implements Parcelable {
   }
 
   private boolean isStorageNotLow(Context context) {
-    return Util.registerReceiverNotExported(
-            context, /* receiver= */ null, new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW))
+    return context.registerReceiver(
+            /* receiver= */ null, new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW))
         == null;
   }
 
