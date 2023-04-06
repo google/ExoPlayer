54ebfa986afce51edd8994ab44e9ae6acb13b84c
tofunmi <tofunmi@google.com>
2023-04-06 16:53:40 +0100

    disable transfer colors in DefaultShaderProgram.createApplyingOetf
    PiperOrigin-RevId: 522347729

--- libraries/effect/src/main/java/androidx/media3/effect/DefaultShaderProgram.java
+++ libraries/effect/src/main/java/androidx/media3/effect/DefaultShaderProgram.java
@@ -294,7 +294,8 @@ import java.util.List;
       Context context,
       List<GlMatrixTransformation> matrixTransformations,
       List<RgbMatrix> rgbMatrices,
-      ColorInfo outputColorInfo)
+      ColorInfo outputColorInfo,
+      boolean enableColorTransfers)
       throws VideoFrameProcessingException {
     boolean outputIsHdr = ColorInfo.isTransferHdr(outputColorInfo);
     String vertexShaderFilePath =
@@ -303,6 +304,9 @@ import java.util.List;
         outputIsHdr
             ? FRAGMENT_SHADER_OETF_ES3_PATH
             : FRAGMENT_SHADER_TRANSFORMATION_SDR_OETF_ES2_PATH;
+    if (!enableColorTransfers) {
+      fragmentShaderFilePath = FRAGMENT_SHADER_TRANSFORMATION_PATH;
+    }
     GlProgram glProgram = createGlProgram(context, vertexShaderFilePath, fragmentShaderFilePath);
 
     @C.ColorTransfer int outputColorTransfer = outputColorInfo.colorTransfer;
@@ -310,8 +314,9 @@ import java.util.List;
       checkArgument(
           outputColorTransfer == C.COLOR_TRANSFER_HLG
               || outputColorTransfer == C.COLOR_TRANSFER_ST2084);
+      checkArgument(enableColorTransfers);
       glProgram.setIntUniform("uOutputColorTransfer", outputColorTransfer);
-    } else {
+    } else if (enableColorTransfers) {
       checkArgument(
           outputColorTransfer == C.COLOR_TRANSFER_SDR
               || outputColorTransfer == C.COLOR_TRANSFER_GAMMA_2_2);
