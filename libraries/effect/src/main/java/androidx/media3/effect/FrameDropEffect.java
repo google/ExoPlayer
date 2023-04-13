2f23774d53dbd42d46b26e445a71a2594cb7729d
tofunmi <tofunmi@google.com>
2023-04-13 18:01:39 +0100

    Add FrameDropping GlEffect
    PiperOrigin-RevId: 524030672

---
+++ libraries/effect/src/main/java/androidx/media3/effect/FrameDropEffect.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.media3.effect;
+
+import android.content.Context;
+import androidx.media3.common.VideoFrameProcessingException;
+import androidx.media3.common.util.UnstableApi;
+
+/** Drops frames to lower average frame rate to around {@code targetFrameRate}. */
+@UnstableApi
+public class FrameDropEffect implements GlEffect {
+
+  private final float targetFrameRate;
+
+  /**
+   * Creates an instance.
+   *
+   * @param targetFrameRate The number of frames per second the output video should roughly have.
+   */
+  public FrameDropEffect(float targetFrameRate) {
+    this.targetFrameRate = targetFrameRate;
+  }
+
+  @Override
+  public GlShaderProgram toGlShaderProgram(Context context, boolean useHdr)
+      throws VideoFrameProcessingException {
+    return new FrameDroppingShaderProgram(context, useHdr, targetFrameRate);
+  }
+}
