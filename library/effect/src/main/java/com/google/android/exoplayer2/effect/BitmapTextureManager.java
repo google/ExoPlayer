35ed33d01608e2a3d4f359c724145d71ab77fbe9
tofunmi <tofunmi@google.com>
2023-04-28 12:11:27 +0100

    Remove volatile from boolean in BitmapTextureManager
    The field is only accessed on the GL thread.
    
    PiperOrigin-RevId: 527844674

--- library/effect/src/main/java/com/google/android/exoplayer2/effect/BitmapTextureManager.java
+++ library/effect/src/main/java/com/google/android/exoplayer2/effect/BitmapTextureManager.java
@@ -47,7 +47,7 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
   private int framesToQueueForCurrentBitmap;
   private double currentPresentationTimeUs;
   private boolean useHdr;
-  private volatile boolean inputEnded;
+  private boolean inputEnded;
 
   /**
    * Creates a new instance.
