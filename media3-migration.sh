690e172b3775e73647ee4a3a69b11a0a27335d8d
bachinger <bachinger@google.com>
2023-05-05 16:52:11 +0000

    Update migration script for 1.0.2/2.18.7
    #minor-release
    
    PiperOrigin-RevId: 529742339
    (cherry picked from commit c12fea75eec0792a88787da6b8ce4e67ecd0489e)

--- media3-migration.sh
+++ media3-migration.sh
@@ -128,8 +128,8 @@ extension-workmanager media3-exoplayer-workmanager'
 # Rewrites classes, packages and dependencies from the legacy ExoPlayer package structure
 # to androidx.media3 structure.
 
-MEDIA3_VERSION="1.0.1"
-LEGACY_PEER_VERSION="2.18.6"
+MEDIA3_VERSION="1.0.2"
+LEGACY_PEER_VERSION="2.18.7"
 
 function usage() {
   echo "usage: $0 [-p|-c|-d|-v]|[-m|-l [-x <path>] [-f] PROJECT_ROOT]"
