f38cbad98558145b3b89f1bb57b25f3105ea36f9
bachinger <bachinger@google.com>
2023-05-19 17:48:32 +0100

    Set video size to 0/0 when video render is disabled
    In terms of MCVR with a `VideoRendererEventListener`, the video size is set to
    0/0 right after `onVideoDisabled()` is called and is set to the actual size as
    soon as the video size is known after 'onVideoEnabled()`.
    
    For ExoPlayer and in terms of the `Player` interface, `Player.getVideoSize()`
    returns a video size of 0/0 when `Player.getCurrentTracks()` does not support
    `C.TRACK_TYPE_VIDEO`. This is ensured by the masking behavior of
    `ExoPlayerImpl` that sets an empty track selection result when the playing
    period changes due to a seek or timeline removal.
    
    When transitioning playback from a video media item to the next, or when
    seeking within the same video media item, the renderer is not disabled.
    
    #minor-release
    
    PiperOrigin-RevId: 533479600

--- library/common/src/main/java/com/google/android/exoplayer2/Player.java
+++ library/common/src/main/java/com/google/android/exoplayer2/Player.java
@@ -981,7 +981,7 @@ public interface Player {
     default void onDeviceVolumeChanged(int volume, boolean muted) {}
 
     /**
-     * Called each time there's a change in the size of the video being rendered.
+     * Called each time when {@link Player#getVideoSize()} changes.
      *
      * <p>{@link #onEvents(Player, Events)} will also be called to report this event along with
      * other events that happen in the same {@link Looper} message queue iteration.
@@ -3066,8 +3066,8 @@ public interface Player {
   /**
    * Gets the size of the video.
    *
-   * <p>The video's width and height are {@code 0} if there is no video or its size has not been
-   * determined yet.
+   * <p>The video's width and height are {@code 0} if there is {@linkplain
+   * Tracks#isTypeSupported(int) no supported video track} or its size has not been determined yet.
    *
    * @see Listener#onVideoSizeChanged(VideoSize)
    */
