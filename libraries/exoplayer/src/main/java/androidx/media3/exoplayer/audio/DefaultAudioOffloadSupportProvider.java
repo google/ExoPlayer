8ab1a68939d6df2e98462d2fc6c9653596df50ef
Cedric T <cedric.tio@xperi.com>
2023-05-31 19:58:12 +0800

    Return offload unsupported for DTSX P2 and API < 34.

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/audio/DefaultAudioOffloadSupportProvider.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/audio/DefaultAudioOffloadSupportProvider.java
@@ -81,17 +81,16 @@ public final class DefaultAudioOffloadSupportProvider
 
     @C.Encoding
     int encoding = MimeTypes.getEncoding(checkNotNull(format.sampleMimeType), format.codecs);
-    if (encoding == C.ENCODING_INVALID) {
+    // AudioFormat.ENCODING_DTS_UHD_P2 is defined from API 34 onwards. We return offload
+    // unsupported to prevent crash in Util.getAudioFormat() below when it tries to create
+    // an AudioFormat with ENCODING_DTS_UHD_P2.
+    if ((encoding == C.ENCODING_INVALID) ||
+        ((Util.SDK_INT < 34) && (encoding == C.ENCODING_DTS_UHD_P2))) {
       return AudioOffloadSupport.DEFAULT_UNSUPPORTED;
     }
     int channelConfig = Util.getAudioTrackChannelConfig(format.channelCount);
     if (channelConfig == AudioFormat.CHANNEL_INVALID) {
       return AudioOffloadSupport.DEFAULT_UNSUPPORTED;
-    }
-    // AudioFormat.ENCODING_DTS_UHD_P2 is defined from API 34 onwards. Before that encoding should
-    // be set to C.ENCODING_DTS, which is supported by TV ICs for offload playback.
-    if ((Util.SDK_INT < 34) && (encoding == C.ENCODING_DTS_UHD_P2)) {
-      encoding = C.ENCODING_DTS;
     }
     AudioFormat audioFormat = Util.getAudioFormat(format.sampleRate, channelConfig, encoding);
     if (Util.SDK_INT >= 31) {
