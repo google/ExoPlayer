bc94a0e189b92310cc28c644aa11771d534987d5
jbibik <jbibik@google.com>
2023-06-07 13:36:41 +0000

    Fix 1 ErrorProneStyle finding
    PiperOrigin-RevId: 538469993

--- library/core/src/main/java/com/google/android/exoplayer2/audio/DefaultAudioSink.java
+++ library/core/src/main/java/com/google/android/exoplayer2/audio/DefaultAudioSink.java
@@ -1075,7 +1075,6 @@ public final class DefaultAudioSink implements AudioSink {
         && (outputBuffer == null || !outputBuffer.hasRemaining());
   }
 
-  @SuppressWarnings("ReferenceEquality")
   /**
    * Writes the provided buffer to the audio track.
    *
@@ -1083,6 +1082,7 @@ public final class DefaultAudioSink implements AudioSink {
    * @param avSyncPresentationTimeUs The tunneling AV sync presentation time for the buffer, or
    *     {@link C#TIME_END_OF_SOURCE} when draining remaining buffers at the end of the stream.
    */
+  @SuppressWarnings("ReferenceEquality")
   private void writeBuffer(ByteBuffer buffer, long avSyncPresentationTimeUs) throws WriteException {
     if (!buffer.hasRemaining()) {
       return;
