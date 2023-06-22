ca766b6979a595052456aba565d37de97d6b9475
ibaker <ibaker@google.com>
2023-06-22 16:41:35 +0000

    Check the two Extractor instantiation sites in HLS remain consistent
    PiperOrigin-RevId: 542585842

--- libraries/exoplayer_hls/src/main/java/androidx/media3/exoplayer/hls/DefaultHlsExtractorFactory.java
+++ libraries/exoplayer_hls/src/main/java/androidx/media3/exoplayer/hls/DefaultHlsExtractorFactory.java
@@ -159,6 +159,7 @@ public final class DefaultHlsExtractorFactory implements HlsExtractorFactory {
       Format format,
       @Nullable List<Format> muxedCaptionFormats,
       TimestampAdjuster timestampAdjuster) {
+    // LINT.IfChange(extractor_instantiation)
     switch (fileType) {
       case FileTypes.WEBVTT:
         return new WebvttExtractor(format.language, timestampAdjuster);
