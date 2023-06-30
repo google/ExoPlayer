d876028a743fe7a6bc944fe069a3b69b0b919304
ibaker <ibaker@google.com>
2023-06-30 16:35:46 +0000

    CEA-608: Only truncate to 32 visible characters
    We introduced truncation to 32 chars in <unknown commit>
    and included indent and offset in the calculation. I think this is
    technically correct, but it causes problems with the content in
    Issue: google/ExoPlayer#11019 and it doesn't seem a problem to only truncate actual
    cue text (i.e. ignore offset and indent).
    
    #minor-release
    
    PiperOrigin-RevId: 544677965
    (cherry picked from commit d5e45209eeb7b3ce96cc9dcf32ea98bb42cf12ce)

--- library/extractor/src/main/java/com/google/android/exoplayer2/text/cea/Cea608Decoder.java
+++ library/extractor/src/main/java/com/google/android/exoplayer2/text/cea/Cea608Decoder.java
@@ -39,7 +39,6 @@ import com.google.android.exoplayer2.util.Assertions;
 import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.ParsableByteArray;
-import com.google.android.exoplayer2.util.Util;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -955,8 +954,7 @@ public final class Cea608Decoder extends CeaDecoder {
     }
 
     public void append(char text) {
-      // Don't accept more than 32 chars. We'll trim further, considering indent & tabOffset, in
-      // build().
+      // Don't accept more than 32 chars.
       if (captionStringBuilder.length() < SCREEN_CHARWIDTH) {
         captionStringBuilder.append(text);
       }
@@ -974,17 +972,14 @@ public final class Cea608Decoder extends CeaDecoder {
 
     @Nullable
     public Cue build(@Cue.AnchorType int forcedPositionAnchor) {
-      // The number of empty columns before the start of the text, in the range [0-31].
-      int startPadding = indent + tabOffset;
-      int maxTextLength = SCREEN_CHARWIDTH - startPadding;
       SpannableStringBuilder cueString = new SpannableStringBuilder();
       // Add any rolled up captions, separated by new lines.
       for (int i = 0; i < rolledUpCaptions.size(); i++) {
-        cueString.append(Util.truncateAscii(rolledUpCaptions.get(i), maxTextLength));
+        cueString.append(rolledUpCaptions.get(i));
         cueString.append('\n');
       }
       // Add the current line.
-      cueString.append(Util.truncateAscii(buildCurrentLine(), maxTextLength));
+      cueString.append(buildCurrentLine());
 
       if (cueString.length() == 0) {
         // The cue is empty.
@@ -992,6 +987,8 @@ public final class Cea608Decoder extends CeaDecoder {
       }
 
       int positionAnchor;
+      // The number of empty columns before the start of the text, in the range [0-31].
+      int startPadding = indent + tabOffset;
       // The number of empty columns after the end of the text, in the same range.
       int endPadding = SCREEN_CHARWIDTH - startPadding - cueString.length();
       int startEndPaddingDelta = startPadding - endPadding;
