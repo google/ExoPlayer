9b06da817458036668bb435a3a57a818dbed7789
samrobinson <samrobinson@google.com>
2023-07-10 19:03:52 +0100

    Remove redundant assertion.
    TransformerAndroidTestRunner#run will throw any exception that occurs,
    so there is no need to assert the result exception is null.
    
    PiperOrigin-RevId: 546923823

--- libraries/transformer/src/androidTest/java/androidx/media3/transformer/TransformerWithInAppMuxerEndToEndTest.java
+++ libraries/transformer/src/androidTest/java/androidx/media3/transformer/TransformerWithInAppMuxerEndToEndTest.java
@@ -15,8 +15,6 @@
  */
 package androidx.media3.transformer;
 
-import static com.google.common.truth.Truth.assertThat;
-
 import android.content.Context;
 import android.net.Uri;
 import androidx.media3.common.Effect;
@@ -61,12 +59,9 @@ public class TransformerWithInAppMuxerEndToEndTest {
             .setEffects(new Effects(/* audioProcessors= */ ImmutableList.of(), videoEffects))
             .build();
 
-    ExportTestResult result =
-        new TransformerAndroidTestRunner.Builder(context, transformer)
-            .build()
-            .run(testId, editedMediaItem);
-
-    assertThat(result.exportResult.exportException).isNull();
+    new TransformerAndroidTestRunner.Builder(context, transformer)
+        .build()
+        .run(testId, editedMediaItem);
   }
 
   @Test
@@ -86,11 +81,8 @@ public class TransformerWithInAppMuxerEndToEndTest {
                     /* videoEffects= */ ImmutableList.of()))
             .build();
 
-    ExportTestResult result =
-        new TransformerAndroidTestRunner.Builder(context, transformer)
-            .build()
-            .run(testId, editedMediaItem);
-
-    assertThat(result.exportResult.exportException).isNull();
+    new TransformerAndroidTestRunner.Builder(context, transformer)
+        .build()
+        .run(testId, editedMediaItem);
   }
 }
