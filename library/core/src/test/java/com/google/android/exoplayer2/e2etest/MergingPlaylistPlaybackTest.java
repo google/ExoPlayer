3235c951f0f10835b3a8ae95f32bef47d7e5252c
tonihei <tonihei@google.com>
2023-07-14 10:01:59 +0100

    Add e2etests for merged sources in a playlist
    These tests allow to verify the samples sent for decoding,
    when decoders are reset, which samples are dropped as decode-only
    for video and which samples are sent to the AudioTrack for playback.
    
    The test exercises all combinations of merges where audio or video
    is the primary track and where audio, video or both are clipped.
    
    PiperOrigin-RevId: 548061254

---
+++ library/core/src/test/java/com/google/android/exoplayer2/e2etest/MergingPlaylistPlaybackTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.exoplayer2.e2etest;
+
+import static org.junit.Assume.assumeTrue;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.view.Surface;
+import androidx.test.core.app.ApplicationProvider;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.robolectric.PlaybackOutput;
+import com.google.android.exoplayer2.robolectric.ShadowMediaCodecConfig;
+import com.google.android.exoplayer2.robolectric.TestPlayerRunHelper;
+import com.google.android.exoplayer2.source.ClippingMediaSource;
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
+import com.google.android.exoplayer2.source.FilteringMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MergingMediaSource;
+import com.google.android.exoplayer2.testutil.CapturingRenderersFactory;
+import com.google.android.exoplayer2.testutil.DumpFileAsserts;
+import com.google.android.exoplayer2.testutil.FakeClock;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.ParameterizedRobolectricTestRunner;
+
+/** End-to-end tests for playlists with merged media. */
+@RunWith(ParameterizedRobolectricTestRunner.class)
+public final class MergingPlaylistPlaybackTest {
+
+  private static final String TEST_MP4_URI = "asset:///media/mp4/sample.mp4";
+
+  @ParameterizedRobolectricTestRunner.Parameters(
+      name = "videoPrimary({0}), first({1}_{2}), second({3}_{4})")
+  public static List<Boolean[]> configs() {
+    return Sets.cartesianProduct(
+            /* videoIsPrimaryMergedSource */ ImmutableSet.of(true, false),
+            /* firstItemVideoClipped */ ImmutableSet.of(true, false),
+            /* firstItemAudioClipped */ ImmutableSet.of(true, false),
+            /* secondItemVideoClipped */ ImmutableSet.of(true, false),
+            /* secondItemAudioClipped */ ImmutableSet.of(true, false))
+        .stream()
+        .map(s -> s.toArray(new Boolean[0]))
+        .collect(Collectors.toList());
+  }
+
+  @ParameterizedRobolectricTestRunner.Parameter(0)
+  public Boolean videoIsPrimaryMergedSource;
+
+  @ParameterizedRobolectricTestRunner.Parameter(1)
+  public Boolean firstItemVideoClipped;
+
+  @ParameterizedRobolectricTestRunner.Parameter(2)
+  public Boolean firstItemAudioClipped;
+
+  @ParameterizedRobolectricTestRunner.Parameter(3)
+  public Boolean secondItemVideoClipped;
+
+  @ParameterizedRobolectricTestRunner.Parameter(4)
+  public Boolean secondItemAudioClipped;
+
+  @Rule
+  public ShadowMediaCodecConfig mediaCodecConfig =
+      ShadowMediaCodecConfig.forAllSupportedMimeTypes();
+
+  @Test
+  public void test() throws Exception {
+    // TODO: These cases are reliably failing due to a bug in ExoPlayerImplInternal that doesn't
+    // reset its loading period when reading a discontinuity and the current period requires more
+    // loading as a result.
+    assumeTrue(!firstItemVideoClipped);
+
+    Context applicationContext = ApplicationProvider.getApplicationContext();
+    CapturingRenderersFactory capturingRenderersFactory =
+        new CapturingRenderersFactory(applicationContext);
+    ExoPlayer player =
+        new ExoPlayer.Builder(applicationContext, capturingRenderersFactory)
+            .setClock(new FakeClock(/* isAutoAdvancing= */ true))
+            .build();
+    Surface surface = new Surface(new SurfaceTexture(/* texName= */ 1));
+    player.setVideoSurface(surface);
+    PlaybackOutput playbackOutput = PlaybackOutput.register(player, capturingRenderersFactory);
+
+    player.addMediaSource(createMergingMediaSource(firstItemVideoClipped, firstItemAudioClipped));
+    player.addMediaSource(createMergingMediaSource(secondItemVideoClipped, secondItemAudioClipped));
+    player.prepare();
+    player.play();
+    TestPlayerRunHelper.runUntilPlaybackState(player, Player.STATE_ENDED);
+    player.release();
+    surface.release();
+
+    DumpFileAsserts.assertOutput(
+        applicationContext,
+        playbackOutput,
+        "playbackdumps/merging/"
+            + (videoIsPrimaryMergedSource ? "video" : "audio")
+            + "_"
+            + firstItemVideoClipped
+            + "_"
+            + firstItemAudioClipped
+            + "_"
+            + secondItemVideoClipped
+            + "_"
+            + secondItemAudioClipped
+            + ".dump");
+  }
+
+  private MergingMediaSource createMergingMediaSource(boolean videoClipped, boolean audioClipped) {
+    MediaSource.Factory mediaSourceFactory =
+        new DefaultMediaSourceFactory((Context) ApplicationProvider.getApplicationContext());
+    MediaSource videoSource =
+        new FilteringMediaSource(
+            mediaSourceFactory.createMediaSource(MediaItem.fromUri(TEST_MP4_URI)),
+            C.TRACK_TYPE_VIDEO);
+    MediaSource audioSource =
+        new FilteringMediaSource(
+            mediaSourceFactory.createMediaSource(MediaItem.fromUri(TEST_MP4_URI)),
+            C.TRACK_TYPE_AUDIO);
+    if (videoClipped) {
+      videoSource =
+          new ClippingMediaSource(
+              videoSource, /* startPositionUs= */ 300_000, /* endPositionUs= */ 600_000);
+    }
+    if (audioClipped) {
+      audioSource =
+          new ClippingMediaSource(
+              audioSource, /* startPositionUs= */ 500_000, /* endPositionUs= */ 800_000);
+    }
+    return new MergingMediaSource(
+        /* adjustPeriodTimeOffsets= */ true,
+        /* clipDurations= */ true,
+        videoIsPrimaryMergedSource
+            ? new MediaSource[] {videoSource, audioSource}
+            : new MediaSource[] {audioSource, videoSource});
+  }
+}
