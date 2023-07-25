3a14d323be5642a3a74b8b5666d3a9bac7e1517c
tonihei <tonihei@google.com>
2023-07-25 17:01:07 +0100

    Fix last sample detection issues
    1. For codecs that change timestamps, we need to use greater
       or equal instead of just equal for comparison.
    2. We should only make this comparison once a value has been
       set.
    3. The largest queue timestamp isn't updated before using it
       for this detection, meaning that we also mark the
       second-to-last sample as the last one.
    
    PiperOrigin-RevId: 550901978

--- library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecRenderer.java
@@ -1311,11 +1311,6 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
       return true;
     }
 
-    if (hasReadStreamToEnd() || buffer.isLastSample()) {
-      // Notify output queue of the last buffer's timestamp.
-      lastBufferInStreamPresentationTimeUs = largestQueuedPresentationTimeUs;
-    }
-
     if (result == C.RESULT_NOTHING_READ) {
       return false;
     }
@@ -1403,6 +1398,10 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
       waitingForFirstSampleInFormat = false;
     }
     largestQueuedPresentationTimeUs = max(largestQueuedPresentationTimeUs, presentationTimeUs);
+    if (hasReadStreamToEnd() || buffer.isLastSample()) {
+      // Notify output queue of the last buffer's timestamp.
+      lastBufferInStreamPresentationTimeUs = largestQueuedPresentationTimeUs;
+    }
     buffer.flip();
     if (buffer.hasSupplementalData()) {
       handleInputBufferSupplementalData(buffer);
@@ -1921,11 +1920,12 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
           && outputBufferInfo.presentationTimeUs == 0
           && (outputBufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0
           && largestQueuedPresentationTimeUs != C.TIME_UNSET) {
-        outputBufferInfo.presentationTimeUs = largestQueuedPresentationTimeUs;
+        outputBufferInfo.presentationTimeUs = lastBufferInStreamPresentationTimeUs;
       }
       isDecodeOnlyOutputBuffer = outputBufferInfo.presentationTimeUs < getLastResetPositionUs();
       isLastOutputBuffer =
-          lastBufferInStreamPresentationTimeUs == outputBufferInfo.presentationTimeUs;
+          lastBufferInStreamPresentationTimeUs != C.TIME_UNSET
+              && lastBufferInStreamPresentationTimeUs <= outputBufferInfo.presentationTimeUs;
       updateOutputFormatForTime(outputBufferInfo.presentationTimeUs);
     }
 
