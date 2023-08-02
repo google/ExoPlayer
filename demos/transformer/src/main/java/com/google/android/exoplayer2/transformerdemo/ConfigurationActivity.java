426f31ae7e423666f0cfb0256c6c34fde721d7c3
samrobinson <samrobinson@google.com>
2023-08-02 10:01:15 +0000

    Add basic volume scaling to the Transformer demo app.
    PiperOrigin-RevId: 553086635

--- demos/transformer/src/main/java/com/google/android/exoplayer2/transformerdemo/ConfigurationActivity.java
+++ demos/transformer/src/main/java/com/google/android/exoplayer2/transformerdemo/ConfigurationActivity.java
@@ -125,6 +125,7 @@ public final class ConfigurationActivity extends AppCompatActivity {
   public static final int SAMPLE_RATE_INDEX = 1;
   public static final int SKIP_SILENCE_INDEX = 2;
   public static final int CHANNEL_MIXING_INDEX = 3;
+  public static final int VOLUME_SCALING_INDEX = 4;
 
   // Color filter options.
   public static final int COLOR_FILTER_GRAYSCALE = 0;
@@ -171,7 +172,11 @@ public final class ConfigurationActivity extends AppCompatActivity {
     "720p H264 video with no audio",
   };
   private static final String[] AUDIO_EFFECTS = {
-    "High pitched", "Sample rate of 48000Hz", "Skip silence", "Mix channels into mono"
+    "High pitched",
+    "Sample rate of 48000Hz",
+    "Skip silence",
+    "Mix channels into mono",
+    "Scale volume to 50%"
   };
   private static final String[] VIDEO_EFFECTS = {
     "Dizzy crop",
