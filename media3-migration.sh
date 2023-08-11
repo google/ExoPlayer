8220b1b7763033313bd409853da7c11943c94a89
tianyifeng <tianyifeng@google.com>
2023-08-11 14:42:13 +0000

    Bump version numbers to Media3 1.1.1 and ExoPlayer 2.19.1
    PiperOrigin-RevId: 555948664
    (cherry picked from commit 5501f1335c98f4b771fc24dfee781e41302dbfef)

--- media3-migration.sh
+++ media3-migration.sh
@@ -128,8 +128,8 @@ extension-workmanager media3-exoplayer-workmanager'
 # Rewrites classes, packages and dependencies from the legacy ExoPlayer package structure
 # to androidx.media3 structure.
 
-MEDIA3_VERSION="1.1.0"
-LEGACY_PEER_VERSION="2.19.0"
+MEDIA3_VERSION="1.1.1"
+LEGACY_PEER_VERSION="2.19.1"
 
 function usage() {
   echo "usage: $0 [-p|-c|-d|-v]|[-m|-l [-x <path>] [-f] PROJECT_ROOT]"
