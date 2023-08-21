9ee8c96e31d57304b9ffea5c472797531539704f
rohks <rohks@google.com>
2023-08-21 15:32:24 +0100

    Add nullness annotations to `SimpleCache`
    #fixit
    
    PiperOrigin-RevId: 558783228

--- library/datasource/src/main/java/com/google/android/exoplayer2/upstream/cache/SimpleCache.java
+++ library/datasource/src/main/java/com/google/android/exoplayer2/upstream/cache/SimpleCache.java
@@ -656,7 +656,8 @@ public final class SimpleCache implements Cache {
       updateFile = true;
     }
     SimpleCacheSpan newSpan =
-        contentIndex.get(key).setLastTouchTimestamp(span, lastTouchTimestamp, updateFile);
+        Assertions.checkNotNull(contentIndex.get(key))
+            .setLastTouchTimestamp(span, lastTouchTimestamp, updateFile);
     notifySpanTouched(span, newSpan);
     return newSpan;
   }
@@ -677,7 +678,7 @@ public final class SimpleCache implements Cache {
     }
     while (true) {
       SimpleCacheSpan span = cachedContent.getSpan(position, length);
-      if (span.isCached && span.file.length() != span.length) {
+      if (span.isCached && Assertions.checkNotNull(span.file).length() != span.length) {
         // The file has been modified or deleted underneath us. It's likely that other files will
         // have been modified too, so scan the whole in-memory representation.
         removeStaleSpans();
@@ -705,7 +706,7 @@ public final class SimpleCache implements Cache {
     }
     totalSpace -= span.length;
     if (fileIndex != null) {
-      String fileName = span.file.getName();
+      String fileName = Assertions.checkNotNull(span.file).getName();
       try {
         fileIndex.remove(fileName);
       } catch (IOException e) {
@@ -726,7 +727,7 @@ public final class SimpleCache implements Cache {
     ArrayList<CacheSpan> spansToBeRemoved = new ArrayList<>();
     for (CachedContent cachedContent : contentIndex.getAll()) {
       for (CacheSpan span : cachedContent.getSpans()) {
-        if (span.file.length() != span.length) {
+        if (Assertions.checkNotNull(span.file).length() != span.length) {
           spansToBeRemoved.add(span);
         }
       }
