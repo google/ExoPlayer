40da4316e75446d21258e60a577b755f5cbc56cc
rohks <rohks@google.com>
2023-08-22 11:29:00 +0100

    Add nullness annotations to `SampleQueue`
    #fixit
    
    PiperOrigin-RevId: 559058211

--- library/core/src/main/java/com/google/android/exoplayer2/source/SampleQueue.java
+++ library/core/src/main/java/com/google/android/exoplayer2/source/SampleQueue.java
@@ -833,14 +833,14 @@ public class SampleQueue implements TrackOutput {
 
     if (sharedSampleMetadata.isEmpty()
         || !sharedSampleMetadata.getEndValue().format.equals(upstreamFormat)) {
+      Format upstreamFormat = checkNotNull(this.upstreamFormat);
       DrmSessionReference drmSessionReference =
           drmSessionManager != null
               ? drmSessionManager.preacquireSession(drmEventDispatcher, upstreamFormat)
               : DrmSessionReference.EMPTY;
 
       sharedSampleMetadata.appendSpan(
-          getWriteIndex(),
-          new SharedSampleMetadata(checkNotNull(upstreamFormat), drmSessionReference));
+          getWriteIndex(), new SharedSampleMetadata(upstreamFormat, drmSessionReference));
     }
 
     length++;
@@ -924,7 +924,8 @@ public class SampleQueue implements TrackOutput {
    */
   private void onFormatResult(Format newFormat, FormatHolder outputFormatHolder) {
     boolean isFirstFormat = downstreamFormat == null;
-    @Nullable DrmInitData oldDrmInitData = isFirstFormat ? null : downstreamFormat.drmInitData;
+    @Nullable
+    DrmInitData oldDrmInitData = downstreamFormat == null ? null : downstreamFormat.drmInitData;
     downstreamFormat = newFormat;
     @Nullable DrmInitData newDrmInitData = newFormat.drmInitData;
 
