0b86bc363a2561d8e7827336316688b0fc1eb23d
samrobinson <samrobinson@google.com>
2023-10-03 04:04:39 -0700

    Deprecate experimental keepAudioTrackOnSeek methods.
    #minor-release
    
    PiperOrigin-RevId: 570340714

--- library/core/src/main/java/com/google/android/exoplayer2/audio/MediaCodecAudioRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/audio/MediaCodecAudioRenderer.java
@@ -275,14 +275,9 @@ public class MediaCodecAudioRenderer extends MediaCodecRenderer implements Media
   }
 
   /**
-   * Sets whether to enable the experimental feature that keeps and flushes the {@link
-   * android.media.AudioTrack} when a seek occurs, as opposed to releasing and reinitialising. Off
-   * by default.
-   *
-   * <p>This method is experimental, and will be renamed or removed in a future release.
-   *
-   * @param enableKeepAudioTrackOnSeek Whether to keep the {@link android.media.AudioTrack} on seek.
+   * @deprecated Experimental method being removed.
    */
+  @Deprecated
   public void experimentalSetEnableKeepAudioTrackOnSeek(boolean enableKeepAudioTrackOnSeek) {
     this.experimentalKeepAudioTrackOnSeek = enableKeepAudioTrackOnSeek;
   }
