dc0bee930770bf6676c6259700dfbbf0b049812d
claincly <claincly@google.com>
2023-10-17 04:34:01 -0700

    Perform case insensitive comparison in tests
    The model is "Redmi 6A" on the device
    
    PiperOrigin-RevId: 574103629

--- libraries/transformer/src/androidTest/java/androidx/media3/transformer/TransformerSequenceEffectTest.java
+++ libraries/transformer/src/androidTest/java/androidx/media3/transformer/TransformerSequenceEffectTest.java
@@ -45,6 +45,7 @@ import androidx.media3.effect.RgbFilter;
 import androidx.media3.effect.ScaleAndRotateTransformation;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.google.common.base.Ascii;
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.util.List;
@@ -118,7 +119,9 @@ public final class TransformerSequenceEffectTest {
     assumeFalse(
         "Some older MediaTek encoders have a pixel alignment of 16, which results in a 360 pixel"
             + " width being re-scaled to 368.",
-        SDK_INT == 27 && (Util.MODEL.equals("redmi 6a") || Util.MODEL.equals("vivo 1820")));
+        SDK_INT == 27
+            && (Ascii.equalsIgnoreCase(Util.MODEL, "redmi 6a")
+                || Ascii.equalsIgnoreCase(Util.MODEL, "vivo 1820")));
 
     if (AndroidTestUtil.skipAndLogIfFormatsUnsupported(
         context,
