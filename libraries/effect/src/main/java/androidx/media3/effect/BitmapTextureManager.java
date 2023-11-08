f172923f0d09582d0804a8c6805aced7ed98d27b
claincly <claincly@google.com>
2023-11-08 10:37:02 -0800

    Remove possibly retained bitmap reference
    PiperOrigin-RevId: 580579054

--- libraries/effect/src/main/java/androidx/media3/effect/BitmapTextureManager.java
+++ libraries/effect/src/main/java/androidx/media3/effect/BitmapTextureManager.java
@@ -53,9 +53,11 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
   private @MonotonicNonNull GlTextureInfo currentGlTextureInfo;
   private int downstreamShaderProgramCapacity;
-  // TODO - b/262693274: Support HDR. Currently this variable is not used and will trigger error
-  //  prone warning.
+
+  // TODO - b/262693274: Support HDR.
+  @SuppressWarnings({"UnusedVariable", "FieldCanBeLocal"})
   private boolean useHdr;
+
   private boolean currentInputStreamEnded;
   private boolean isNextFrameInTexture;
 
@@ -127,6 +129,7 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
           if (currentGlTextureInfo != null) {
             currentGlTextureInfo.release();
           }
+          pendingBitmaps.clear();
         });
   }
 
@@ -174,7 +177,8 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
     if (!currentBitmapInfo.inStreamOffsetsUs.hasNext()) {
       isNextFrameInTexture = false;
-      pendingBitmaps.remove();
+      BitmapFrameSequenceInfo finishedBitmapInfo = pendingBitmaps.remove();
+      finishedBitmapInfo.bitmap.recycle();
       if (pendingBitmaps.isEmpty() && currentInputStreamEnded) {
         // Only signal end of stream after all pending bitmaps are processed.
         shaderProgram.signalEndOfCurrentInputStream();
