f8fe5f2dda54784a44a9416dbb0365e2a187ee6f
jbibik <jbibik@google.com>
2023-11-14 10:07:57 -0800

    Add HLS support for parsing TTML subs muxed into MP4 during extraction
    This removes the flakiness of the HLS playback test as well.
    
    Previously, this flow only worked for standalone WebVTT subtitles (https://github.com/google/ExoPlayer/commit/f6aefcefce9fdddfdbc8c9c3119ecfdc2c221297)
    
    PiperOrigin-RevId: 582359383

--- library/hls/src/test/java/com/google/android/exoplayer2/source/hls/e2etest/HlsPlaybackTest.java
+++ library/hls/src/test/java/com/google/android/exoplayer2/source/hls/e2etest/HlsPlaybackTest.java
@@ -32,7 +32,6 @@ import com.google.android.exoplayer2.testutil.CapturingRenderersFactory;
 import com.google.android.exoplayer2.testutil.DumpFileAsserts;
 import com.google.android.exoplayer2.testutil.FakeClock;
 import com.google.android.exoplayer2.upstream.DefaultDataSource;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -72,14 +71,15 @@ public final class HlsPlaybackTest {
   }
 
   @Test
-  @Ignore(
-      "Disabled until subtitles are reliably asserted in robolectric tests [internal b/174661563].")
   public void ttmlInMp4() throws Exception {
     Context applicationContext = ApplicationProvider.getApplicationContext();
     CapturingRenderersFactory capturingRenderersFactory =
         new CapturingRenderersFactory(applicationContext);
     ExoPlayer player =
         new ExoPlayer.Builder(applicationContext, capturingRenderersFactory)
+            .setMediaSourceFactory(
+                new HlsMediaSource.Factory(new DefaultDataSource.Factory(applicationContext))
+                    .experimentalParseSubtitlesDuringExtraction(true))
             .setClock(new FakeClock(/* isAutoAdvancing= */ true))
             .build();
     player.setVideoSurface(new Surface(new SurfaceTexture(/* texName= */ 1)));
