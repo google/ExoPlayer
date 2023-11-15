d5fbf0007b8a0fdf3ce02ba9771447ca05d4a476
samrobinson <samrobinson@google.com>
2023-11-15 09:12:32 -0800

    Migrate to Util.durationUsToSampleCount in transformer audio.
    PiperOrigin-RevId: 582700443

--- libraries/transformer/src/main/java/androidx/media3/transformer/SilentAudioGenerator.java
+++ libraries/transformer/src/main/java/androidx/media3/transformer/SilentAudioGenerator.java
@@ -16,7 +16,6 @@
 
 package androidx.media3.transformer;
 
-import androidx.media3.common.C;
 import androidx.media3.common.audio.AudioProcessor.AudioFormat;
 import androidx.media3.common.util.Util;
 import java.nio.ByteBuffer;
@@ -49,14 +48,11 @@ import java.util.concurrent.atomic.AtomicLong;
    * @param durationUs The duration of the additional silence to generate, in microseconds.
    */
   public void addSilence(long durationUs) {
-    // The number of frames is not a timestamp, however this utility method provides
-    // overflow-safe multiplication & division.
-    long outputFrameCount =
-        Util.scaleLargeTimestamp(
-            /* timestamp= */ durationUs,
-            /* multiplier= */ audioFormat.sampleRate,
-            /* divisor= */ C.MICROS_PER_SECOND);
+    long outputFrameCount = Util.durationUsToSampleCount(durationUs, audioFormat.sampleRate);
+    // If the durationUs maps to a non-integer number of samples, then an extra sample is output.
+    // In the worst case, this is one sample (~22us of audio) per media item.
 
+    // TODO(b/260618558): Track leftover duration when generating in mixer.
     remainingBytesToOutput.addAndGet(audioFormat.bytesPerFrame * outputFrameCount);
   }
 
