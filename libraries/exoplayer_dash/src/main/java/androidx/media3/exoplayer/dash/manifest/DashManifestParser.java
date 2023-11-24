479344d74e1e63d83cd94ed14517e27030c85e6a
ibaker <ibaker@google.com>
2023-11-24 00:29:48 -0800

    Fix typo in `DashManifestParser`
    PiperOrigin-RevId: 585017285

--- libraries/exoplayer_dash/src/main/java/androidx/media3/exoplayer/dash/manifest/DashManifestParser.java
+++ libraries/exoplayer_dash/src/main/java/androidx/media3/exoplayer/dash/manifest/DashManifestParser.java
@@ -648,9 +648,11 @@ public class DashManifestParser extends DefaultHandler
   }
 
   /**
-   * Parses children of AdaptationSet elements not specifically parsed elsewhere.
+   * Parses a child of an {@link AdaptationSet} element.
    *
-   * @param xpp The XmpPullParser from which the AdaptationSet child should be parsed.
+   * <p>Called for child elements that are not specifically parsed elsewhere.
+   *
+   * @param xpp The {@link XmlPullParser} from which the child should be parsed.
    * @throws XmlPullParserException If an error occurs parsing the element.
    * @throws IOException If an error occurs reading the element.
    */
