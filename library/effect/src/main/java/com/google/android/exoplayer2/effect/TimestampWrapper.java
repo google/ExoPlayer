e19c78a2be6912c7a284ee942c08b18a3f4dc1db
huangdarwin <huangdarwin@google.com>
2023-11-27 03:22:49 -0800

    Document TimestampWrapper not working with previewing.
    Mentioned in https://github.com/androidx/media/issues/821
    
    PiperOrigin-RevId: 585595948

--- library/effect/src/main/java/com/google/android/exoplayer2/effect/TimestampWrapper.java
+++ library/effect/src/main/java/com/google/android/exoplayer2/effect/TimestampWrapper.java
@@ -26,6 +26,8 @@ import com.google.android.exoplayer2.util.VideoFrameProcessingException;
  * Applies a {@link GlEffect} from {@code startTimeUs} to {@code endTimeUs}, and no change on all
  * other timestamps.
  *
+ * <p>This currently does not work with {@code ExoPlayer#setVideoEffects}.
+ *
  * @deprecated com.google.android.exoplayer2 is deprecated. Please migrate to androidx.media3 (which
  *     contains the same ExoPlayer code). See <a
  *     href="https://developer.android.com/guide/topics/media/media3/getting-started/migration-guide">the
