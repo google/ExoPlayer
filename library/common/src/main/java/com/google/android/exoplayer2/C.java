d611911189e45a111841ac73d80be24954db6d02
tonihei <tonihei@google.com>
2023-12-20 08:13:51 -0800

    Use platform constants for PCM 24/32 bit encoding
    These constants are used when setting up the AudioTrack
    and should match the definition in the platform.
    
    PiperOrigin-RevId: 592564644

--- library/common/src/main/java/com/google/android/exoplayer2/C.java
+++ library/common/src/main/java/com/google/android/exoplayer2/C.java
@@ -249,13 +249,13 @@ public final class C {
   public static final int ENCODING_PCM_16BIT_BIG_ENDIAN = 0x10000000;
 
   /** PCM encoding with 24 bits per sample. */
-  public static final int ENCODING_PCM_24BIT = 0x20000000;
+  public static final int ENCODING_PCM_24BIT = AudioFormat.ENCODING_PCM_24BIT_PACKED;
 
   /** Like {@link #ENCODING_PCM_24BIT} but with the bytes in big endian order. */
   public static final int ENCODING_PCM_24BIT_BIG_ENDIAN = 0x50000000;
 
   /** PCM encoding with 32 bits per sample. */
-  public static final int ENCODING_PCM_32BIT = 0x30000000;
+  public static final int ENCODING_PCM_32BIT = AudioFormat.ENCODING_PCM_32BIT;
 
   /** Like {@link #ENCODING_PCM_32BIT} but with the bytes in big endian order. */
   public static final int ENCODING_PCM_32BIT_BIG_ENDIAN = 0x60000000;
