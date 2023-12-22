e23af10de100791845d397cb07c163ba565506ff
ibaker <ibaker@google.com>
2023-12-22 03:39:56 -0800

    Add parentheses to fix `UnexpectedLoaderException` message logic
    These were missing in https://github.com/google/ExoPlayer/commit/ad924db324dd6a7c228fca07e554801ae06b53e2
    
    Without these, the `": null"` is still logged (but the `"Unexpected
    IllegalStateException"` bit is not), because the **whole** string
    concatenation is compared to null as the boolean condition of the
    ternary, and this condition is always false (the result of a string
    concatentation is never `null`) i.e. (with excess parentheses for
    clarity):
    
    ```
    (("Unexpected " + cause.getClass().getSimpleName() + cause.getMessage()) != null)
        ? ": " + cause.getMessage()
        : ""
    ```
    
    Also add a test because obviously this isn't as simple as I'd thought.
    
    PiperOrigin-RevId: 593079975

--- library/core/src/main/java/com/google/android/exoplayer2/upstream/Loader.java
+++ library/core/src/main/java/com/google/android/exoplayer2/upstream/Loader.java
@@ -54,9 +54,9 @@ public final class Loader implements LoaderErrorThrower {
 
     public UnexpectedLoaderException(Throwable cause) {
       super(
-          "Unexpected " + cause.getClass().getSimpleName() + cause.getMessage() != null
-              ? ": " + cause.getMessage()
-              : "",
+          "Unexpected "
+              + cause.getClass().getSimpleName()
+              + (cause.getMessage() != null ? ": " + cause.getMessage() : ""),
           cause);
     }
   }
