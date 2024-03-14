47c5ee0e40304790b5ddcfefb9bb5fea8a9e442f
kimvde <kimvde@google.com>
2024-03-14 03:41:45 -0700

    Add clipFloatOutput parameter to AudioMixer
    PiperOrigin-RevId: 615720340

--- library/common/src/main/java/com/google/android/exoplayer2/audio/ChannelMixingAudioProcessor.java
+++ library/common/src/main/java/com/google/android/exoplayer2/audio/ChannelMixingAudioProcessor.java
@@ -89,7 +89,8 @@ public final class ChannelMixingAudioProcessor extends BaseAudioProcessor {
         outputAudioFormat,
         channelMixingMatrix,
         framesToMix,
-        /* accumulate= */ false);
+        /* accumulate= */ false,
+        /* clipFloatOutput= */ true);
     outputBuffer.flip();
   }
 }
