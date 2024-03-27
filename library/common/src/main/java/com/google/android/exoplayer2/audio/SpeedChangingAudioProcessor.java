51ce53e48619728039a1c348f249374356521bf7
tofunmi <tofunmi@google.com>
2024-03-27 10:52:44 -0700

    Make calculateSpeedAdjustedTime() output monotonically increasing
    calculate based on the output of consecutive calls rather than the speed provider speed change point to ensure the timestamps are monotonically increasing.
    
    PiperOrigin-RevId: 619584001

--- library/common/src/main/java/com/google/android/exoplayer2/audio/SpeedChangingAudioProcessor.java
+++ library/common/src/main/java/com/google/android/exoplayer2/audio/SpeedChangingAudioProcessor.java
@@ -16,6 +16,7 @@
 
 package com.google.android.exoplayer2.audio;
 
+import static com.google.android.exoplayer2.util.Assertions.checkArgument;
 import static java.lang.Math.min;
 import static java.lang.Math.round;
 
@@ -64,8 +65,11 @@ public final class SpeedChangingAudioProcessor extends BaseAudioProcessor {
 
   private float currentSpeed;
   private long bytesRead;
-  private long lastProcessedInputTime;
+  private long lastProcessedInputTimeUs;
+  private long lastSpeedAdjustedInputTimeUs;
+  private long lastSpeedAdjustedOutputTimeUs;
   private boolean endOfStreamQueuedToSonic;
+  private long speedAdjustedTimeAsyncInputTimeUs;
 
   public SpeedChangingAudioProcessor(SpeedProvider speedProvider) {
     this.speedProvider = speedProvider;
@@ -78,6 +82,7 @@ public final class SpeedChangingAudioProcessor extends BaseAudioProcessor {
     inputSegmentStartTimesUs.add(0);
     outputSegmentStartTimesUs.add(0);
     currentSpeed = 1f;
+    speedAdjustedTimeAsyncInputTimeUs = C.TIME_UNSET;
   }
 
   @Override
@@ -147,7 +152,7 @@ public final class SpeedChangingAudioProcessor extends BaseAudioProcessor {
       buffer.flip();
     }
     bytesRead += inputBuffer.position() - startPosition;
-    lastProcessedInputTime = updateLastProcessedInputTime();
+    lastProcessedInputTimeUs = updateLastProcessedInputTime();
     inputBuffer.limit(inputBufferLimit);
   }
 
@@ -204,8 +209,11 @@ public final class SpeedChangingAudioProcessor extends BaseAudioProcessor {
    *     from the caller of this method.
    */
   public void getSpeedAdjustedTimeAsync(long inputTimeUs, LongConsumer callback) {
+    checkArgument(speedAdjustedTimeAsyncInputTimeUs < inputTimeUs);
+    speedAdjustedTimeAsyncInputTimeUs = inputTimeUs;
     synchronized (pendingCallbacksLock) {
-      if (inputTimeUs <= lastProcessedInputTime || isEnded()) {
+      if ((inputTimeUs <= lastProcessedInputTimeUs && pendingCallbackInputTimesUs.isEmpty())
+          || isEnded()) {
         callback.accept(calculateSpeedAdjustedTime(inputTimeUs));
         return;
       }
@@ -223,11 +231,16 @@ public final class SpeedChangingAudioProcessor extends BaseAudioProcessor {
     while (floorIndex > 0 && inputSegmentStartTimesUs.get(floorIndex) > inputTimeUs) {
       floorIndex--;
     }
-    long lastSegmentInputDuration = inputTimeUs - inputSegmentStartTimesUs.get(floorIndex);
     long lastSegmentOutputDuration;
     if (floorIndex == inputSegmentStartTimesUs.size() - 1) {
+      if (lastSpeedAdjustedInputTimeUs < inputSegmentStartTimesUs.get(floorIndex)) {
+        lastSpeedAdjustedInputTimeUs = inputSegmentStartTimesUs.get(floorIndex);
+        lastSpeedAdjustedOutputTimeUs = outputSegmentStartTimesUs.get(floorIndex);
+      }
+      long lastSegmentInputDuration = inputTimeUs - lastSpeedAdjustedInputTimeUs;
       lastSegmentOutputDuration = getPlayoutDurationAtCurrentSpeed(lastSegmentInputDuration);
     } else {
+      long lastSegmentInputDuration = inputTimeUs - lastSpeedAdjustedInputTimeUs;
       lastSegmentOutputDuration =
           round(
               lastSegmentInputDuration
@@ -237,7 +250,9 @@ public final class SpeedChangingAudioProcessor extends BaseAudioProcessor {
                       inputSegmentStartTimesUs.get(floorIndex + 1)
                           - inputSegmentStartTimesUs.get(floorIndex)));
     }
-    return outputSegmentStartTimesUs.get(floorIndex) + lastSegmentOutputDuration;
+    lastSpeedAdjustedInputTimeUs = inputTimeUs;
+    lastSpeedAdjustedOutputTimeUs += lastSegmentOutputDuration;
+    return lastSpeedAdjustedOutputTimeUs;
   }
 
   private static double divide(long dividend, long divisor) {
@@ -247,7 +262,7 @@ public final class SpeedChangingAudioProcessor extends BaseAudioProcessor {
   private void processPendingCallbacks() {
     synchronized (pendingCallbacksLock) {
       while (!pendingCallbacks.isEmpty()
-          && (pendingCallbackInputTimesUs.element() <= lastProcessedInputTime || isEnded())) {
+          && (pendingCallbackInputTimesUs.element() <= lastProcessedInputTimeUs || isEnded())) {
         pendingCallbacks
             .remove()
             .accept(calculateSpeedAdjustedTime(pendingCallbackInputTimesUs.remove()));
